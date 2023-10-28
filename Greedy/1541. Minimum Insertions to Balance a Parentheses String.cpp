871fecf18ca69d90d507565f9e230dc1148f4074
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-10-28 21:40:31 +0800

    Update 1541. Minimum Insertions to Balance a Parentheses String.cpp

--- Greedy/1541. Minimum Insertions to Balance a Parentheses String.cpp
+++ Greedy/1541. Minimum Insertions to Balance a Parentheses String.cpp
@@ -2,6 +2,39 @@
 1541. Minimum Insertions to Balance a Parentheses String
 */
 
+//這個寫法更好一點 可以應對如果變成 一個左括弧需要三個右括號的狀況 if(count%3 != 0)...res+=(count%3); count-=(count%3);
+class Solution {
+public:
+    int minInsertions(string s) {
+        int count = 0;
+        int res = 0;
+        for(int i = 0; i<s.size(); i++){
+            if(s[i] == '('){
+                count+=2;
+                //一個左括號需要兩個右括號 如果這時count 為奇數代表還缺一個右括號的時候就接到另一個左括號了
+                if(count%2 == 1){
+                    res+=1;
+                    count-=1;
+                }
+            }
+            else if(s[i] == ')'){
+                count-=1;
+                if(count == -1){
+                    res+=1;
+                    count+=2;
+                }
+            }
+        }
+
+        //缺右括號
+        if(count > 0){
+            res+=count;
+        }
+
+        return res;
+    }
+};
+
 //greedy TC:O(n) SC:O(1)
 class Solution {
     //greedy
