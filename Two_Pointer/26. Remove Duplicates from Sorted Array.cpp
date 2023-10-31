0afc61af1f8d330c4f95614047fa48fa400d793e
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-10-31 22:08:35 +0800

    Create 26. Remove Duplicates from Sorted Array.cpp

---
+++ Two_Pointer/26. Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,20 @@
+/*
+26. Remove Duplicates from Sorted Array
+*/
+
+//TC:O(n) SC:O(1)
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int slow = 0; int fast = 1;
+        while(fast < nums.size()){
+            if(nums[slow] != nums[fast]){
+                slow++;
+                nums[slow] = nums[fast];
+            }
+            fast++;
+        }
+
+        return slow+1;
+    }
+};
