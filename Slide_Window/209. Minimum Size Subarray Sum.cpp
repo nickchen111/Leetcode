688b7338ce6c11beecd1c8859b200831fda55257
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-11-01 18:09:14 +0800

    Update 209. Minimum Size Subarray Sum.cpp

--- Slide_Window/209. Minimum Size Subarray Sum.cpp
+++ Slide_Window/209. Minimum Size Subarray Sum.cpp
@@ -2,35 +2,53 @@
 209. Minimum Size Subarray Sum
 */
 
-// slide window TC: O(n) sc:O(1)
+//Deque TC: O(n) SC:O(n)
 class Solution {
-    // slide window
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
+        //deque
+        deque<int> dq;
         int sum = 0;
-        int res = INT_MAX;
-        int count = 0;
 
-        int left = 0; int right = 0;
-        while(right < nums.size()){
-            //擴大窗口
-            sum+=nums[right];
-            count++;
-            right++;
-            //縮小窗口
-            while(sum >= target){
-                res = min(res, count);
-                sum-=nums[left];
-                left++;
-                count--;
+        int res = INT_MAX/2;
+        for(int i =0; i<nums.size(); i++){
+            dq.push_back(i);
+            sum+=nums[i];
+            while(!dq.empty() && sum >= target){
+                res = min(res, i-dq.front()+1);
+                sum-=nums[dq.front()];
+                dq.pop_front();
             }
         }
 
-        return res == INT_MAX ? 0:res;
+        return res == INT_MAX/2 ? 0:res;
+           
     }
 };
 
-/*
-感覺slide window 
-presum都可以解
-*/
+// Slide Window TC: O(n) SC:O(1)
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        // slide window
+        int sum = 0;
+        int res = INT_MAX/2;
+        int left = 0;
+        int right = 0;
+        int n = nums.size();
+        while(right < n){
+            //開始擴大窗口
+            sum+=nums[right];
+
+            //縮小窗口
+            while(left < n && sum >= target){
+                res = min(res, right-left+1);
+                sum-=nums[left];
+                left++;
+            }
+            right++;
+        }
+
+        return res == INT_MAX/2 ? 0:res;
+    }
+};
