0cc342225f56447877d992bfeaddd553964d952f
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-11-08 16:18:35 +0800

    Create 901. Online Stock Span.cpp

---
+++ Stack/901. Online Stock Span.cpp
@@ -0,0 +1,41 @@
+/*
+901. Online Stock Span
+*/
+
+// TC:O(n) SC:O(n)
+class StockSpanner {
+    vector<int> arr;
+    stack<int> s;
+    int i;
+public:
+    StockSpanner() {
+        i = 0;
+    }
+    
+    int next(int price) {
+        arr.push_back(price);
+        int res;
+        if(s.empty() || arr[s.top()] > arr[i]){
+            res = 1;
+            s.push(i);
+        }
+        else{
+            while(!s.empty() && arr[s.top()] <= arr[i]){
+                s.pop();
+            }
+            if(!s.empty()){
+                res = i-s.top();//x yyy i
+            }
+            else{
+                res = i+1;
+            }
+            s.push(i);
+        }
+
+        i++;
+        return res;
+    }
+};
+/*
+此題就是求previous smaller or equal
+*/
