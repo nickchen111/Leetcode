b0fd34939513cca7453923f11cedf1a83c356759
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-11-09 22:30:46 +0800

    Create 2903. Find Indices With Index and Value Difference I.cpp

---
+++ Array/2903. Find Indices With Index and Value Difference I.cpp
@@ -0,0 +1,18 @@
+/*
+2903. Find Indices With Index and Value Difference I
+*/
+
+
+// TC:O(n^2) SC:O(1)
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
+        int n = nums.size();
+        for(int i = 0; i<n; i++){
+            for(int j = i+indexDifference; j<n; j++){
+                if(abs(nums[i]-nums[j]) >= valueDifference) return{i,j};
+            }
+        }
+        return {-1,-1};
+    }
+};
