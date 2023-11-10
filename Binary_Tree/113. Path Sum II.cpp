11c76ec73c9a6573c35e9f18375841c2bd6bd8fd
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-11-11 02:50:01 +0800

    Update 113. Path Sum II.cpp

--- Binary_Tree/113. Path Sum II.cpp
+++ Binary_Tree/113. Path Sum II.cpp
@@ -1,1 +1,37 @@
+/*
+113. Path Sum II
+*/
 
+class Solution {
+    vector<int> onpath;
+    vector<vector<int>> ans;
+    int sum = 0;
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        traverse(root, targetSum);
+
+        return ans;
+    }
+    void traverse(TreeNode* root, int targetSum){
+        if(root == nullptr) return;
+
+
+
+        onpath.push_back(root->val);
+        sum += root->val;
+        if(root->left == nullptr && root->right == nullptr && sum == targetSum){
+            ans.push_back(onpath);
+        }
+        traverse(root->left, targetSum);
+        traverse(root->right, targetSum);
+
+
+        onpath.pop_back();
+        sum -=root->val;
+    }
+};
+
+/*
+解題思路 邊走邊紀錄節點 如果數值對 就把此路徑加入解答
+tc: o(n) sc:o(h) 
+*/
