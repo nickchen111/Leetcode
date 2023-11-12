a8c2049beb02127d3bb8f04ee2da62f81e9a97b0
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-11-12 23:05:48 +0800

    Update 222. Count Complete Tree Nodes.cpp

--- Binary_Tree/222. Count Complete Tree Nodes.cpp
+++ Binary_Tree/222. Count Complete Tree Nodes.cpp
@@ -2,17 +2,7 @@
 222. Count Complete Tree Nodes
 */
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+ //滿二叉樹算法 TC:(lgn*lgn) SC:O(lgn)
 class Solution {
 public:
     int countNodes(TreeNode* root) {
