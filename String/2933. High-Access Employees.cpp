87eb16cded5fde0e6f2b6599418a8690ab1f43d5
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-11-12 15:21:46 +0800

    Update 2933. High-Access Employees.cpp

--- String/2933. High-Access Employees.cpp
+++ String/2933. High-Access Employees.cpp
@@ -2,6 +2,48 @@
 2933. High-Access Employees
 */
 
+// Sort
+class Solution {
+public:
+    vector<string> findHighAccessEmployees(vector<vector<string>>& access_times) {
+        unordered_set<string> ret;
+        vector<string> res;
+        unordered_map<string, vector<string>> map;
+        for(int i = 0; i<access_times.size(); i++){
+            map[access_times[i][0]].push_back(access_times[i][1]);
+        }
+       
+       
+        for(auto &p:map){
+            sort(p.second.begin(),p.second.end());
+        }
+        // 0848 0758  0901 0904 0947 
+        
+        for(auto p:map){
+            for(int i = 0; i<p.second.size(); i++){
+                int count = 1;
+                int time1 = stoi(p.second[i]);
+                for(int j = i+1; j<p.second.size(); j++){
+                    int time2 = stoi(p.second[j]);
+                    if(time2-time1 < 100) count+=1;
+                    else break;
+                    if(count == 3){
+                        ret.insert(p.first);
+                        break;
+                    }
+                    
+                }
+            }
+        }
+        
+        for(auto s:ret){
+            res.push_back(s);
+        }
+        
+        return res;
+    }
+};
+
 // TC:O(m*n^2) SC:O(mn) Hash table
 class Solution {
 public:
