26437e4f6c57983be513b7325e2197122b8786ac
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-11-13 15:17:47 +0800

    Create 2785. Sort Vowels in a String.cpp

---
+++ String/2785. Sort Vowels in a String.cpp
@@ -0,0 +1,30 @@
+/*
+2785. Sort Vowels in a String
+*/
+
+// TC:O(n+nlgn) SC:O(n)
+class Solution {
+public:
+    string sortVowels(string s) {
+        int n = s.size();
+        vector<char> table;
+        for(int i =0; i<n; i++){
+            if(string("aeiouAEIOU").find(s[i]) != string::npos){
+                table.push_back(s[i]);
+            }
+        }
+
+        sort(table.begin(),table.end(),greater<char>());
+        string res;
+        for(int i =0; i<n; i++){
+            if(string("aeiouAEIOU").find(s[i]) != string::npos){
+                res+=table.back();
+                table.pop_back();
+            }
+            else res+=s[i];
+        }
+
+        return res;
+
+    }
+};
