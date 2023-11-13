485ec069f6502c9d653f3af28a1df45a5e8ea8c3
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-11-13 20:58:22 +0800

    Update and rename 701_Insert_into_a_Binary_Search_Tree.cpp to 701. Insert into a Binary Search Tree.cpp

--- Binary_Search_Tree/701. Insert into a Binary Search Tree.cpp
+++ Binary_Search_Tree/701. Insert into a Binary Search Tree.cpp
@@ -2,17 +2,7 @@
 701. Insert into a Binary Search Tree
 */
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// Recursion TC:O(h) SC:O(h)
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
@@ -34,5 +24,4 @@ public:
 二叉搜索樹中的增：插入一個數字 
 往左或者往右子樹去找 直到指導適當的位置 最後return root就是把沒找到的那些點return接回去 
 找到時root會等於空 就會新增點上去
-tc: o(h) sc:o(h)
 */
