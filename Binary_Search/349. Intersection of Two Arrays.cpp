a5eb26617d1bfde3c15f72e21e5488066ea8c631
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-11-14 14:31:40 +0800

    Update and rename intersection_of_two_arrays.cpp to 349. Intersection of Two Arrays.cpp

--- Binary_Search/349. Intersection of Two Arrays.cpp
+++ Binary_Search/349. Intersection of Two Arrays.cpp
@@ -1,23 +1,7 @@
 /*
 349. Intersection of Two Arrays
-Given two integer arrays nums1 and nums2, return an array of their intersection. 
-Each element in the result must be unique and you may return the result in any order.
-
-Example 1:
-Input: nums1 = [1,2,2,1], nums2 = [2,2]
-Output: [2]
-
-Example 2:
-Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
-Output: [9,4]
-Explanation: [4,9] is also accepted.
-
 */
 
-#include <iostream>
-#include <vector>
-#include <unordered_set>
-using namespace std;
 
 //用unordered_set的解答 time: o(n) space: o(m)  
 class Solution {
@@ -34,9 +18,7 @@ public:
                 set.erase(x);
             }
         }
-        for(auto a:ans){
-            cout<< a<<endl;
-        }
+
         return ans;
     }
 };
@@ -65,18 +47,7 @@ public:
             }
             else left = mid + 1;
         }
-        }
-        for(auto e:ans){
-            cout << e << endl;
-        }
+
         return ans;
     }
 };
-int main(){
-    vector<int> nums1 = {4,9,5};
-    vector<int> nums2 = {9,4,9,8,4};
-    Solution a;
-    a.intersection(nums1, nums2);
-    Solution1 b;
-    b.intersection(nums1, nums2);
-}
