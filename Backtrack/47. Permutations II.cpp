c0aa72ee183707c673ef24457f31422b806b1c36
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-11-15 01:58:55 +0800

    Update and rename 47_permutations_II.cpp to 47. Permutations II.cpp

--- Backtrack/47. Permutations II.cpp
+++ Backtrack/47. Permutations II.cpp
@@ -1,32 +1,40 @@
 /*
 47. Permutations II
-Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
-
-Example 1:
-Input: nums = [1,1,2]
-Output:
-[[1,1,2],
- [1,2,1],
- [2,1,1]]
-
-Example 2:
-Input: nums = [1,2,3]
-Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
-
-Constraints:
-1 <= nums.length <= 8
--10 <= nums[i] <= 10
 */
 
+// TC: O(n!)  SC: O(n!)
+class Solution {
+    vector<vector<int>> res;
+    vector<int> track;
+    vector<bool> visited;
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        visited = vector<bool>(nums.size(),0);
+        sort(nums.begin(),nums.end());
+        backtrack(nums);
 
+        return res;
+    }
 
-#include <iostream>
-#include <vector>
-#include <unordered_set>
-using namespace std;
+    void backtrack(vector<int>& nums){
+        if(track.size() == nums.size()){
+            res.push_back(track);
+            return;
+        }
 
+        for(int i = 0; i<nums.size(); i++){
+            if(visited[i] == true) continue;
+            if(i && nums[i] == nums[i-1] && visited[i-1] == 0) continue;
+            track.push_back(nums[i]);
+            visited[i] = true;
+            backtrack(nums);
+            track.pop_back();
+            visited[i] = false;
+        }
+    }
+};
 
-//permutation的變形題 除了要考慮遞迴時不能用到自己(需再從i = 0 迴圈頭出發找 所以需要考慮不能用自己) 還要考慮到用到重複使用同樣的數字去做排列組合
+//unordered_set去重 
 class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
@@ -57,46 +65,13 @@ public:
 };
 
 
-//按照la的邏輯寫一次
-class Solution {
-public:
-    vector<vector<int>> ans;
-    vector<int> track;
-    bool* used;
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        used = new bool[nums.size()]();
-        sort(nums.begin(), nums.end());
-        backtrack(nums);
-
-        return ans;
-    }
-    void backtrack(vector<int>& nums){
-        //確認停止條件
-        if(track.size() == nums.size()){
-            ans.push_back(track);
-            return;
-        }
-
-        for(int i = 0; i < nums.size(); i++){
-            if(used[i]) continue;
-
-            if(i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
-
-            track.push_back(nums[i]);
-            used[i] = true;
-            backtrack(nums);
-            track.pop_back();
-            used[i] = false;
-        }
-    }
-};
 
 /*
 排列問題 套用模板 需use數組確認是否加入到track數組中了
 此題有重複項 除了需要sort 還需要特殊條件去重（因為是排列題注重順序）：如果重複了 他的上一項跟他重複的還沒用過 你就不准用
 */
 
-//la大的
+//la
 class Solution {
 public:
     // 保存结果
