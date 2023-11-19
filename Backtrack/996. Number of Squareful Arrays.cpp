9e390ad6851425e6603c8c754dcc2e92934db40b
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-11-19 21:11:59 +0800

    Update 996. Number of Squareful Arrays.cpp

--- Backtrack/996. Number of Squareful Arrays.cpp
+++ Backtrack/996. Number of Squareful Arrays.cpp
@@ -2,7 +2,7 @@
 996. Number of Squareful Arrays
 */
 
-// TC:O(2^n) SC:O(2^n)
+// TC:O(2^n) SC:O(2^n) int last 寫法
 class Solution {
     int n;
     vector<vector<int>> graph;
@@ -45,6 +45,53 @@ public:
             visited[i] = 1;
             DFS(nums,i,count+1);
             visited[i] = 0;
+        }
+    }
+};
+
+// 另種寫法
+class Solution {
+    int n;
+    vector<vector<int>> graph;
+    int res = 0;
+    bool visited[12];
+public:
+    int numSquarefulPerms(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        n = nums.size();
+        graph.resize(n);
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                if(i == j) continue;
+                if(sqrt(nums[i]+nums[j]) == (int)sqrt(nums[i]+nums[j])){
+                    graph[i].push_back(j);
+                }
+            }
+        }
+        for(int i = 0; i<n; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            visited[i] = 1;
+            DFS(nums,i,1);
+            visited[i] = 0;
+        }
+
+        return res;
+    }
+    void DFS(vector<int>& nums,int cur, int count){
+        if(count == n){
+            res+=1;
+            return;
+        }
+        
+        int last = -1;
+        for(int i = 0; i<graph[cur].size(); i++){
+            if(visited[graph[cur][i]]) continue;
+            if(i && visited[graph[cur][i-1]] == 0 && nums[graph[cur][i-1]] == nums[graph[cur][i]]) continue;
+            
+            visited[graph[cur][i]] = 1;
+            DFS(nums,graph[cur][i],count+1);
+            visited[graph[cur][i]] = 0;
         }
     }
 };
