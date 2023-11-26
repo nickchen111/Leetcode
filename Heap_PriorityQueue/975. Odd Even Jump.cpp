9a0b2a363ad888d3e00f765cc4aaf8755f740485
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-11-26 23:35:36 +0800

    Update 975. Odd Even Jump.cpp

--- Heap_PriorityQueue/975. Odd Even Jump.cpp
+++ Heap_PriorityQueue/975. Odd Even Jump.cpp
@@ -43,6 +43,46 @@ public:
     }
 };
 
+//單純用map更快
+class Solution {
+public:
+    int oddEvenJumps(vector<int>& arr) {
+        int n = arr.size();
+        vector<bool> dp_odd(n);
+        vector<bool> dp_even(n);
+        dp_odd[n-1] = true;
+        dp_even[n-1] = true;
+        map<int,int> map;//val->idx
+        map[arr.back()] = n-1;
+        
+        for(int i = n-2; i>=0; i--){
+            //假設這一步是奇數
+            auto iter1 = map.lower_bound(arr[i]);
+            if(iter1 != map.end()){
+                int j = iter1->second;
+                dp_odd[i] = dp_even[j];
+            }
+            //假設是偶數步
+            auto iter2 = map.upper_bound(arr[i]);
+            if(iter2 != map.begin()){
+                iter2 = prev(iter2,1);
+                int j = iter2->second;
+                dp_even[i] = dp_odd[j];
+            }
+
+            map[arr[i]] = i;//遇到一樣的數字總是只會記得最靠前的
+        }
+
+        int count = 0;
+        for(int i = 0; i<n; i++){
+            if(dp_odd[i] == 1) count++;
+        }
+
+        return count;
+    }
+};
+
+
 /*
 此題的規則是如果某個數字是走第奇數次的步數 那她就要找下一個比他大於等於的數字 如果這種的有重複 那麼優先選最前面的
 走偶數次的話 就要找跟他最靠近的小於等於的數字 重複一樣找最靠前的
