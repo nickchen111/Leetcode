2308c5f212c327e52adc1afd583f0eef6e41e815
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-12-11 22:39:55 +0800

    Update Sub_Rect_Sum_2D.cpp

--- Template/Sub_Rect_Sum_2D.cpp
+++ Template/Sub_Rect_Sum_2D.cpp
@@ -2,31 +2,28 @@
 Sub_Rect_Sum_2D
 */
 
-class RegionSum {
-    vector<vector<int>>presum;
+class RegionSum{
+    vector<vector<int>> presum;
 public:
-    RegionSum(vector<vector<int>>& A)
-    {
-        int m = A.size();
-        int n = A[0].size();
-        presum.resize(m);
-        for (int i=0; i<m; i++)
-            presum[i].resize(n);
-        for (int i=0; i<m; i++)
-            for (int j=0; j<n; j++)
-            {
-                int a = i==0?0:presum[i-1][j];
-                int b = j==0?0:presum[i][j-1];
-                int c = (i==0||j==0)?0:presum[i-1][j-1];
-                presum[i][j] = a + b - c + A[i][j];
+    RegionSum(vector<vector<int>>& A){
+        int m = A.size(), n = A[0].size();
+        presum.resize(m+1);
+        for(int i = 0; i<=m; i++){
+            presum[i].resize(n+1);
+        }
+
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<=n; j++){
+                int a = presum[i-1][j];
+                int b = presum[i][j-1];
+                int c = presum[i-1][j-1];
+                presum[i][j] = a+b-c+A[i-1][j-1];
             }
+        }
     }
-    int query(int i, int j, int x, int y)
-    {
-        int a = j==0?0:presum[x][j-1];
-        int b = i==0?0:presum[i-1][y];
-        int c = (i==0||j==0)?0:presum[i-1][j-1];
-        int area = presum[x][y] - a - b + c;
-        return area;
+
+    int query(int i, int j, int x, int y){
+        return presum[x+1][y+1]-presum[x+1][j]-presum[i][y+1]+presum[i][j];
     }
+
 };
