51fca4543ca470501d65d5cbb84e25f7e76d23cd
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-12-12 14:52:15 +0800

    Update 1464. Maximum Product of Two Elements in an Array.cpp

--- Array/1464. Maximum Product of Two Elements in an Array.cpp
+++ Array/1464. Maximum Product of Two Elements in an Array.cpp
@@ -2,6 +2,27 @@
 1464. Maximum Product of Two Elements in an Array
 */
 
+// TC:O(n) SC:O(1)
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int first_element = nums[0];
+        int second_element = 0;
+
+        for(int i = 1; i < n; i++){
+            if(nums[i] >= first_element){
+                second_element = first_element;
+                first_element = nums[i];
+            }
+            else if(nums[i] > second_element){
+                second_element = nums[i];
+            }
+        }
+
+        return (first_element - 1) * (second_element - 1);
+    }
+};
 
 // TC:O(nlgn) SC:O(1)
 class Solution {
