64ee2cdec46d239e274fb0f199c8f72f4a7ac4db
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-12-14 18:30:18 +0800

    Create 2365. Task Scheduler II.cpp

---
+++ Greedy/2365. Task Scheduler II.cpp
@@ -0,0 +1,28 @@
+/*
+2365. Task Scheduler II
+*/
+
+// TC:O(n) SC:O(n)
+class Solution {
+    using LL = long long;
+public:
+    long long taskSchedulerII(vector<int>& tasks, int space) {
+        unordered_map<int, LL> time;
+        int n = tasks.size();
+        LL cur = 0;
+        for(int i = 0; i < n; i++){
+            if(time.find(tasks[i]) != time.end()){
+                cur = max(cur, time[tasks[i]]);
+            }
+            time[tasks[i]] = cur + space + 1;//此任務下次可以做的時間
+            cur++;
+        }
+
+        return cur;
+    }
+};
+
+/*
+此題規定任務只能按照順序做 不能自己排序 所以每過一個任務就給他冷凍期 表明他下次可以做的時間
+此題跟 Task Scheduler I 比起來簡單很多
+*/
