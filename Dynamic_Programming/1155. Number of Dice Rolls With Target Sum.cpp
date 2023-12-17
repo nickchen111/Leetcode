fe08b36a29a7e9955870f7b615b76e0215c35b6f
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-12-17 22:53:26 +0800

    Create 1155. Number of Dice Rolls With Target Sum.cpp

---
+++ Dynamic_Programming/1155. Number of Dice Rolls With Target Sum.cpp
@@ -0,0 +1,62 @@
+/*
+1155. Number of Dice Rolls With Target Sum
+*/
+
+// top down  TC:O(n*k*target) SC:O(k*target)
+class Solution {
+    long M = 1e9+7;
+    int dp[31][1001];
+public:
+    int numRollsToTarget(int n, int k, int target) {
+        for(int i = 0; i < 31; i++){
+            for(int j = 0; j < 1000; j++){
+                dp[i][j] = -1;
+            }
+        }
+        long long res = solve(n,k,target)%M;
+
+        return res;
+        
+    }
+    long long solve(int n, int k, int target){
+        if(n == 0 && target == 0) return 1;
+        if(n < 0 || target < 0) return 0;
+        if(dp[n][target] != -1) return dp[n][target];
+        long long chance = 0;
+        for(int i = 1; i <= k; i++){
+            chance = (chance + solve(n-1, k, target - i)) % M;
+        }
+
+        dp[n][target] = chance;
+
+        return chance;
+    }
+};
+
+// bottom up TC:O(n*k*target) SC:O(k*target)
+class Solution {
+    long M = 1e9+7;
+public:
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>> dp(n+1, vector<int>(target+1));
+        dp[0][0] = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= k; j++){
+                for(int ta = j; ta <= target; ta++){
+                    dp[i][ta] = (dp[i][ta] + dp[i-1][ta-j]) % M;
+                }
+            }
+        }
+
+        return dp[n][target];
+    }
+};
+
+
+/*
+投n個摋子with k 面 (1-k)種選擇 問有多少種組合可以累加出target
+backtrack ? 30^30次方太大了
+int dp[31][1001];
+也就會是經過30輪時 可以達到1000的總數目為多少
+DP -> 要達到target 1000 前面有可能的狀況是 target - k 要達到這個就是 target - 2*k可能
+*/
