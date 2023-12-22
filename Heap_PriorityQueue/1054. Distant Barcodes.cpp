cd62d22b038cd5c4688c571a38ec3854208757bc
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-12-22 17:28:10 +0800

    Update 1054. Distant Barcodes.cpp

--- Heap_PriorityQueue/1054. Distant Barcodes.cpp
+++ Heap_PriorityQueue/1054. Distant Barcodes.cpp
@@ -39,6 +39,44 @@ public:
     }
 };
 
+// 另一種寫法 general一點
+class Solution {
+    using PII = pair<int, int>;
+public:
+    vector<int> rearrangeBarcodes(vector<int>& barcodes) {
+        int n = barcodes.size();
+        unordered_map<int,int> map; // val -> freq;
+        for(int i = 0; i < n; i++){
+            map[barcodes[i]] += 1;
+        }
+
+        priority_queue<PII, vector<PII>, less<PII>> pq;
+        for(auto &[val, freq] : map){
+            pq.push({freq,val});
+        }
+
+        vector<int> res;
+        while(!pq.empty()){
+            if(pq.size() == 1 && pq.top().first > 1) return {};//失敗條件
+            int len = pq.size();
+            vector<PII> temp;
+            for(int i = 0; i < min(2, len); i++){
+                auto [freq, val] = pq.top();
+                pq.pop();
+                res.push_back(val);
+                freq -= 1;
+                if(freq > 0) temp.push_back({freq,val});
+            }
+            for(int i = 0; i < temp.size(); i++){
+                pq.push({temp[i].first, temp[i].second});
+            }
+        }
+
+        return res;
+    }
+};
+
+
 /*
 希望相鄰數字不重複並保證一定有此答案將排序順序返回
 跟之前的要求你安排順序題目一樣明顯用PQ + Greedy
