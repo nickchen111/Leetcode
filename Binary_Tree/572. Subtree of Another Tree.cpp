0e41de0d4964f180702b68cb8a5d40dfa269c79d
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2023-12-31 02:08:51 +0800

    Update 572. Subtree of Another Tree.cpp

--- Binary_Tree/572. Subtree of Another Tree.cpp
+++ Binary_Tree/572. Subtree of Another Tree.cpp
@@ -35,21 +35,21 @@ public:
 //recursion TC:O(m*n) SC:O(max(m,n))
 class Solution {
 public:
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(root == NULL || subRoot == NULL){
-            return root==subRoot;
-        }
-        if(isSameTree(root,subRoot)) return true;
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) { 
+        if(root == NULL) return false;
 
-        return isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
+        if(isSameTree(root, subRoot)) return true;
+
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
 
     bool isSameTree(TreeNode* p, TreeNode* q){
-        if(!p && !q) return true;
-        if(!p || !q) return false;
+        if(p == NULL && q == NULL) return true;
+        if(p == NULL || q == NULL) return false;
+
         if(p->val != q->val) return false;
 
-        return isSameTree(p->left,q->left) && isSameTree(p->right, q->right);
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
 
