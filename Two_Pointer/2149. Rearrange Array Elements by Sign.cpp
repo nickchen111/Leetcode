1273635711030968f7aa17510789fd1ddd1d19a7
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2024-02-14 11:56:22 +0800

    Update 2149. Rearrange Array Elements by Sign.cpp

--- Two_Pointer/2149. Rearrange Array Elements by Sign.cpp
+++ Two_Pointer/2149. Rearrange Array Elements by Sign.cpp
@@ -2,6 +2,45 @@
 2149. Rearrange Array Elements by Sign
 */
 
+//  TC:O(n) SC:O(n)
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> res;
+        bool flag = 0;
+        int left = 0, right = 0;
+        while(left < n && right < n){
+            if(flag == 0){
+                while(left < n && nums[left] < 0){
+                    left++;
+                }
+                if(left < n) res.push_back(nums[left]);
+                left++;
+                flag = 1;
+            }
+            else if(flag == 1){
+                while(right < n && nums[right] > 0){
+                    right++;
+                }
+                if(right < n) res.push_back(nums[right]);
+                right++;
+                flag = 0;
+            }
+        }
+
+        while(right < n){
+            if(nums[right] < 0)res.push_back(nums[right]);
+            right++;
+        }
+
+       
+
+        return res;
+    }
+};
+
 // TC:O(n) SC:O(n)
 class Solution {
 public:
