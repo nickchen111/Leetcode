d9890f538c5d439e8f4b0aa61346df0e16d18b10
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2024-02-26 17:05:15 +0800

    Update 1723. Find Minimum Time to Finish All Jobs.cpp

--- Backtrack/1723. Find Minimum Time to Finish All Jobs.cpp
+++ Backtrack/1723. Find Minimum Time to Finish All Jobs.cpp
@@ -2,7 +2,7 @@
 1723. Find Minimum Time to Finish All Jobs
 */
 
-// TC:O(k*2^n) SC:(2^n)
+// Backtrack + Binary Search TC:O(k*2^n) SC:(2^n)
 class Solution {
     int k;
 public:
@@ -43,6 +43,43 @@ public:
         }
 
         return false;
+    }
+};
+
+
+//狀態壓縮DP  TC:O(k*3^n) SC: O(k*3^n) 
+class Solution {
+    int dp[13][4096]; // 4096 -> 2^12
+    int time[4096];
+public:
+    int minimumTimeRequired(vector<int>& jobs, int k) {
+        int n = jobs.size();
+        for(int state = 0; state < (1<<n); state++){
+            int sum = 0;
+            for(int i = 0; i < n; i++){
+                if((state>>i)&1){
+                    sum += jobs[i];
+                }
+            }
+            time[state] = sum;
+        }
+
+        for(int state = 1;  state < (1<<n); state++){
+            dp[0][state] = INT_MAX;
+        }
+        dp[0][0] = 0;
+
+        for(int i = 1; i <= k; i++){
+            
+            for(int state = 0; state < (1<<n); state++){
+                dp[i][state] = INT_MAX;
+                for(int subset = state; subset > 0; subset = (subset-1)&state){
+                   dp[i][state] = min(dp[i][state], max(dp[i-1][state-subset], time[subset]));
+                }
+            }
+        }
+
+        return dp[k][(1<<n)-1];
     }
 };
 
