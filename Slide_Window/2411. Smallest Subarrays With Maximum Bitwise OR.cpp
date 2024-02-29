9f200c40317aa0367ef1eac3095e2f1401615d7c
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2024-02-29 21:25:43 +0800

    Update 2411. Smallest Subarrays With Maximum Bitwise OR.cpp

--- Slide_Window/2411. Smallest Subarrays With Maximum Bitwise OR.cpp
+++ Slide_Window/2411. Smallest Subarrays With Maximum Bitwise OR.cpp
@@ -33,6 +33,71 @@ public:
         return true;
     }
 };
+
+// TC:O(64*n) SC:O(32) 2/29
+class Solution {
+public:
+    vector<int> smallestSubarrays(vector<int>& nums) {
+        int n = nums.size();
+    
+        vector<int> count(32,0);
+        for(int i = 0; i < n; i++){
+            int cur = nums[i];
+            int k = 0;
+            while((cur >> k) !=  0){
+                int now = (cur>>k)&1;
+                if(now) count[k] += 1;
+                k++;
+            }
+        }
+
+        int need = 0;
+        for(int i =0; i < 32; i++){
+            if(count[i] != 0) need += 1;
+        }
+        
+        vector<int> Count(32,0);
+        vector<int> res(n,1);
+        int j = 0;
+        int curNeed = 0;
+        for(int i = 0; i < n; i++){
+            while(j < n && curNeed < need){
+                int k = 0;
+                while((nums[j]>>k) != 0){
+                    int now = (nums[j]>>k)&1;
+                    if(now){
+                        Count[k] += 1;
+                        if(Count[k] == 1) curNeed += 1;
+                    }
+                    k++;
+                }
+                j++;
+            }
+            if(need == 0){
+                res[i] = 1;
+            }
+            else if(curNeed == need){
+                res[i] = (j-i);
+            }
+            int k = 0;
+            int cur = nums[i];
+            while((cur >> k) != 0){
+                int now = (cur>>k)&1;
+                if(now){
+                    count[k] -= 1;
+                    Count[k] -= 1;
+                    if(count[k] == 0) need -= 1;
+                    if(Count[k] == 0) curNeed -= 1;
+                }
+                k++;
+            }
+        }
+
+        return res;
+    }
+};
+
+
 /*
 以每個元素作為左邊界 去找右邊的subarray使他的OR運算盡量大 但是subarray盡量短
 要從最右邊開始做 這樣才不用重複做 時間複雜度才會是線性
