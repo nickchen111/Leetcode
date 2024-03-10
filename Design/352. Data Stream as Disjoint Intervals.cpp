65e97c89050b31466445b06862eb0d3395193352
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2024-03-10 16:04:32 +0800

    Update 352. Data Stream as Disjoint Intervals.cpp

--- Design/352. Data Stream as Disjoint Intervals.cpp
+++ Design/352. Data Stream as Disjoint Intervals.cpp
@@ -2,7 +2,50 @@
 352. Data Stream as Disjoint Intervals
 */
 
-// TC:O(n) SC:O(n)
+// Greedy Interval TC:O(nlgn + m) SC:O(n) m為每次整理過後的區間數量
+class SummaryRanges {
+    using PII = pair<int,int>;
+    vector<PII> arr;
+public:
+    SummaryRanges() {
+        
+    }
+    
+    void addNum(int value) {
+        arr.push_back({value, value});
+    }
+    
+    vector<vector<int>> getIntervals() {
+        if(arr.size() == 0) return {};
+        auto cmp = [](const PII& a, const PII& b){
+            if(a.first != b.first) return a.first < b.first;
+            else return a.second > b.second;
+        };
+
+        sort(arr.begin(), arr.end(), cmp);
+        vector<vector<int>> res;
+
+        
+        int j = 1;
+        for(int i = 0; i < arr.size(); i++){
+            int start = arr[i].first;
+            int curMax = arr[i].second;
+            j = max(j, i+1);
+            while(j < arr.size() && arr[j].first <= curMax+1){
+                curMax = max(curMax, arr[j].second);
+                j++;
+            }
+            res.push_back({start, curMax});
+            i = j-1;
+        }
+
+        return res;
+        
+    }
+};
+
+
+// Heap TC:O(nlgn + n) SC:O(n) n 為 新增數據的多寡
 class SummaryRanges {
     set<int> set;
 public:
