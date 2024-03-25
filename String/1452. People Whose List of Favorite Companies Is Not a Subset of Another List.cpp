7ae9691ab740cc8b8272862e97b63b3d8841e6ba
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2024-03-25 16:08:39 +0800

    Update 1452. People Whose List of Favorite Companies Is Not a Subset of Another List.cpp

--- String/1452. People Whose List of Favorite Companies Is Not a Subset of Another List.cpp
+++ String/1452. People Whose List of Favorite Companies Is Not a Subset of Another List.cpp
@@ -2,6 +2,45 @@
 1452. People Whose List of Favorite Companies Is Not a Subset of Another List
 */
 
+// BitMask 
+
+// 優化資料結構
+class Solution {
+public:
+    vector<int> peopleIndexes(vector<vector<string>>& favoriteCompanies) {
+        int n = favoriteCompanies.size();
+        vector<unordered_set<string>> arr;
+
+        for(int i = 0; i < n; i++){
+            unordered_set<string> set(favoriteCompanies[i].begin(), favoriteCompanies[i].end());
+            arr.push_back(set);
+        }
+
+        vector<int> res;
+        for(int i = 0; i < n; i++){
+            bool flag = 1;
+            for(int j = 0; j < n; j++){
+                if(i == j) continue;
+                bool included = true;
+                for(auto str : arr[i]){
+                    if(arr[j].find(str) == arr[j].end()){
+                        included = false;
+                        break;
+                    }
+                }
+
+                if(included){
+                    flag = 0;
+                    break;
+                }
+            }
+            if(flag) res.push_back(i);
+        }
+
+        return res;
+    }
+};
+
 // TC:O(n^2*m) SC:O(n*m)
 class Solution {
 public:
