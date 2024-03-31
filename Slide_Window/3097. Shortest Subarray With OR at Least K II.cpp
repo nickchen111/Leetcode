a64f8ea459e6aece63a9b7c92a134db96d6900de
Nick Chen <139751688+nickchen111@users.noreply.github.com>
2024-03-31 14:38:26 +0800

    Update 3097. Shortest Subarray With OR at Least K II.cpp

--- Slide_Window/3097. Shortest Subarray With OR at Least K II.cpp
+++ Slide_Window/3097. Shortest Subarray With OR at Least K II.cpp
@@ -2,7 +2,7 @@
 3097. Shortest Subarray With OR at Least K II
 */
 
-// TC:O(n) SC:O(1)
+// 變動長度滑窗 TC:O(n) SC:O(1)
 class Solution {
 public:
     int minimumSubarrayLength(vector<int>& nums, int k) {
@@ -42,3 +42,50 @@ public:
         return len == INT_MAX ? -1 : len;
     }
 };
+
+//次佳解Binary Search + 固定長度滑窗 TC:O(32*n) SC:O(1)
+class Solution {
+public:
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        int left = 1, right = n;
+        while(left < right){
+            int mid = left + (right-left)/2;
+            if(check(mid, nums,k)){
+                right = mid;
+            }
+            else left = mid+1;
+        }
+        if(check(left, nums, k) == false) return -1;
+        return left;
+    }
+    bool check(int len, vector<int>& nums, int k){
+        vector<int> count(31,0);
+        int n = nums.size();
+        for(int i = 0; i < len-1; i++){
+            for(int j = 0; j < 31; j++){
+                count[j] += ((nums[i]>>j)&1);
+            }
+        }
+
+        for(int i = len-1; i < n; i++){
+            for(int j = 0; j < 31; j++){
+                count[j] += ((nums[i]>>j)&1);
+            }
+            int sum = 0;
+            for(int j = 0; j < 31; j++){
+                if(count[j]) sum += (1<<j); 
+            }
+            if(sum >= k) return true;
+
+            for(int j = 0; j < 31; j++){
+                count[j] -= (nums[i-len+1]>>j)&1;
+            }
+        }
+
+        return false;
+    }
+};
+
+
+
