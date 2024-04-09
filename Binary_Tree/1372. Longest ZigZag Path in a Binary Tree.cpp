3c5f803f959bfa2d195bc030f07805a30811f695
Nick Chen <s27206383@gmail.com>
2024-04-09 15:22:22 +0800

    Update 1372. Longest ZigZag Path in a Binary Tree.cpp

--- Binary_Tree/1372. Longest ZigZag Path in a Binary Tree.cpp
+++ Binary_Tree/1372. Longest ZigZag Path in a Binary Tree.cpp
@@ -2,6 +2,35 @@
 1372. Longest ZigZag Path in a Binary Tree
 */
 
+// 4/9
+class Solution {
+    int res = 0;
+public:
+    int longestZigZag(TreeNode* root) {
+        DFS(root, 0, -1);
+
+        return res;
+    }
+    // 0 -> left, 1 -> right
+    void DFS(TreeNode* root, int count, int dir){
+        if(root == NULL) return;
+        res = max(res, count);
+        if(dir == 0){
+            DFS(root->right, count+1, 1);
+            DFS(root->left, 1,0);
+        }
+        else if(dir == 1){
+            DFS(root->left, count+1, 0);
+            DFS(root->right, 1, 1);
+        }
+        else if(dir == -1){
+            DFS(root->left, count+1, 0);
+            DFS(root->right, count+1, 1);
+        }
+    }
+};
+
+
 // TC:O(n) SC:O(n)
 class Solution {
     int res = 0;
