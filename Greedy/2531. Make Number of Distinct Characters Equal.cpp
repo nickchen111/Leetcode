b1bec32bbf1cc66c0a073d7ed033c321417b0cb5
Nick Chen <s27206383@gmail.com>
2024-04-16 17:07:20 +0800

    Update 2531. Make Number of Distinct Characters Equal.cpp

--- Greedy/2531. Make Number of Distinct Characters Equal.cpp
+++ Greedy/2531. Make Number of Distinct Characters Equal.cpp
@@ -2,6 +2,54 @@
 2531. Make Number of Distinct Characters Equal
 */
 
+// 4/16
+class Solution {
+public:
+    bool isItPossible(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        vector<int> count1(26,0);
+        vector<int> count2(26,0);
+        int num1 = 0, num2 = 0;
+        for(auto ch : word1){
+            count1[ch-'a'] += 1;
+            if(count1[ch-'a'] == 1) num1 += 1;
+        }
+        for(auto ch : word2){
+            count2[ch-'a'] += 1;
+            if(count2[ch-'a'] == 1) num2 += 1;
+        }
+
+        if(abs(num1 - num2) > 2) return false;
+
+        for(int i = 0; i < 26; i++){
+            if(count1[i] == 0) continue;
+            for(int j = 0; j < 26; j++){
+                if(count2[j] == 0) continue;
+                int tmp1 = num1;
+                int tmp2 = num2;
+                if(i == j){
+                    if(tmp1 == tmp2) return true;
+                }
+                else {
+                    if(count1[i]-1 == 0) tmp1 -= 1;
+                    if(count2[j]-1 == 0) tmp2 -= 1;
+                    if(count1[j] == 0) tmp1 += 1;
+                    if(count2[i] == 0) tmp2 += 1;
+                    if(tmp1 == tmp2) return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+
+/*
+能不能靠換一個字 讓兩邊的獨特元素數量相等
+*/
+
 // TC:O(26*26 + n+m) SC:O(52)
 class Solution {
 public:
