091e963048edd190e2d1798d82227103a3120ca6
Nick Chen <s27206383@gmail.com>
2024-04-16 16:52:19 +0800

    Update 1238. Circular Permutation in Binary Representation.cpp

--- Backtrack/1238. Circular Permutation in Binary Representation.cpp
+++ Backtrack/1238. Circular Permutation in Binary Representation.cpp
@@ -2,6 +2,62 @@
 1238. Circular Permutation in Binary Representation
 */
 
+
+// Gray code TC:O(n^2 + 2^n) SC:O(2^n)
+class Solution {
+public:
+    vector<int> circularPermutation(int n, int start) {
+        vector<int> arr;
+        arr.push_back(0);
+
+        for(int i = 0; i < n; i++){
+            int len = arr.size();
+            for(int j = len-1; j >= 0; j--){
+                arr.push_back(arr[j]+(1<<i));
+            }
+        }
+
+        int j = 0;
+        while(arr[j] != start){
+            j++;
+        }
+        vector<int> res;
+        for(int i = j; i < arr.size(); i++){
+            res.push_back(arr[i]);
+        }
+        for(int i = 0; i < j; i++){
+            res.push_back(arr[i]);
+        }
+
+        return res;
+    }
+};
+
+/*
+之前backtrack不給過了＠＠
+只能改用數學 gray code
+ex :
+1. 先去對某個長度的數字做鏡像
+2. 然後前半部補0 後半部鏡像的補1 這樣就是格雷馬數了
+00
+01
+11
+10 
+11
+10
+01
+00
+
+000
+001
+011
+010 
+011
+111
+110
+000
+*/
+
 // TC:O(2^n) SC:O(2^n)
 class Solution {
     vector<bool> visited;
