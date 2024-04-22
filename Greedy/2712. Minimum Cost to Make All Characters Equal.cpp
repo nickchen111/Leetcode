18f80c7f9c9b65b8c21ebac5720659ed2b1589de
Nick Chen <s27206383@gmail.com>
2024-04-22 16:25:44 +0800

    Update 2712. Minimum Cost to Make All Characters Equal.cpp

--- Greedy/2712. Minimum Cost to Make All Characters Equal.cpp
+++ Greedy/2712. Minimum Cost to Make All Characters Equal.cpp
@@ -21,6 +21,72 @@ public:
     }
 };
 
+
+// 更直觀一點的解法 
+using LL = long long;
+class Solution {
+public:
+    long long minimumCost(string s) 
+    {
+        LL ret1 = solve(s);
+        for (int i=0; i<s.size(); i++)
+            if (s[i]=='1')
+                s[i]='0';
+            else
+                s[i]='1';
+        LL ret2 = solve(s);
+        return min(ret1,ret2);
+    }
+    
+    LL solve(string s)
+    {
+        int n = s.size();
+        vector<LL>left(n);
+        
+        LL sum = 0;
+        for (int i=0; i<n; i++)
+        {
+            if (s[i]=='0')
+            {
+                left[i] = sum;
+                continue;                
+            }
+            
+            if (i>=1 && s[i-1]=='1')
+                sum = sum+1;
+            else
+                sum += (i+1) + i;
+
+            left[i] = sum;
+            
+        }
+        
+        vector<LL>right(n);
+        
+        sum = 0;
+        for (int i=n-1; i>=0; i--)
+        {
+            if (s[i]=='0')
+            {
+                right[i] = sum;
+                continue;                
+            }
+            
+            if (i+1<n && s[i+1]=='1')
+                sum  = sum+1;
+            else
+                sum += (n-i) + (n-i-1);
+            right[i] = sum;
+        }
+        
+        LL ret = right[0];
+        for (int i=0; i<n-1; i++)
+            ret = min(ret, left[i]+right[i+1]);
+        return ret;
+    }
+};
+
+
 /*
 
 此題給你一個binary string 每一次你都可以讓左段或者右段區間翻轉  翻轉會有翻左段或者右段的cost
