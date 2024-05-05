c242eb7f7c4cc09ec9883e5811f25fa22cee6aeb
Nick Chen <s27206383@gmail.com>
2024-05-05 14:02:24 +0800

    Update and rename Bit_for_排列組合.cpp to Bit_for_排列組合_Gosper'sHack.cpp

--- Template/Bit_for_排列組合_Gosper'sHack.cpp
+++ Template/Bit_for_排列組合_Gosper'sHack.cpp
@@ -1,4 +1,17 @@
+// Gosper's Hack
+// Iterate all the m-bit state where there are k 1-bits.
 
+int state = (1 << k) - 1;            
+while (state < (1 << m))
+{
+    doSomething(state);
+
+    int c = state & - state;
+    int r = state + c;
+    state = (((r ^ state) >> 2) / c) | r;
+}
+
+// 3116題時的程式碼
 int m = nums.size();
 for(int k = 1; k <= m; k++){
     int state = (1<<k) -1;
