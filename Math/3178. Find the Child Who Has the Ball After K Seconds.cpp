99f0c9e225fc8ad4fdbd19a19905a4eb32d843b0
Nick Chen <s27206383@gmail.com>
2024-06-09 14:56:02 +0800

    Update 3178. Find the Child Who Has the Ball After K Seconds.cpp

--- Math/3178. Find the Child Who Has the Ball After K Seconds.cpp
+++ Math/3178. Find the Child Who Has the Ball After K Seconds.cpp
@@ -27,3 +27,24 @@ public:
         
     }
 };
+
+class Solution {
+public:
+    int numberOfChild(int n, int k) {
+        
+        if((k/(n-1))%2) return n-1-(k%(n-1));
+        else return k%(n-1);
+        
+    }
+};
+
+/*
+第k秒是哪個小孩拿著球
+n = 3 
+第一個人為 0 -> 4 -> 8 ....
+k <= 1
+k / (n-1) 會知道他在第幾輪
+%2 就知道是正著還是逆著過程 正個就給他 n - 1 - r 逆著就是r
+如果是正著過去的話 (k/(n-1))%2 == 0  答案r r為餘數
+反過來的話就會是n-1-r 
+*/
