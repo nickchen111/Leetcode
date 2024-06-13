d2672574124711960c0406682d00af45f09b5720
Nick Chen <s27206383@gmail.com>
2024-06-13 13:33:26 +0800

    Create 2037. Minimum Number of Moves to Seat Everyone.cpp

---
+++ Array/2037. Minimum Number of Moves to Seat Everyone.cpp
@@ -0,0 +1,19 @@
+/*
+2037. Minimum Number of Moves to Seat Everyone
+*/
+
+
+// TC:O(nlgn) SC:O(1)
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+        int res = 0;
+        for(int i = 0; i < students.size(); i++){
+            res += abs(students[i] - seats[i]);
+        }
+
+        return res;
+    }
+};
