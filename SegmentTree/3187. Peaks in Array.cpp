f872480ec38f92fb1154dd3518dbb67ba0785e20
Nick Chen <s27206383@gmail.com>
2024-06-17 01:52:28 +0800

    Update 3187. Peaks in Array.cpp

--- SegmentTree/3187. Peaks in Array.cpp
+++ SegmentTree/3187. Peaks in Array.cpp
@@ -2,7 +2,7 @@
 3187. Peaks in Array
 */
 
-// TC:O(n + m log n) SC:O(n)
+// Segment Tree TC:O(n + m log n) SC:O(n)
 using LL = long long;
 class SegTreeNode
 {
@@ -149,6 +149,134 @@ public:
         
     }
 };
+
+// BIT TC:O(n + m log n) SC:O(n)
+using LL = long long;
+class BIT{
+    public:
+    int N;    
+    vector<long long>bitArr; // Note: all arrays are 1-index
+    long long M = 1e9+7;
+
+    void init(int N)
+    {
+        this->N = N;
+        bitArr.resize(N+1,0);    
+    }
+
+    // increase nums[i] by delta
+    void updateDelta(int i, long long delta) {
+        int idx = i;
+        while (idx <= N)
+        {
+            bitArr[idx]+=delta;
+            // bitArr[idx] %= M;
+            idx+=idx&(-idx);
+        }
+    }
+
+    // sum of a range nums[1:j] inclusively
+    long long queryPreSum(int idx){
+        long long result = 0;
+        while (idx){
+            result += bitArr[idx];
+            // result %= M;
+            idx-=idx&(-idx);
+        }
+        return result;
+    }
+
+    // sum of a range nums[i:j] inclusively
+    long long sumRange(int i, int j) {    
+        return queryPreSum(j)-queryPreSum(i-1);
+    } 
+};
+
+class Solution {
+public:
+    vector<int> countOfPeaks(vector<int>& nums, vector<vector<int>>& queries) 
+    {
+        int n = nums.size();
+        vector<int>vals(n, 0);
+        BIT bit;
+        bit.init(n+10);
+        for (int i=1; i<n-1; i++)
+        {
+            if (nums[i]>nums[i-1] && nums[i]>nums[i+1])
+                vals[i] = 1;
+        }
+        
+        
+        for(int i = 1; i < vals.size(); i++){
+            if(vals[i]) bit.updateDelta(i+1, 1);
+        }
+        
+        vector<int>rets;
+        for (auto query: queries)
+        {
+            if (query[0]==1)
+            {
+                int a = query[1], b = query[2];
+                
+                if (a+2 <= b)
+                    rets.push_back(bit.sumRange(a+2, b));
+                else
+                    rets.push_back(0);
+            }
+                
+            else
+            {
+                int i = query[1];
+                nums[i] = query[2];
+                
+                if (i>=1 && i<n-1)
+                {
+                    int j = i;
+                    int v = (nums[j]>nums[j-1] && nums[j]>nums[j+1]);
+                    if(vals[j] == 1 && v == 0) {
+                        bit.updateDelta(j+1,-1);   
+                        vals[j] = 0;
+                    }
+                    else if(vals[j] == 0 && v == 1){
+                        bit.updateDelta(j+1,1);   
+                        vals[j] = 1;
+                    }
+                }
+                if (i-1>=1 && i-1<n-1)
+                {
+                    int j = i-1;
+                    int v = (nums[j]>nums[j-1] && nums[j]>nums[j+1]);
+                    if(vals[j] == 1 && v == 0) {
+                        bit.updateDelta(j+1,-1);   
+                        vals[j] = 0;
+                    }           
+                    else if(vals[j] == 0 && v == 1){
+                        bit.updateDelta(j+1,1);   
+                        vals[j] = 1;
+                    }
+                }
+                if (i+1>=1 && i+1<n-1)
+                {
+                    int j = i+1;
+                    int v = (nums[j]>nums[j-1] && nums[j]>nums[j+1]);
+                    if(vals[j] == 1 && v == 0) {
+                        bit.updateDelta(j+1,-1);   
+                        vals[j] = 0;
+                    }         
+                    else if(vals[j] == 0 && v == 1){
+                        bit.updateDelta(j+1,1);   
+                        vals[j] = 1;
+                    }
+                }
+                
+            }                
+        }
+        
+        return rets;
+        
+    }
+};
+
 
 /*
 問說按照query順序 每當queries[i][0] = 1 時判斷出他給出的區間有多少個peak
