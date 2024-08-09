aaa21e197010e0f0c68f2970424a4ef5948f9cfc
Nick Chen <s27206383@gmail.com>
2024-08-10 02:18:46 +0800

    add re-root DP method

--- Graph/310. Minimum Height Trees.cpp
+++ Graph/310. Minimum Height Trees.cpp
@@ -2,6 +2,7 @@
 310. Minimum Height Trees
 */
 
+// Topological Sort TC:O(n) SC:O(n)
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
@@ -54,6 +55,73 @@ public:
             }
         }
         return ans;
+    }
+};
+
+// Re-root DP TC:O(n) SC:O(n)
+class Solution {
+    vector<int> res;
+    vector<vector<int>> next;
+    vector<tuple<int,int,int>> nodes;
+    int maxDepth = INT_MAX;
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        next.resize(n);
+        nodes.resize(n);
+        if(n == 1) return {0};
+        if(n == 2) return {0,1};
+        
+        for(auto edge : edges){
+            next[edge[0]].push_back(edge[1]);
+            next[edge[1]].push_back(edge[0]);
+        }
+
+        DFS(0, -1);
+        
+        reRoot(0, -1, 0);
+
+        return res;
+        
+    }
+    int DFS(int cur, int pa){
+        int md = 0;
+        int md2 = 0;
+        int subroot = -1;
+        for(auto nxt : next[cur]){
+            if(nxt == pa) continue;
+            int nxtd = DFS(nxt, cur) + 1;
+            if(nxtd > md){
+                md2 = md;
+                md = nxtd;
+                subroot = nxt;
+            }
+            else if(nxtd > md2){
+                md2 = nxtd;
+            }
+        }
+
+        nodes[cur] = {md, md2, subroot};
+
+        return md;
+    }
+    void reRoot(int cur, int pa, int from_up){
+        auto &[max_d, max_d2, my] = nodes[cur];
+        int curDepth = max(max_d, from_up);
+        if(curDepth < maxDepth){
+            maxDepth = curDepth;
+            res.clear();
+            res.push_back(cur);
+        }
+        else if(curDepth== maxDepth){
+            res.push_back(cur);
+        }
+
+        for(auto nxt : next[cur]){
+            if(pa == nxt) {
+                continue;
+            }
+            reRoot(nxt, cur, max(from_up, (nxt == my ? max_d2:max_d)) + 1);
+        }
     }
 };
 
