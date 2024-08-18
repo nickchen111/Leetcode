fb307401675a46b3427032101ff105af43aa204b
Nick Chen <s27206383@gmail.com>
2024-08-18 13:04:29 +0800

    Update 3256. Maximum Value Sum by Placing Three Rooks I.cpp

--- Dynamic_Programming/3256. Maximum Value Sum by Placing Three Rooks I.cpp
+++ Dynamic_Programming/3256. Maximum Value Sum by Placing Three Rooks I.cpp
@@ -4,13 +4,13 @@
 
 // 最佳解 
 
-// 次佳解 TC:O(m*m*9) SC:O(n^3)
+// 次佳解 TC:O(m*m*9) SC:O(n^2)
 class Solution {
     using LL = long long;
     int m, n;
-    LL res = LLONG_MIN;
+    LL res = LLONG_MIN/2; 
     vector<vector<int>> board;
-    LL memo[501][501][501];
+    LL memo[101][101];
     vector<vector<pair<LL, int>>> arr;
     vector<bool> visited;
 public:
@@ -20,11 +20,7 @@ public:
         n = board[0].size();
         for(int i = 0; i <= n; i++){
             for(int j = 0; j <= n; j++){
-                for(int k = 0; k <= n; k++){
-                    
-                    memo[i][j][k] = LLONG_MIN/2;
-                    
-                }
+                memo[i][j] = LLONG_MIN/2;
             }
         }
         
@@ -53,7 +49,7 @@ public:
             for(int j = 0; j < 3; j++){
                 int col = arr[i][j].second; LL val = arr[i][j].first;
                 visited[col] = 1;
-                DFS(i+1, 1, col+1, 0, 0, val);
+                DFS(i+1, 1, col+1, 0, val);
                 visited[col] = 0;
             }
         }
@@ -61,20 +57,20 @@ public:
         
         return res;
     }
-    void DFS(int row, int count, int col1, int col2, int col3, LL sum){
+    void DFS(int row, int count, int col1, int col2, LL sum){
         if(row == m && count < 3) return;
-        
-        
-        if(memo[col1][col2][col3][count] > sum){
-            return;
-        }
-        
-        memo[col1][col2][col3][count] = sum;
         
         if(count == 3){
             res = max(res, sum);
             return;
         }
+
+        if(memo[col1][col2] > sum){
+            return;
+        }
+        
+        memo[col1][col2] = sum;
+    
         
         
         for(int j = 0; j < 3; j++){
@@ -82,13 +78,23 @@ public:
             if(visited[col]) continue;
             visited[col] = 1;
             if(count == 1){
-                DFS(row+1, count+1, col1, col+1, col3, sum + val);
+                DFS(row+1, count+1, col1, col+1,sum + val);
             }
             else if(count == 2){
-                DFS(row+1, count+1, col1,col2, col+1, sum + val);
+                DFS(row+1, count+1, col1,col2, sum + val);
             }
             visited[col] = 0;
         }    
-        DFS(row+1, count, col1, col2, col3, sum);
+        DFS(row+1, count, col1, col2, sum);
     }
 };
+
+/*
+同一個row col 只能有個rook 要放三個rooks
+最多得分是多少?
+100 * 2, 三個
+
+[[95,-90,-62,-94],[62,59,-21,46],[23,24,19,9],[-79,-12,91,16]]
+[[82,13,51],[-43,-96,60],[-25,70,-79],[86,37,-97],[-87,87,-87],[-67,73,6],[-80,-65,-72]]
+[[-12,70,-48,-74],[-71,-75,63,-67],[-46,81,4,-86],[29,-51,-51,16]]
+*/
