821f654a3c657dde9c79eea5e8649f4adea07c38
Nick Chen <s27206383@gmail.com>
2024-10-13 14:22:49 +0800

    feat : quick select make time complexity to O(n)

--- Binary_Tree/3319. K-th Largest Perfect Subtree Size in Binary Tree.cpp
+++ Binary_Tree/3319. K-th Largest Perfect Subtree Size in Binary Tree.cpp
@@ -15,6 +15,35 @@
  */
 
 
+// TC:O(n) SC:O(n) quick select 去找第k大的數字 完全是線性了
+class Solution {
+    vector<int> arr;
+    static bool greater(int a, int b) {
+        return a > b;
+    }
+public:
+    int kthLargestPerfectSubtree(TreeNode* root, int k) {
+        DFS(root);
+        
+        if(k-1 < arr.size()) {
+            std::nth_element(arr.begin(), arr.begin() + k - 1, arr.end(), greater);
+            return (1 << arr[k-1]) - 1;
+        }
+        else return -1;
+    }
+
+    int DFS(TreeNode* root) {
+        if(root == nullptr) return 0;
+        int left = DFS(root->left); // subtree, nodeNumber
+        int right = DFS(root->right);
+        if(left == -1 || right == -1) return -1;
+        if(left != right) return -1;
+
+        arr.push_back(left + 1);
+        return left + 1;
+    }
+};
+
 
 // TC:O(n + mlgm) m為是完全二叉樹的節點數量 SC:O(n + m)
 class Solution {
