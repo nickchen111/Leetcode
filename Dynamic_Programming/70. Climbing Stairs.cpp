67ef4a7efb2b31a967188cc8ea2d6e6b5ced6bc4
Nick Chen <s27206383@gmail.com>
2024-10-28 01:52:25 +0800

    Update 70. Climbing Stairs.cpp

--- Dynamic_Programming/70. Climbing Stairs.cpp
+++ Dynamic_Programming/70. Climbing Stairs.cpp
@@ -2,6 +2,49 @@
 70. Climbing Stairs
 */
 
+// 矩陣快速冪 TC:O(lgn) SC:O(2*4)
+class Solution {
+    using LL = long long;
+public:
+    int climbStairs(int n) {
+        if(n == 1) return 1;
+        vector<vector<LL>> arr = {{1, 1}}; // 第一項，第零項
+        vector<vector<LL>> m = {{1, 1}, {1, 0}};
+
+        return multiply(arr, pow(m, n-1))[0][0];
+    }
+    vector<vector<LL>> multiply(const vector<vector<LL>>& a, const vector<vector<LL>>& b) {
+        int n = a.size();
+        int m = b[0].size();
+        int k = a[0].size();
+        vector<vector<LL>> res(n, vector<LL>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int c = 0; c < k; c++) { 
+                    res[i][j] = (res[i][j] + (LL)a[i][c] * b[c][j]);
+                }
+            }
+        }
+        return res;
+    }
+
+    vector<vector<LL>> pow(vector<vector<LL>>& m, int p) {
+        int n = m.size();
+        vector<vector<LL>> res(n, vector<LL>(n));
+        for (int i = 0; i < n; i++) {
+            res[i][i] = 1; // 單位矩陣
+        }
+
+        for(; p != 0; p >>= 1) {
+            if(p & 1) {
+                res = multiply(res, m);
+            }
+            m = multiply(m, m);
+        }
+        return res;
+    }
+};
+
 //iterative tc: O(n) sc: O(n) 可在優化
 class Solution {
     //iterative 
