253de7f78203a92df8ee87071046fe0e2ae04d45
Nick Chen <s27206383@gmail.com>
2024-10-29 01:23:05 +0800

    Update 1220. Count Vowels Permutation.cpp

--- Dynamic_Programming/1220. Count Vowels Permutation.cpp
+++ Dynamic_Programming/1220. Count Vowels Permutation.cpp
@@ -2,6 +2,54 @@
 1220. Count Vowels Permutation
 */
 
+// 矩陣快速冪 TC:O(lgn) SC:O(1)
+class Solution {
+    using LL = long long;
+    LL MOD = 1e9+7;
+public:
+    int countVowelPermutation(int n) {
+        vector<vector<LL>> start = {{1, 1, 1, 1, 1}};
+        vector<vector<LL>> base = {{0,1,0,0,0}, {1,0,1,0,0}, {1,1,0,1,1}, {0,0,1,0,1}, {1,0,0,0,0}};
+        vector<vector<LL>> a = multiply(start, pow(base, n-1));
+        int res = 0;
+        for(LL &x : a[0]) {
+            res = (res + x) % MOD;
+        }
+
+        return res;
+    }
+    vector<vector<LL>> multiply(const vector<vector<LL>>& a, const vector<vector<LL>>& b) {
+        int n = a.size();
+        int m = b[0].size();
+        int k = a[0].size();
+        vector<vector<LL>> res(n, vector<LL>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int c = 0; c < k; c++) { 
+                    res[i][j] = (res[i][j] + (LL)a[i][c] * b[c][j]) % MOD;
+                }
+            }
+        }
+        return res;
+    }
+
+    vector<vector<LL>> pow(vector<vector<LL>>& m, int p) {
+        int n = m.size();
+        vector<vector<LL>> res(n, vector<LL>(n));
+        for (int i = 0; i < n; i++) {
+            res[i][i] = 1; // 單位矩陣
+        }
+
+        for(; p != 0; p >>= 1) {
+            if(p & 1) {
+                res = multiply(res, m);
+            }
+            m = multiply(m, m);
+        }
+        return res;
+    }
+};
+
 //TC:O(n) SC:O(5*n)
 class Solution {
 public:
