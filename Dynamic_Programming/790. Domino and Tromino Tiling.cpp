06b2bd9b7fdccc953885576c6e242065d2fe6642
Nick Chen <s27206383@gmail.com>
2024-10-28 22:20:59 +0800

    Update 790. Domino and Tromino Tiling.cpp

--- Dynamic_Programming/790. Domino and Tromino Tiling.cpp
+++ Dynamic_Programming/790. Domino and Tromino Tiling.cpp
@@ -2,6 +2,56 @@
 790. Domino and Tromino Tiling
 */
 
+// 矩陣快速冪: TC:O(lgn) SC:O(1)
+class Solution {
+    using LL = long long;
+    LL MOD = 1e9 + 7;
+public:
+    int numTilings(int n) {
+        return f2(n-1);
+    }
+    int f2(int n) {
+        if(n == 0) return 1;
+        if(n == 1) return 2;
+        if(n == 2) return 5;
+
+        vector<vector<LL>> arr = {{5,2,1}};
+        vector<vector<LL>> m = {{2,1,0}, {0,0,1},{1,0,0}};
+
+        return multiply(arr, pow(m, n-2))[0][0];
+    }
+    vector<vector<LL>> multiply(const vector<vector<LL>>& a, const vector<vector<LL>>& b) {
+        int n = a.size();
+        int m = b[0].size();
+        int k = a[0].size();
+        vector<vector<LL>> res(n, vector<LL>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int c = 0; c < k; c++) { 
+                    res[i][j] = (res[i][j] + (LL)a[i][c] * b[c][j]) % MOD;
+                }
+            }
+        }
+        return res;
+    }
+
+    vector<vector<LL>> pow(vector<vector<LL>>& m, int p) {
+        int n = m.size();
+        vector<vector<LL>> res(n, vector<LL>(n));
+        for (int i = 0; i < n; i++) {
+            res[i][i] = 1; // 單位矩陣
+        }
+
+        for(; p != 0; p >>= 1) {
+            if(p & 1) {
+                res = multiply(res, m);
+            }
+            m = multiply(m, m);
+        }
+        return res;
+    }
+};
+
 // TC:O(n) SC:O(n)
 class Solution {
 public:
