19a4ff1edcdc3b2f05ee5930f3302c34220e67a7
Nick Chen <s27206383@gmail.com>
2024-10-28 22:13:09 +0800

    Create 矩陣快速冪.cpp

---
+++ Template/矩陣快速冪.cpp
@@ -0,0 +1,31 @@
+using LL = long long;
+vector<vector<LL>> multiply(const vector<vector<LL>>& a, const vector<vector<LL>>& b) {
+    int n = a.size();
+    int m = b[0].size();
+    int k = a[0].size();
+    vector<vector<LL>> res(n, vector<LL>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            for (int c = 0; c < k; c++) { 
+                res[i][j] = (res[i][j] + (LL)a[i][c] * b[c][j]) % MOD;
+            }
+        }
+    }
+    return res;
+}
+
+vector<vector<LL>> pow(vector<vector<LL>>& m, int p) {
+    int n = m.size();
+    vector<vector<LL>> res(n, vector<LL>(n));
+    for (int i = 0; i < n; i++) {
+        res[i][i] = 1; // 單位矩陣
+    }
+
+    for(; p != 0; p >>= 1) {
+        if(p & 1) {
+            res = multiply(res, m);
+        }
+        m = multiply(m, m);
+    }
+    return res;
+}
