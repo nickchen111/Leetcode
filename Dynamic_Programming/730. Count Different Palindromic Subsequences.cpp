81371fdbe1d8dbf88a397cb0c6e70e9732cfbbb3
Nick Chen <s27206383@gmail.com>
2024-11-03 02:05:43 +0800

    Update 730. Count Different Palindromic Subsequences.cpp

--- Dynamic_Programming/730. Count Different Palindromic Subsequences.cpp
+++ Dynamic_Programming/730. Count Different Palindromic Subsequences.cpp
@@ -2,6 +2,55 @@
 730. Count Different Palindromic Subsequences
 */
 
+// 先對出現位置做預處理 TC:O(26*n + n^2) SC:O(n^2)
+class Solution {
+    using LL = long long;
+    LL MOD = 1e9 + 7;
+public:
+    int countPalindromicSubsequences(string s) {
+        int n = s.size(); 
+        vector<vector<LL>> dp(n, vector<LL>(n));
+        vector<int> left(n,-1);
+        vector<int> right(n,n);
+        vector<int> record(26,-1);
+        for(int i = 0; i < n; i++) {
+            left[i] = record[s[i]-'a'];
+            record[s[i]-'a'] = i;
+        }
+        vector<int> record2(26,n);
+        for(int i = n-1; i >= 0; i--) {
+            right[i] = record2[s[i]-'a'];
+            record2[s[i]-'a'] = i;
+        }
+
+        for(int i = n-1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for(int j = i+1; j < n; j++) {
+                if(s[i] != s[j]) {
+                    dp[i][j] = (dp[i+1][j] + dp[i][j-1] - dp[i+1][j-1] + MOD) % MOD;
+                }
+                else {
+                    int l = right[i];
+                    int r = left[j];
+                    if(l > r) {
+                        dp[i][j] = (dp[i+1][j-1] * 2 + 2) % MOD; 
+                    }
+                    else if(l == r) {
+                        dp[i][j] = (dp[i+1][j-1] * 2 + 1) % MOD; 
+                    }
+                    else {
+                        dp[i][j] = (dp[i+1][j-1] * 2 - dp[l+1][r-1] + MOD) % MOD;
+                    }
+                }
+            }
+        }
+
+        return dp[0][n-1];
+
+    }
+};
+
+
 // TC:O(26*n^2) SC:O(26 + n^2)
 class Solution {
     using LL = long long;
