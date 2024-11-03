feaf6304fa9cbfa6793c08cc32a5c25508dae2dd
Nick Chen <s27206383@gmail.com>
2024-11-03 13:50:21 +0800

    Update 64. Minimum Path Sum.cpp

--- Dynamic_Programming/64. Minimum Path Sum.cpp
+++ Dynamic_Programming/64. Minimum Path Sum.cpp
@@ -52,22 +52,20 @@ public:
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size(); int n = grid[0].size();
-        vector<int> dp(n, INT_MAX);
-        //base case
-        dp[0] =grid[0][0];
-        for(int i = 1; i < n; i++){
-            dp[i] = dp[i-1]+grid[0][i];
-        }
+        int m = grid.size(); 
+        int n = grid[0].size();
+        vector<int> dp(n, INT_MAX/2);
+        // base case
+        dp[0] = grid[0][0];
+        for(int j = 1; j < n; j++) dp[j] = dp[j-1] + grid[0][j];
 
-        for(int i = 1; i <m; i++){
-            for(int j =0; j<n; j++){
-                dp[j] = dp[j] + grid[i][j];
-                if(j-1 >= 0)
-                    dp[j] = min(dp[j],dp[j-1]+grid[i][j]);
+        for(int i = 1; i < m; i++) {
+            dp[0] = dp[0] + grid[i][0];
+            for(int j = 1; j < n; j++) {
+                dp[j] = min(dp[j], dp[j-1]) + grid[i][j];
             }
         }
-
+        
         return dp[n-1];
     }
 };
