2e9f6cd45d6f19f6f50d52b689649c12ec14f41b
Nick Chen <s27206383@gmail.com>
2024-11-05 15:12:09 +0800

    Update 2914. Minimum Number of Changes to Make Binary String Beautiful.cpp

--- String/2914. Minimum Number of Changes to Make Binary String Beautiful.cpp
+++ String/2914. Minimum Number of Changes to Make Binary String Beautiful.cpp
@@ -2,6 +2,23 @@
 2914. Minimum Number of Changes to Make Binary String Beautiful
 */
 
+// TC:O(n/2) SC:O(1)
+class Solution {
+public:
+    int minChanges(string s) {
+        int n = s.size();
+        int res = 0;
+        for(int i = 0; i < n; i+=2) {
+            if(s[i] != s[i+1]) {
+                res += 1;
+            }
+        }
+
+        return res;
+    }
+};
+
+
 // TC:(n) SC:O(1)
 class Solution {
 public:
