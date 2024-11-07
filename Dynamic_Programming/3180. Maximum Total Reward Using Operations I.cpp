32afb35db2185562b1d9236f3bd1cb178155fd72
Nick Chen <s27206383@gmail.com>
2024-11-08 00:02:36 +0800

    Update 3180. Maximum Total Reward Using Operations I.cpp

--- Dynamic_Programming/3180. Maximum Total Reward Using Operations I.cpp
+++ Dynamic_Programming/3180. Maximum Total Reward Using Operations I.cpp
@@ -1,6 +1,66 @@
 /*
 3180. Maximum Total Reward Using Operations I
 */
+// 遞歸 + 遞推
+class Solution {
+public:
+    int maxTotalReward(vector<int>& rewardValues) {
+        int n = rewardValues.size();
+        sort(rewardValues.begin(), rewardValues.end());
+        vector<int> dp(rewardValues[n-1]+1);
+        // TC:O(n * maxVal + nlgn) SC:O(n)
+        for(int i = 0; i < n; i++) {
+            for(int j = rewardValues[i]-1; j >= 0; j--) {
+                j + rewardValues[i] >= rewardValues[n-1] ? dp[rewardValues[n-1]] = max(dp[rewardValues[n-1]], dp[j] + rewardValues[i]) : dp[j + rewardValues[i]] = max(dp[j + rewardValues[i]], dp[j] + rewardValues[i]);
+            }
+        }
+
+        int res = 0;
+        for(int j = 0; j <= rewardValues[n-1]; j++) {
+            res = max(res, dp[j]);
+        }
+
+        return res;
+        /*
+        遞推 TC:O(n*maxVal+ nlgn) SC:O(n*maxVal)
+        vector<vector<int>> dp(n+1, vector<int>(rewardValues[n-1]+1, INT_MIN/2));
+        int res = 0;
+        for(int j = 0; j <= rewardValues[n-1]; j++) {
+            dp[0][j] = 0;
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = rewardValues[n-1]; j >= 0; j--) {
+                dp[i+1][j] = dp[i][j];
+                if(j < rewardValues[i]) {
+                    if(j + rewardValues[i] >= rewardValues[n-1]) {
+                        dp[i+1][rewardValues[n-1]] = max(dp[i+1][rewardValues[n-1]], dp[i][j] + rewardValues[i]);
+                    }
+                    else dp[i+1][j + rewardValues[i]] = max(dp[i+1][j + rewardValues[i]], dp[i][j] + rewardValues[i]);
+                }
+            }
+        }
+
+        for(int j = 0; j <= rewardValues[n-1]; j++) {
+            res = max(res, dp[n][j]);
+        }
+
+        return res;
+        */
+        /*
+        遞歸 TC:O(n*maxVal+ nlgn) SC:O(n*maxVal)
+        vector<vector<int>> memo(n, vector<int>(rewardValues[0]+2, -1));
+        function<int(int i, int sum, int curMax)> dfs = [&](int i, int sum, int curMax) -> int {
+            if(i < 0) return sum;
+            if(memo[i][curMax] != -1) return memo[i][curMax];
+
+            return memo[i][curMax] = max(dfs(i-1, sum, curMax), curMax < rewardValues[i] ? dfs(i-1, sum + rewardValues[i], curMax + rewardValues[i] >= rewardValues[0] ? rewardValues[0]+1 : curMax + rewardValues[i]) : INT_MIN);
+        };
+
+        return dfs(n-1, 0, 0);
+        */
+    }
+};
 
 // DP TC:O(nlgn + n^2) SC:O(n) Max(n) = 2000
 class Solution {
