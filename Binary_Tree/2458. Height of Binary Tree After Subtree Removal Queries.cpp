de35b6b475026d1a4ff99123b5e9edb81295f722
Nick Chen <s27206383@gmail.com>
2024-11-17 02:16:53 +0800

    Update 2458. Height of Binary Tree After Subtree Removal Queries.cpp

--- Binary_Tree/2458. Height of Binary Tree After Subtree Removal Queries.cpp
+++ Binary_Tree/2458. Height of Binary Tree After Subtree Removal Queries.cpp
@@ -2,6 +2,62 @@
 2458. Height of Binary Tree After Subtree Removal Queries
 */
 
+// DFN序號 TC:O(n) SC:O(n)
+const int MAXN = 100010;
+// 下標示節點的值
+int dfn[MAXN];
+// 下標是dfn序號
+int deep[MAXN]; // 每個序號的深度
+int siz[MAXN]; //每個序號的子樹包含自己的數量
+int maxl[MAXN]; // 根據deep數組從序號0開始到i有多少的深度
+int maxr[MAXN]; // 根據deep數組從序號MAX+1到i有多少深度
+int dfnCnt;
+class Solution {
+public:
+    vector<int> treeQueries(TreeNode* root, vector<int>& queries) {
+        dfnCnt = 0;
+        traverse(root, 0);
+        
+        // 左側最大深度
+        for (int i = 1; i <= dfnCnt; i++) {
+            maxl[i] = max(maxl[i - 1], deep[i]);
+        }
+        
+        // 右側最大深度
+        maxr[dfnCnt + 1] = 0;
+        for (int i = dfnCnt; i >= 1; i--) {
+            maxr[i] = max(maxr[i + 1], deep[i]);
+        }
+        
+        vector<int> ans(queries.size());
+        for (int i = 0; i < queries.size(); i++) {
+            int queryVal = queries[i];
+            int leftMax = maxl[dfn[queryVal] - 1];
+            int rightMax = maxr[dfn[queryVal] + siz[dfn[queryVal]]];
+            ans[i] = max(leftMax, rightMax);
+        }
+        return ans;
+    }
+    // 遍歷樹，記錄 dfn, deep, size
+    void traverse(TreeNode* node, int depth) {
+        if (!node) return;
+        int currentIndex = ++dfnCnt;
+        dfn[node->val] = currentIndex;
+        deep[currentIndex] = depth;
+        siz[currentIndex] = 1;
+        
+        if (node->left) {
+            traverse(node->left, depth + 1);
+            siz[currentIndex] += siz[dfn[node->left->val]];
+        }
+        if (node->right) {
+            traverse(node->right, depth + 1);
+            siz[currentIndex] += siz[dfn[node->right->val]];
+        }
+    }
+};
+
+
 // TC:O(n) SC:O(n)
 class Solution {
     unordered_map<int, int> node2depth; // 目前節點的層數
