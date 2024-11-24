761cc342236372e64a84af650a164220282859df
Nick Chen <s27206383@gmail.com>
2024-11-24 17:24:44 +0800

    Create 3362. Zero Array Transformation III.cpp

---
+++ Greedy/3362. Zero Array Transformation III.cpp
@@ -0,0 +1,34 @@
+/*
+3362. Zero Array Transformation III
+*/
+
+// TC:O(nlgm + m) SC:O(m)
+class Solution {
+public:
+    int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
+        sort(queries.begin(), queries.end());
+        int m = queries.size();
+        int n = nums.size();
+        int idx = 0;
+        int sum = 0;
+        vector<int> diff(n+1);
+        priority_queue<int, vector<int>, less<>> pq;// 彈出最長的終點 但是暫時沒用到 反悔貪心
+        for(int i = 0; i < n; i++) {
+            sum += diff[i];
+            while(idx < m && queries[idx][0] <= i) {
+                pq.push(queries[idx][1]);
+                idx++;
+            }
+            while(sum < nums[i] && !pq.empty() && pq.top() >= i) {
+                diff[pq.top()+1] -= 1;
+                pq.pop();
+                sum += 1;
+            }
+
+            if(sum < nums[i]) return -1;
+        }
+
+        return (int)pq.size();
+        
+    }
+};
