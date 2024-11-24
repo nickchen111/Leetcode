822d795ccbff805043ce0b74957c6afc660cb6e6
Nick Chen <s27206383@gmail.com>
2024-11-24 14:50:37 +0800

    Update 3364. Minimum Positive Sum Subarray.cpp

--- Slide_Window/3364. Minimum Positive Sum Subarray.cpp
+++ Slide_Window/3364. Minimum Positive Sum Subarray.cpp
@@ -2,6 +2,40 @@
 3364. Minimum Positive Sum Subarray
 */
 
+// 最佳解 Binary Search + Presum + Slide Window TC:O(n-l * lg(r-l)) SC:O(r-l)
+class Solution {
+public:
+    int minimumSumSubarray(vector<int>& nums, int l, int r) {
+        int n = nums.size();
+        int res = INT_MAX;
+        multiset<int> set;
+        vector<int> arr(n+1);
+        for(int j = 0; j < n; j++) {
+            arr[j+1] = arr[j] + nums[j];
+            if(j+1 < l) continue;
+            set.insert(arr[j-l+1]);
+            auto iter = set.lower_bound(arr[j+1]);
+            if(iter != set.begin()) {
+                res = min(res, arr[j+1] - *--iter);
+            }
+            if(j+1 >= r) {
+                set.erase(set.find(arr[j+1-r]));
+            }
+        }
+        
+        
+        return res == INT_MAX ? -1 : res;
+    }
+};
+
+/*
+multiset 可以做O(n)
+l <= j-i+1 <= r
+j-r+1 <= i <= j-l+1
+
+*/
+
+
 // TC:O(n^2) SC:O(1)
 class Solution {
 public:
