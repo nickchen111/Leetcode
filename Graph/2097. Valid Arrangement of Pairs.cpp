29be3e69bdc6d940c41c7db66df9f2d86bf2ff03
Nick Chen <s27206383@gmail.com>
2024-11-30 15:07:12 +0800

    Update 2097. Valid Arrangement of Pairs.cpp

--- Graph/2097. Valid Arrangement of Pairs.cpp
+++ Graph/2097. Valid Arrangement of Pairs.cpp
@@ -2,6 +2,37 @@
 2097. Valid Arrangement of Pairs
 */
 
+// 1130 TC:O(n) SC:O(n)
+class Solution {
+public:
+    vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
+        unordered_map<int, vector<int>> next;
+        unordered_map<int, int> deg;
+        vector<vector<int>> res;
+        for(auto &pair : pairs) {
+            next[pair[0]].push_back(pair[1]);
+            deg[pair[0]] -= 1;
+            deg[pair[1]] += 1;
+        }
+        auto dfs = [&](auto &&dfs, int cur) -> void {
+            vector<int> &e = next[cur];
+            while(!e.empty()) {
+                int nxt = e.back();
+                e.pop_back();
+                dfs(dfs, nxt);
+                res.push_back({cur,nxt});
+            }
+        };
+        for(auto it = deg.begin(); it != deg.end(); it++) {
+            if(it->second == -1) dfs(dfs, it->first);
+        }
+        if(res.empty()) dfs(dfs, deg.begin()->first);
+        reverse(res.begin(), res.end());
+
+        return res;
+    }
+};
+
 // TC:O(E+V) SC:O(E+V)
 class Solution {
     unordered_map<int,queue<int>> next;
