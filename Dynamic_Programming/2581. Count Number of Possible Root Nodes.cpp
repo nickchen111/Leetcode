f4f61c719d456ae81146a72eed192f2a587347ae
Nick Chen <s27206383@gmail.com>
2024-12-03 22:42:21 +0800

    Update 2581. Count Number of Possible Root Nodes.cpp

--- Dynamic_Programming/2581. Count Number of Possible Root Nodes.cpp
+++ Dynamic_Programming/2581. Count Number of Possible Root Nodes.cpp
@@ -2,6 +2,62 @@
 2581. Count Number of Possible Root Nodes
 */
 
+// 1203
+class Solution {
+    using LL = long long;
+public:
+    int rootCount(vector<vector<int>>& edges, vector<vector<int>>& guesses, int k) {
+        int n = edges.size() + 1;
+        unordered_set<LL> set;
+        for(auto &g : guesses) {
+            set.insert(((LL)g[0]<<32) | g[1]);
+        }
+        vector<vector<LL>> next(n);
+        for(auto &e : edges) {
+            next[e[0]].push_back(e[1]);
+            next[e[1]].push_back(e[0]);
+        }
+        int root0 = 0;
+        vector<int> validRoot(n);
+        auto dfs0 = [&](auto &&dfs0, LL cur, LL prev) -> void {
+            for(auto &nxt : next[cur]) {
+                if(nxt != prev) {
+                    if(set.find((cur << 32) | nxt) != set.end()) {
+                        root0 += 1;
+                    }
+                    dfs0(dfs0, nxt, cur);
+                }
+            }
+        };
+        dfs0(dfs0, 0, -1);
+        validRoot[0] = root0;
+        int res = root0 >= k;
+        auto dfs = [&](auto &&dfs, LL cur, LL prev) -> void {
+            for(auto &nxt : next[cur]) {
+                if(nxt != prev) {
+                    if(set.find((nxt<<32) | cur) == set.end() && set.find((cur<<32) | nxt) != set.end()) {
+                        validRoot[nxt] = validRoot[cur] - 1;
+                    }
+                    else if(set.find((nxt<<32) | cur) != set.end() && set.find((cur<<32) | nxt) == set.end()) {
+                        validRoot[nxt] = validRoot[cur] + 1;
+                    }
+                    else validRoot[nxt] = validRoot[cur];
+                    res += (validRoot[nxt] >= k);
+                    dfs(dfs, nxt, cur);
+                }
+            }
+        };
+        dfs(dfs, 0, -1);
+        
+        return res;
+    }
+};
+
+/*
+所以問題轉變成 某個點當root Bob猜的edge有哪些是對的 並且至少要有k個猜對
+
+*/
+
 // TC:O(n) SC:O(n+m)
 class Solution {
     using LL = long long;
