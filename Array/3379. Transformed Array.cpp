a57db6b3de50b02384840f96b7bb540561cbda8e
Nick Chen <s27206383@gmail.com>
2024-12-08 13:19:58 +0800

    Create 3379. Transformed Array.cpp

---
+++ Array/3379. Transformed Array.cpp
@@ -0,0 +1,19 @@
+/*
+3379. Transformed Array
+*/
+
+// TC:O(n) SC:O(1)
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n);
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != 0) 
+                res[i] = nums[((i + nums[i])%n + n) % n];
+            else res[i] = nums[i];
+        }
+        
+        return res;
+    }
+};
