560df1119c11eb52d9211f43a7e9c6fc060f0c13
Nick Chen <s27206383@gmail.com>
2024-12-22 14:14:14 +0800

    Update 3397. Maximum Number of Distinct Elements After Operations.cpp

--- Greedy/3397. Maximum Number of Distinct Elements After Operations.cpp
+++ Greedy/3397. Maximum Number of Distinct Elements After Operations.cpp
@@ -8,23 +8,13 @@ public:
     int maxDistinctElements(vector<int>& nums, int k) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-       
+        
         int res = 1;
         for(int i = 0; i < n; i++) {
             if(i == 0) nums[i] = nums[i] - k;
             else {
-                int minR = nums[i] - k, maxR = nums[i] + k;
-                if(nums[i-1] < minR) {
-                    nums[i] = minR;
-                    res += 1;
-                }
-                else if(nums[i-1] == maxR) {
-                    nums[i] = nums[i-1];
-                }
-                else {
-                    nums[i] = nums[i-1] + 1;
-                    res += 1;
-                }
+                nums[i] = min(nums[i]+k, max(nums[i-1] + 1, nums[i] - k));
+                res += (nums[i] != nums[i-1]);
             }
         }
         
