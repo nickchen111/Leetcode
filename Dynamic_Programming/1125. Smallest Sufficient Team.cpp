a6e61d88f9a37cd16425cf07630ae94005944af0
Nick Chen <s27206383@gmail.com>
2024-12-30 19:04:48 +0800

    Update 1125. Smallest Sufficient Team.cpp

--- Dynamic_Programming/1125. Smallest Sufficient Team.cpp
+++ Dynamic_Programming/1125. Smallest Sufficient Team.cpp
@@ -2,6 +2,71 @@
 1125. Smallest Sufficient Team
 */
 
+// 2024.12.30 新增輸出具體方案的雙遞歸  TC:O(T + m * 2^n) SC:O(m * 2^n)
+class Solution {
+    using LL = long long;
+public:
+    vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
+        int n = req_skills.size();
+        int m = people.size();
+        unordered_map<string, int> map;
+        for(int i = 0; i < n; i++) map[req_skills[i]] = i;
+        vector<int> arr(m);
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < people[i].size(); j++) {
+                arr[i] |= (1 << map[people[i][j]]);
+            }
+        }
+        // 輸出具體方案雙遞迴寫法
+        vector<vector<LL>> memo((1 << n), vector<LL>(m,-1));
+        auto dfs = [&](auto &&dfs, int mask, int j) -> int {
+            if(mask == (1 << n) - 1) return 0;
+            if(j == m) return m;
+            if(memo[mask][j] != -1) return memo[mask][j];
+            int ret = min(dfs(dfs, mask, j + 1), dfs(dfs, mask | arr[j], j + 1) + 1);
+            return memo[mask][j] = ret;
+        };
+
+        vector<int> res;
+        auto print_ans = [&](auto &&print_ans, int mask, int j) -> void {
+            if(mask == (1 << n) - 1) return;
+            if(j == m) return;
+            int ret1 = dfs(dfs, mask, j + 1);
+            int ret2 = dfs(dfs, mask | arr[j], j + 1) + 1;
+            if(ret1 < ret2) {
+                print_ans(print_ans, mask, j + 1);
+            }
+            else {
+                res.push_back(j);
+                print_ans(print_ans, mask | arr[j], j + 1);
+            }
+        };
+        print_ans(print_ans, 0, 0);
+        return res;
+        /*
+        遞歸 TC:O(T + m * 2^n) SC:O(m * 2^n)
+        vector<vector<LL>> memo((1 << n), vector<LL>(m,-1));
+        auto dfs = [&](auto &&dfs, int mask, int j) -> LL {
+            if(mask == (1 << n) - 1) return 0;
+            if(j == m) return (1LL << m) - 1LL;
+            if(memo[mask][j] != -1) return memo[mask][j];
+            LL ret1 = dfs(dfs, mask, j + 1);
+            LL ret2 = dfs(dfs, mask | arr[j], j + 1) | (1LL << j);
+            
+            return memo[mask][j] = __builtin_popcountll(ret1) < __builtin_popcountll(ret2) ? ret1 : ret2;
+        };
+        LL ret = dfs(dfs, 0, 0);
+        vector<int> res;
+        
+        for(int i = 0; i < m; i++) {
+            if((ret >> i) & 1) res.push_back(i);
+        }
+
+        return res;
+        */
+    }
+};
+
 // TC:O(T + n*2^m) T為字串加總長度 SC:O(n*2^m)
 class Solution {
 public:
