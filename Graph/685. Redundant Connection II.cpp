a21104e1870f05fcf69209e0fd21ee5176d3f697
Nick Chen <s27206383@gmail.com>
2025-01-04 18:09:05 +0800

    Update 685. Redundant Connection II.cpp

--- Graph/685. Redundant Connection II.cpp
+++ Graph/685. Redundant Connection II.cpp
@@ -2,6 +2,51 @@
 685. Redundant Connection II
 */
 
+// 2025.01.04 TC:O(n * ⍺(n)) SC:O(n)
+class Solution {
+public:
+    vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int> parent(n+1);
+        iota(parent.begin(), parent.end(), 0);
+        auto find = [&](int x) -> int {
+            int rt = x;
+            while (rt != parent[rt]) {
+                rt = parent[rt];
+            }
+            if(parent[x] != rt) {
+                parent[x] = rt;
+            }
+            return rt;
+        };
+        // 找出 parent[b] 已經有的點
+        vector<int> cand1;
+        vector<int> cand2;
+        for(auto &edge : edges) {
+            int a = edge[0], b = edge[1];
+            if(parent[b] == b) {
+                parent[b] = a;
+            }
+            else {
+                cand1 = {a,b};
+                cand2 = {parent[b], b};
+            }
+        }
+        iota(parent.begin(), parent.end(), 0);
+        for(auto &edge : edges) {
+            if(edge == cand1) continue;
+            int a = edge[0], b = edge[1];
+            if(find(a) == find(b)) {
+                if(!cand2.empty()) return cand2;
+                else return edge;
+            }
+            else parent[b] = a;
+        }
+
+        return cand1;
+    }
+};
+
 // Union_Find TC:O(V+E) SC:O(V)
 class Solution {
     unordered_map<int,int> parent;
