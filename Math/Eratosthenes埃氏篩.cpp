32ce9625df96e5cdf9076dd6a7cf8f3b5b6275fa
Nick Chen <s27206383@gmail.com>
2025-01-12 01:04:10 +0800

    Update Eratosthenes埃氏篩.cpp

--- Math/Eratosthenes埃氏篩.cpp
+++ Math/Eratosthenes埃氏篩.cpp
@@ -1,18 +1,18 @@
 class Solution{
     vector<int> Eratosthenes(int n){
-        vector<int> q(n+1,0);
-        for(int i = 2; i<=sqrt(n); i++){
-            if(q[i] == 1) continue;
-            int j = 2*i; 
+        vector<int> np(n+1);
+        for(int i = 2; i*i <= n; i++){
+            if(np[i] == 1) continue;
+            int j = i*i; 
             while(j<=n){
-                q[j] = 1;
+                np[j] = 1;
                 j+=i;
             }
         }
 
         vector<int> prime;
         for(int i = 2; i<=n; i++){
-            if(q[i] == 0) prime.push_back(i);
+            if(np[i] == 0) prime.push_back(i);
         }
     }
 };
