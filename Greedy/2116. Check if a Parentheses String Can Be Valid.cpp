b18558d9c0ec3a5fb07308f0121d2902bd8a7979
Nick Chen <s27206383@gmail.com>
2025-01-12 15:34:26 +0800

    Create 2116. Check if a Parentheses String Can Be Valid.cpp

---
+++ Greedy/2116. Check if a Parentheses String Can Be Valid.cpp
@@ -0,0 +1,23 @@
+// TC:O(n) SC:O(1)
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int n = s.size();
+        if(n % 2) return false;
+        int cnt = 0; 
+        
+        for(int i = 0; i < n; i++) {
+            if(locked[i] == '0' || s[i] == '(') cnt += 1;
+            else if(cnt > 0) cnt -= 1;
+            else return false;
+        }
+        
+        cnt = 0;
+        for(int i = n-1; i >= 0; i--) {
+            if(locked[i] == '0' || s[i] == ')') cnt += 1;
+            else if(cnt > 0) cnt -= 1;
+            else return false;
+        }
+        return true;
+    }
+};
