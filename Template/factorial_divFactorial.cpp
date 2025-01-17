5d5760d6f22b57b7d893cf1bc13ed5098f898146
Nick Chen <s27206383@gmail.com>
2025-01-17 22:08:11 +0800

    Update factorial_divFactorial.cpp

--- Template/factorial_divFactorial.cpp
+++ Template/factorial_divFactorial.cpp
@@ -15,3 +15,13 @@ auto build = [&](LL limit) {
     }
 };
 build(n);
+long long quickMul(long long a, long long b) {
+    long long ans = 1;
+    while(b) {
+        if(b & 1) ans = (ans*a) % MOD;
+        a = (a*a) % MOD;
+        b >>= 1;
+    }
+
+    return ans;
+}
