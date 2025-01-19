d3c0925bfeacd7869996923a117fb3999d98717e
Nick Chen <s27206383@gmail.com>
2025-01-19 17:14:07 +0800

    Update 3428. Maximum and Minimum Sums of at Most Size K Subsequences.cpp

--- Math/3428. Maximum and Minimum Sums of at Most Size K Subsequences.cpp
+++ Math/3428. Maximum and Minimum Sums of at Most Size K Subsequences.cpp
@@ -50,6 +50,61 @@ public:
     }
 };
 
+// 次佳解 已可以通過 TC:O(nlgn + n) SC:O(n) 對稱性寫法
+class Solution {
+    using LL = long long;
+    LL MOD = 1e9 + 7;
+public:
+    int minMaxSums(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        LL res = 0;
+        vector<LL> fac(n+1);
+        fac[0] = 1;
+        vector<LL> inv(n+1);
+        // 建造 階級的 fac 與逆元 
+        function<LL(LL a, LL b)> quickMul = [&](LL a, LL b) -> LL {
+            LL ans = 1;
+            while(b) {
+                if(b & 1) {
+                    ans = (ans * a) % MOD;
+                }
+                a = (a*a) % MOD;
+                b >>= 1;
+            }
+
+            return ans;
+        };
+        auto build = [&](LL limit) {
+            fac[1] = 1;
+            for(int i = 2; i <= limit; i++) {
+                fac[i] = ((LL) i * fac[i-1]) % MOD;
+            }
+
+            inv[limit] = quickMul(fac[limit], MOD-2);
+            for(int i = limit - 1; i >= 0; i--) {
+                inv[i] = ((LL) (i+1) * inv[i+1]) % MOD;
+            }
+        };
+        build(n);
+        
+        // for(int i = 0; i < n; i++) {
+        //     res = (res + (LL)nums[i]*2) % MOD;
+        // }
+        
+        for(int len = 0; len <= k-1; len++) {
+            for(int i = 0; i < n; i++) {
+                if(i >= len) {
+                    res = (res + ((LL) (nums[i] + nums[n-i-1]) * fac[i] % MOD * inv[len] % MOD * inv[i-len]) % MOD) % MOD;
+                }
+            }
+        }
+        
+        return res % MOD;
+    }
+};
+
+
 
 // 次佳解 已可以通過 TC:O(nlgn + n) SC:O(n)
 class Solution {
