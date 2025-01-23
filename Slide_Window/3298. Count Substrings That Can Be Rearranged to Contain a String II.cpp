a9970c43fe5ae929e56a799a8a4ebf1ba5a6e1d1
Nick Chen <s27206383@gmail.com>
2025-01-24 01:51:39 +0800

    Update 3298. Count Substrings That Can Be Rearranged to Contain a String II.cpp

--- Slide_Window/3298. Count Substrings That Can Be Rearranged to Contain a String II.cpp
+++ Slide_Window/3298. Count Substrings That Can Be Rearranged to Contain a String II.cpp
@@ -2,6 +2,34 @@
 3298. Count Substrings That Can Be Rearranged to Contain a String II
 */
 
+// 2025. 0123
+class Solution {
+    using LL = long long;
+public:
+    long long validSubstringCount(string word1, string word2) {
+        int m = word1.size(), n = word2.size();
+        if(m < n) return 0;
+        vector<LL> cnt2(26);
+        LL variety = 0, valid = 0, ans = 0, i = 0;
+        for(auto &s : word2) {
+            cnt2[s-'a'] += 1;
+            if(cnt2[s-'a'] == 1) variety += 1;
+        }
+        vector<LL> cnt1(26);
+        for(int j = 0; j < m; j++) {
+            cnt1[word1[j] - 'a'] += 1;
+            if(cnt1[word1[j] - 'a'] == cnt2[word1[j] - 'a']) valid += 1;
+            while(valid == variety) {
+                ans += m - j;
+                if(cnt1[word1[i] - 'a'] == cnt2[word1[i] - 'a']) valid -= 1;
+                cnt1[word1[i] - 'a'] -= 1;
+                i += 1;
+            }
+        }
+        return ans;
+    }
+};
+
 // TC:O(n+m) SC:O(n+m)
 class Solution {
     using LL = long long;
