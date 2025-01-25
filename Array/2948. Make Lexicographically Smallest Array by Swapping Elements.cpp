1bcdd90f19ced1804421a249afe369378532ab33
Nick Chen <s27206383@gmail.com>
2025-01-25 15:47:35 +0800

    Update 2948. Make Lexicographically Smallest Array by Swapping Elements.cpp

--- Array/2948. Make Lexicographically Smallest Array by Swapping Elements.cpp
+++ Array/2948. Make Lexicographically Smallest Array by Swapping Elements.cpp
@@ -2,6 +2,78 @@
 2948. Make Lexicographically Smallest Array by Swapping Elements
 */
 
+// 2025.01.25
+class Solution {
+public:
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        int n = nums.size();
+        vector<int> arr(n);
+        iota(arr.begin(), arr.end(), 0);
+        ranges::sort(arr, [&](int i, int j){return nums[i] < nums[j];});
+        vector<int> res(n);
+        int i = 0;
+        while(i < n) {
+            int st = i;
+            i += 1;
+            while(i < n && nums[arr[i]] - nums[arr[i-1]] <= limit) {
+                i += 1;
+            }
+            vector<int> idx(arr.begin() + st, arr.begin() + i);
+            ranges::sort(idx);
+            for(int j = 0; j < idx.size(); j++) {
+                res[idx[j]] = nums[arr[st++]];
+            }
+        }
+        return res;
+    }
+};
+
+// Union find
+class Solution {
+public:
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        int n = nums.size();
+        vector<int> pa(n);
+        iota(pa.begin(), pa.end(), 0);
+        auto find = [&](int x) -> int {
+            int rt = x;
+            while(rt != pa[rt]) rt = pa[rt];
+            if(pa[x] != rt) pa[x] = rt;
+            return rt;
+        };
+        auto merge = [&](int x, int y) {
+            x = find(x), y = find(y);
+            if(x < y) pa[y] = x;
+            else pa[x] = y; 
+        };
+        vector<pair<int,int>> arr;
+        for(int i = 0; i < n; i++) {
+            arr.emplace_back(nums[i], i);
+        }
+        ranges::sort(arr);
+        // 滑窗merge
+        int i = 0;
+        for(int j = 0; j < n; j++) {
+            while(arr[j].first - arr[i].first > limit) i += 1;
+            merge(arr[j].second, arr[i].second);
+        }
+        unordered_map<int, vector<int>> mp; // pa => number
+        for(int i = 0; i < n; i++) mp[find(i)].emplace_back(nums[i]);
+        for(auto &p : mp) ranges::sort(p.second.begin(), p.second.end());
+        vector<int> res(n);
+        vector<int> idx(n);
+        for(int i = 0; i < n; i++) {
+            int idxx = find(i);
+            res[i] = mp[idxx][idx[idxx]++];
+        }
+        return res;
+    }
+};
+
+/*
+可以交換元素 只要abs(nums[i] - nums[j]) <= limit
+*/
+
 // TC:O(nlgn + n) SC:O(n)
 class Solution {
 public:
