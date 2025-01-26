ba51b6fd63fcc624dae6ab0b33523353a05da829
Nick Chen <s27206383@gmail.com>
2025-01-27 01:58:20 +0800

    Update 992. Subarrays with K Different Integers.cpp

--- Slide_Window/992. Subarrays with K Different Integers.cpp
+++ Slide_Window/992. Subarrays with K Different Integers.cpp
@@ -2,6 +2,28 @@
 992. Subarrays with K Different Integers
 */
 
+// 2025.01.26 三指針 one pass
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        int n = nums.size();
+        int i1 = 0, i2 = 0, ans = 0, sum1 = 0, sum2 = 0;
+        unordered_map<int,int> mp1, mp2;
+        for(int j = 0; j < n; j++) {
+            sum1 += (mp1[nums[j]]++ == 0);
+            sum2 += (mp2[nums[j]]++ == 0);
+            while(sum1 >= k) {
+                sum1 -= (mp1[nums[i1++]]-- == 1);
+            }
+            while(sum2 >= k+1) {
+                sum2 -= (mp2[nums[i2++]]-- == 1);
+            }
+            ans += (i1-i2);
+        }
+        return ans;
+    }
+};
+
 // TC:O(n) SC:O(n)
 class Solution {
 public:
