7e72925587618f571c2de34063bfac862f941965
Nick Chen <s27206383@gmail.com>
2025-01-26 13:33:57 +0800

    Create 3432. Count Partitions with Even Sum Difference.cpp

---
+++ Array/3432. Count Partitions with Even Sum Difference.cpp
@@ -0,0 +1,15 @@
+// TC:O(n) SC:O(1)
+class Solution {
+public:
+    int countPartitions(vector<int>& nums) {
+        int n = nums.size();
+        int suffix = reduce(nums.begin(), nums.end(), 0);
+        int prefix = 0, ans = 0;
+        for(int i = 0; i < n-1; i++) {
+            prefix += nums[i];
+            suffix -= nums[i];
+            if((suffix - prefix) % 2 == 0) ans += 1;
+        }
+        return ans;
+    }
+};
