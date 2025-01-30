87d0588cdf4f8b7589fe2a97ef7e9704d90cfb40
Nick Chen <s27206383@gmail.com>
2025-01-30 22:59:00 +0800

    Update 283. Move Zeroes.cpp

--- Two_Pointer/283. Move Zeroes.cpp
+++ Two_Pointer/283. Move Zeroes.cpp
@@ -2,6 +2,20 @@
 283. Move Zeroes
 */
 
+// 2025.01.30
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i0 = 0;
+        for (int& x : nums) {
+            if (x) {
+                swap(x, nums[i0]);
+                i0++;
+            }
+        }
+    }
+};
+
 //TC:O(n) SC:O(1)
 class Solution {
 public:
