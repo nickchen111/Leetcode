ec86fa42fa29daf74fbd9e58990b134eb600d140
Nick Chen <s27206383@gmail.com>
2025-02-05 03:04:17 +0800

    Update 2444. Count Subarrays With Fixed Bounds.cpp

--- Slide_Window/2444. Count Subarrays With Fixed Bounds.cpp
+++ Slide_Window/2444. Count Subarrays With Fixed Bounds.cpp
@@ -2,6 +2,23 @@
 2444. Count Subarrays With Fixed Bounds
 */
 
+// 2025.02.04
+class Solution {
+    using LL = long long;
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        int min_i = -1, max_i = -1, i0 = -1, n = nums.size();
+        LL ans = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == minK) min_i = i;
+            if(nums[i] == maxK) max_i = i;
+            if(nums[i] > maxK || nums[i] < minK) i0 = i;
+            ans += (LL)max(0, min(min_i, max_i) - i0);
+        }
+        return ans;
+    }
+};
+
 //TC:O(n) SC:O(1)
 class Solution {
 public:
