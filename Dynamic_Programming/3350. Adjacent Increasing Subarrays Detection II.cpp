1580abb08eeb6dc1ac895c2db899aa6432b0c9fa
Nick Chen <s27206383@gmail.com>
2025-02-07 20:16:20 +0800

    Update 3350. Adjacent Increasing Subarrays Detection II.cpp

--- Dynamic_Programming/3350. Adjacent Increasing Subarrays Detection II.cpp
+++ Dynamic_Programming/3350. Adjacent Increasing Subarrays Detection II.cpp
@@ -2,6 +2,22 @@
 3350. Adjacent Increasing Subarrays Detection II
 */
 
+// 分組循環 TC:O(n) SC:O(1)
+class Solution {
+public:
+    int maxIncreasingSubarrays(vector<int>& nums) {
+        int n = nums.size(), prev = 0, i = 0, ans = 0;
+        while(i < n) {
+            int start = i;
+            while(i + 1 < n && nums[i] < nums[i+1]) i += 1;
+            ans = max(ans, max( (i - start + 1) / 2, min(prev, i - start + 1)));
+            prev = i - start + 1;
+            i += 1;
+        }
+        return ans;
+    }
+};
+
 // TC:O(n) SC:O(n)
 class Solution {
 public:
