dfe0ce8e303ac2a93f721af6145d4f2e24cac463
Nick Chen <s27206383@gmail.com>
2025-02-07 21:14:24 +0800

    Update 467. Unique Substrings in Wraparound String.cpp

--- Dynamic_Programming/467. Unique Substrings in Wraparound String.cpp
+++ Dynamic_Programming/467. Unique Substrings in Wraparound String.cpp
@@ -6,12 +6,12 @@ public:
         vector<int> cnt(26); // 每個字母開始的長度最大值
         while(i >= 0) {
             int start = i;
-            while(i - 1 >= 0 && ((((s[i] - 'a') - 1) % 26 + 26) % 26) == ((s[i-1] - 'a') % 26)) {
-                cnt[s[i] - 'a'] = max(cnt[s[i] - 'a'], start - i + 1);
-                i -= 1;
-            }
             cnt[s[start] - 'a'] = max(cnt[s[start] - 'a'], 1);
-            cnt[s[i] - 'a'] = max(cnt[s[i] - 'a'], start - i + 1);
+            while(i - 1 >= 0 && ((((s[i] - 'a') - 1) % 26 + 26) % 26) == ((s[i-1] - 'a') % 26)) {
+                i -= 1;
+                cnt[s[i] - 'a'] = max(cnt[s[i] - 'a'], start - i + 1);
+            }
+            
             i -= 1;
         }
 
