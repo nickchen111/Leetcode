441ce779b29106545f32bf659620bd7f162e7768
Nick Chen <s27206383@gmail.com>
2025-02-11 14:52:02 +0800

    Create 1910. Remove All Occurrences of a Substring.cpp

---
+++ String/1910. Remove All Occurrences of a Substring.cpp
@@ -0,0 +1,10 @@
+// TC:O(n*m) SC:O(1)
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(s.length() > 0 && s.length() > s.find(part)){
+           s.erase(s.find(part), part.length());
+        }
+        return s;
+    }
+};
