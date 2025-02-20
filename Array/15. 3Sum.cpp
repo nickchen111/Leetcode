b5b03b876db3e1c7dfe3b724f1efb2634cf7081e
Nick Chen <s27206383@gmail.com>
2025-02-20 19:08:20 +0800

    Update 15. 3Sum.cpp

--- Array/15. 3Sum.cpp
+++ Array/15. 3Sum.cpp
@@ -2,6 +2,37 @@
 15. 3Sum
 */
 
+// 2025.02.20
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        ranges::sort(nums);
+        vector<vector<int>> ans;
+        for(int i = 0; i < n-2; i++) {
+            if(i && nums[i] == nums[i-1]) continue;
+            if(nums[i] + nums[i+1] + nums[i+2] > 0) break;
+            int p1 = i+1, p2 = n-1;
+            while(p1 < p2) {
+                int sum = nums[i] + nums[p1] + nums[p2];
+                if(sum > 0) {
+                    p2 -= 1;
+                }
+                else if(sum < 0) {
+                    p1 += 1;
+                }
+                else {
+                    ans.push_back({nums[i], nums[p1], nums[p2]});
+                    while(p1 < p2 && nums[p1] == nums[p1 + 1]) p1 += 1;
+                    while(p2 > p1 && nums[p2] == nums[p2-1]) p2 -= 1;
+                    p1 += 1, p2 -= 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
 //Brute force TC:O(n^3+nlgn) SC:O(1)
 //也可以用set<vector<int>>   去重
 class Solution {
