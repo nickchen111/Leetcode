c9d9b60ee1646256968edcfffcfa0b64a6e12e87
Nick Chen <s27206383@gmail.com>
2025-02-20 19:10:01 +0800

    Update 15. 3Sum.cpp

--- Array/15. 3Sum.cpp
+++ Array/15. 3Sum.cpp
@@ -12,6 +12,7 @@ public:
         for(int i = 0; i < n-2; i++) {
             if(i && nums[i] == nums[i-1]) continue;
             if(nums[i] + nums[i+1] + nums[i+2] > 0) break;
+            if(nums[i] + nums[n-1] + nums[n-2] < 0) continue;
             int p1 = i+1, p2 = n-1;
             while(p1 < p2) {
                 int sum = nums[i] + nums[p1] + nums[p2];
