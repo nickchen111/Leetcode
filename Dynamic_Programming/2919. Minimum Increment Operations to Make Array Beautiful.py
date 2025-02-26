0f3a74743b3efcb203dfb1ae418010bb0e4805d3
Nick Chen <s27206383@gmail.com>
2025-02-26 21:59:03 +0800

    Update 2919. Minimum Increment Operations to Make Array Beautiful.py

--- Dynamic_Programming/2919. Minimum Increment Operations to Make Array Beautiful.py
+++ Dynamic_Programming/2919. Minimum Increment Operations to Make Array Beautiful.py
@@ -1,4 +1,3 @@
-# TC:O(n) SC:O(1)
 class Solution:
     def minIncrementOperations(self, nums: List[int], k: int) -> int:
         '''
@@ -6,16 +5,14 @@ class Solution:
         每個數字會有的狀態是選擇+ or 不加 
         比k小 我加你 可以跳去前三個數字
         '''
+        # 這是枚舉選哪個的思想寫出的遞推
         n = len(nums)
         f0, f1, f2 = 0, inf, inf
         for i in range(n):
-            prev0 = f0
-            f0 = min(f0, f1, f2) + max(0, k - nums[i])
-            f2 = f1
-            f1 = prev0
+            f0, f1, f2 = min(f0, f1, f2) + max(0, k - nums[i]), f0, f1
         return min(f0, f1, f2)
         '''
-        遞推
+        枚舉選哪個的思想寫出的遞推
         f = [[inf] * (3) for _ in range(n+1)]
         f[0][0] = 0
         for i in range(n):
@@ -25,7 +22,17 @@ class Solution:
         return min(f[n][0], f[n][1], f[n][2])
         '''
         '''
-        遞歸 單狀態
+        選 or 不選的遞推
+        f = [[0] * (3) for _ in range(n+1)]
+        for i in range(n):
+            for j in range(3):
+                f[i+1][j] = f[i][0] + max(0, k - nums[i])
+                if j < 2:
+                    f[i+1][j] = min(f[i+1][j], f[i][j+1])
+        return f[n][0]
+        '''
+        '''
+        遞歸 單狀態 枚舉選哪個思想X
         @cache
         def dfs(i:int) -> int:
             if i < 0:
@@ -34,7 +41,7 @@ class Solution:
         return min(dfs(n-1), dfs(n-2), dfs(n-3))
         '''
         '''
-        遞歸 雙狀態定義成 多用一個j表示前面有多少個數字沒處理
+        遞歸 雙狀態定義成 多用一個j表示前面有多少個數字沒處理 選 or 不選
         @cache
         def dfs(i:int, j:int) -> int:
             if i < 0:
