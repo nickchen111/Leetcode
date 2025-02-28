fcc1958b4cf2bd8a7086eee2797b6a96bd43ae73
Nick Chen <s27206383@gmail.com>
2025-02-28 16:57:25 +0800

    Update 139. Word Break.cpp

--- Dynamic_Programming/139. Word Break.cpp
+++ Dynamic_Programming/139. Word Break.cpp
@@ -32,6 +32,136 @@ public:
     }
 };
 
+// 2025.02.28 DP + Trie 遞推寫法 TC:O(n^2) SC:O(mL + n)
+class Solution {
+public:
+    struct TrieNode {
+        TrieNode() {
+            for(int i = 0; i < 26; i++) children[i] = NULL;
+            isEnd = false;
+        }
+        ~TrieNode() {
+            for(auto child: children){
+                if(child) delete child;
+            }
+        }
+        TrieNode* children[26];
+        bool isEnd;
+    };
+    TrieNode* root;
+    bool wordBreak(string s, vector<string>& wordDict) {
+        // 建立 Trie
+        root = new TrieNode();
+        for(auto &word : wordDict) {
+            TrieNode* node = root;
+            for(auto ch : word) {
+                if(node->children[ch-'a'] == nullptr)
+                    node->children[ch-'a'] = new TrieNode();
+                node = node->children[ch-'a'];
+            }
+            node->isEnd = true;
+        }
+        
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+        dp[n] = true;  // 空字串可以拆分
+        
+        // 從後向前遞推
+        for (int i = n - 1; i >= 0; i--) {
+            TrieNode* node = root;
+            // 嘗試從位置 i 向右擴展
+            for (int j = i; j < n; j++) {
+                if (node->children[s[j]-'a'] == nullptr) break;
+                node = node->children[s[j]-'a'];
+                if (node->isEnd && dp[j + 1]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[0];
+    }
+};
+
+
+// DFS + Trie + memo    TC:O(n^2) SC:O(n^2)
+class Solution {
+    struct TrieNode{
+        TrieNode(){
+            isEnd = false;
+            for(int i = 0; i<26; i++){
+                children.push_back(nullptr);
+            }
+        }
+        ~TrieNode(){
+            for(auto child:children){
+                if(child) delete child;
+            }
+        }
+        vector<TrieNode*> children;
+        bool isEnd;
+    };
+    int memo[300];
+    TrieNode* root;
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        root = new TrieNode();
+        for(auto word:wordDict){
+
+            TrieNode* p = root;
+
+            for(auto ch: word){
+                if(p->children[ch - 'a'] == nullptr){
+                    p->children[ch - 'a'] = new TrieNode();
+                }
+                p = p->children[ch-'a'];
+            }
+            p->isEnd = true;
+        }
+        //分割string s 傳入 並確認他的後面是否也可以被分割 此題只需確認是否可分割 
+        return backtrack(s, 0);
+
+    }
+    bool backtrack(string& s, int cur){        
+        if(cur == s.size()) return true; //代表走到尾了都可以切割 
+        if(memo[cur] == -1) return false;//之前就走過是不行的
+        TrieNode* p = root;
+        for(int i = cur; i < s.size(); i++){
+            if(p->children[s[i] - 'a'] != nullptr){
+                p = p->children[s[i] - 'a'];
+                if(p->isEnd == true && backtrack(s, i+1)) return true;
+            }
+            else break;
+        }
+        memo[cur] = -1;
+        return false;
+    }
+};
+
+// DP解 但不用Hash Set 也不用Trie去做優化 TC: O(n*m*k) SC:  O(n + m)
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) 
+    {
+        int n = s.size();
+        
+        vector<bool>dp(n+1,0);
+        
+        dp[0] = true; //什麼字都沒有 但一定要先設一個true才能繼續抉擇下去
+        
+        for(int i = 1; i<=n; i++){ //建立dp數組 i算是此段substring的尾巴
+        //去比較string的某段sub 是否在字典裡 且他前面的dp是有那麼這一段也是有的
+            for(auto word:wordDict){
+                if(i >= word.size() && s.substr(i-word.size(), word.size()) == word && dp[i-word.size()]){
+                    dp[i] = true;
+                    break;//重新再找搞不好會重複用到此word
+                }
+            }
+        }
+        return dp[n];
+    }
+};
 
 // 暴力解 
 class Solution {
@@ -56,90 +186,3 @@ public:
     }
 };
 
-
-//dfs+trie 在更優化就是加上memorization   tc:O(n^2) sc:O(n^2)
-class Solution {
-    struct TrieNode{//這是一種寫法 也可以寫成class
-        TrieNode(){
-            isEnd = false;
-            for(int i = 0; i<26; i++){
-                children.push_back(nullptr);
-            }
-        }
-        ~TrieNode(){
-            for(auto child:children){
-                if(child) delete child;
-            }
-        }
-        vector<TrieNode*> children;
-        bool isEnd;
-    };
-    int memo[300];
-    TrieNode* root;
-
-public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        root = new TrieNode();
-        
-        //構建trie
-        for(auto word:wordDict){
-
-            TrieNode* p = root;
-
-            for(auto ch: word){
-                if(p->children[ch - 'a'] == nullptr){
-                    p->children[ch - 'a'] = new TrieNode();
-                }
-                p = p->children[ch-'a'];
-            }
-            p->isEnd = true;
-        }
-        //分割string s 傳入 並確認他的後面是否也可以被分割 此題只需確認是否可分割 
-        return backtrack(s, 0);
-
-    }
-    bool backtrack(string& s, int cur){
-        
-        
-        if(cur == s.size()) return true; //代表走到尾了都可以切割 
-        if(memo[cur] == -1) return false;//之前就走過是不行的
-
-        TrieNode* p = root;
-        for(int i = cur; i < s.size(); i++){
-            if(p->children[s[i] - 'a'] != nullptr){
-                p = p->children[s[i] - 'a'];
-                if(p->isEnd == true && backtrack(s, i+1)) return true;
-            }
-            else break;
-        }
-
-        memo[cur] = -1;
-        return false;
-    }
-};
-
-
-//dp解 tc: O(n*m*k) sc:  O(n + m)
-class Solution {
-public:
-    bool wordBreak(string s, vector<string>& wordDict) 
-    {
-        int n = s.size();
-        
-        vector<bool>dp(n+1,0);
-        
-        dp[0] = true; //什麼字都沒有 但一定要先設一個true才能繼續抉擇下去
-        
-        for(int i = 1; i<=n; i++){ //建立dp數組 i算是此段substring的尾巴
-        //去比較string的某段sub 是否在字典裡 且他前面的dp是有那麼這一段也是有的
-            for(auto word:wordDict){
-                if(i >= word.size() && s.substr(i-word.size(), word.size()) == word && dp[i-word.size()]){
-                    dp[i] = true;
-                    break;//重新再找搞不好會重複用到此word
-                }
-            }
-        }
-        return dp[n];
-        
-
-    }
