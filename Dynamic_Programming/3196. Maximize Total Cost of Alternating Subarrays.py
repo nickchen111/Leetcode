718d5603d5d81ae7afec49fb0a7104e1b492e116
Nick Chen <s27206383@gmail.com>
2025-03-01 18:50:19 +0800

    Update 3196. Maximize Total Cost of Alternating Subarrays.py

--- Dynamic_Programming/3196. Maximize Total Cost of Alternating Subarrays.py
+++ Dynamic_Programming/3196. Maximize Total Cost of Alternating Subarrays.py
@@ -33,3 +33,35 @@ class Solution:
             else: return nums[i] * -1 + dfs(i-1, 1)
         return max(dfs(n-1, 0), dfs(n-1, 1))
         '''
+
+# 劃分型DP 思路： 每一個劃分都可以切分成兩個一組 or 一個一組 
+class Solution:
+    def maximumTotalCost(self, nums: List[int]) -> int:
+        n = len(nums)
+        # 空間優化
+        f0, f1 = 0, nums[0]
+        for i in range(1,n):
+            prev0, prev1 = f0, f1
+            f0 = f1
+            f1 = max(prev1 + nums[i], prev0 + nums[i-1] - nums[i])
+        return f1
+        '''
+        遞推 
+        f = [-inf] * (n+1)
+        f[0] = 0
+        f[1] = nums[0]
+        for i in range(1,n):
+            f[i+1] = max(f[i] + nums[i], f[i-1] + nums[i-1] - nums[i])
+        return f[n]
+        '''
+        '''
+        遞歸 TC:O(n) SC:O(n)
+        @cache
+        def dfs(i:int) ->int:
+            if i == 0:
+                return nums[0]
+            if i < 0:
+                return 0
+            return max(dfs(i-1) + nums[i], dfs(i-2) + nums[i-1] - nums[i])
+        return dfs(n-1)
+        '''
