27a61014e18ba85058c3482643522774bfaf1b24
Nick Chen <s27206383@gmail.com>
2025-03-15 02:45:44 +0800

    Update 2463. Minimum Total Distance Traveled.cpp

--- Dynamic_Programming/2463. Minimum Total Distance Traveled.cpp
+++ Dynamic_Programming/2463. Minimum Total Distance Traveled.cpp
@@ -1,7 +1,72 @@
-/*
-2463. Minimum Total Distance Traveled
-*/
-
+// 2025.03.14 遞推 + 遞歸 
+class Solution {
+    using ll = long long;
+public:
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        ranges::sort(robot), ranges::sort(factory);
+        int n = robot.size(), m = factory.size();
+        // 空間優化
+        vector<ll> dp(n + 1, LLONG_MAX/2);
+        dp[0] = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = n-1; j >= 0; j--) {
+                ll total = factory[i][1], posf = factory[i][0], sum = 0;
+                for(int k = j; k >= 0 && total; k--, total--) {
+                    sum += abs(posf - (ll)robot[k]);
+                    dp[j+1] = min(dp[j+1], sum + dp[k]);
+                }
+            }
+        }
+        return dp[n];
+        /*
+        遞推
+        vector dp(m + 1, vector<ll>(n + 1, LLONG_MAX/2));
+        dp[0][0] = 0;
+        for(int i = 0; i < m; i++) {
+            dp[i+1][0] = 0;
+            for(int j = 0; j < n; j++) {
+                ll total = factory[i][1], posf = factory[i][0], sum = 0;
+                dp[i+1][j+1] = dp[i][j+1];
+                for(int k = j; k >= 0 && total; k--, total--) {
+                    sum += abs(posf - (ll)robot[k]);
+                    dp[i+1][j+1] = min(dp[i+1][j+1], sum + dp[i][k]);
+                }
+            }
+        }
+        return dp[m][n];
+        */
+        /*
+        遞歸 
+        vector memo(n, vector<ll>(m, LLONG_MAX/2));
+        auto dfs = [&](auto &&dfs, int i, int j) -> ll {
+            if(i < 0) return 0;// 代表機器人修理完了
+            if(j == 0) {
+                if (factory[j][1] < i + 1) return LLONG_MAX/2;
+                else {
+                    ll sum = 0;
+                    for(int k = 0; k <= i; k++) {
+                        sum += abs(factory[j][0] - robot[k]);
+                    }
+                    return sum;
+                }
+            }
+            ll &ret = memo[i][j];
+            if(ret != LLONG_MAX/2) return ret;
+            ll total = factory[j][1], posf = factory[j][0];
+            ret = dfs(dfs, i, j - 1); // 不選
+            ll sum = 0;
+            while(i >= 0 && total) {
+                sum += abs(posf - (ll) robot[i]);
+                total -= 1;
+                ret = min(ret, dfs(dfs, i - 1, j - 1) + sum);
+                i--;
+            }
+            return ret;
+        };
+        return dfs(dfs, n-1, m-1);
+        */
+    }
+};
 
 // 遞推 + 遞歸 
 class Solution {
@@ -65,86 +130,6 @@ public:
 原問題是 n個工廠 修理m個機器人 子問題會是 f(n-1, m-1), f(n-2, m-1) ... f(n-factory[m], m-1)
 然後加上各自cost 取最小
 */
-
-// 遞歸 TC:O(nm^2) SC:O(m*n)
-class Solution {
-    using LL = long long;
-    int m, n;
-    vector<vector<LL>> memo;
-    vector<int> robot;
-    vector<vector<int>> factory;
-public:
-    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
-        m = robot.size();
-        n = factory.size();
-        sort(factory.begin(), factory.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0];
-        });
-        sort(robot.begin(), robot.end());
-        this -> robot = robot;
-        this -> factory = factory;
-
-        memo.resize(n, vector<LL>(m, -1));
-
-        return helper(0,0);
-    }
-    LL helper(int i, int j){
-        if(j == m) return 0;
-
-        if(i == n-1){
-            if(m-j > factory[i][1]) return LLONG_MAX/2;
-            LL res = 0;
-            for(int k = j; k < m; k++){
-                res += abs(robot[k] - factory[i][0]);
-            }
-            
-            return res;
-        }
-
-        if(memo[i][j] != -1) return memo[i][j];
-
-        LL res = helper(i+1, j); //  不選 -> 這次工場不製造任何機器人
-        LL k = 1, cost = 0;
-        while(k <= factory[i][1] && j + k - 1 < m){
-            cost += abs((LL)robot[j+k-1] - factory[i][0]);
-            res = min(res, cost + helper(i+1,j+k));
-            k++;
-        }
-
-        return memo[i][j] = res;
-    }
-};
-
-// 遞推 TC:O(nm^2) SC:O(m)
-class Solution {
-    using LL = long long;
-    int m, n;
-public:
-    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
-        m = robot.size();
-        n = factory.size();
-        sort(factory.begin(), factory.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0];
-        });
-        sort(robot.begin(), robot.end());
-        
-        vector<LL> dp(m+1, LLONG_MAX/2);
-        dp[0] = 0;
-        // n個工廠
-        for(int i = 0; i < n; i++){
-            // 處理m個robot
-            for(int j = m; j > 0; j--){
-                LL cost = 0;
-                for(int k = 1; k <= min(factory[i][1], j); k++){
-                    cost += abs((LL) robot[j-k] - factory[i][0]);
-                    dp[j] = min(dp[j], cost + dp[j-k]);
-                }
-            }
-        }
-
-        return dp[m];
-    }
-};
 
 /*
 這題突破點要先貪心的想到 每個機器人肯定盡量找這他近的 不可能特地跑遠的 然後讓下一個機器人跑到原本離前一個機器人近的
