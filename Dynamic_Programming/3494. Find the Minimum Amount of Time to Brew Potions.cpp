f10ea05b1bbeec59faf173b214f2d10c5a17e366
Nick Chen <s27206383@gmail.com>
2025-03-24 13:58:07 +0800

    Update 3494. Find the Minimum Amount of Time to Brew Potions.cpp

--- Dynamic_Programming/3494. Find the Minimum Amount of Time to Brew Potions.cpp
+++ Dynamic_Programming/3494. Find the Minimum Amount of Time to Brew Potions.cpp
@@ -7,12 +7,14 @@ public:
         vector<ll> dp(n, 0);
         for(int i = 0; i < m; i++) { 
             ll mx = 0; 
-            vector<ll> pre(n);
-            for(int j = 1; j < n; j++) {
-                pre[j] = pre[j-1] + (ll) skill[j-1] * mana[i]; 
-                mx = max(mx, dp[j] - pre[j]);
+            ll pre = 0;
+            // vector<ll> pre(n);
+            for(int j = 0; j < n; j++) {
+                //pre[j] = pre[j-1] + (ll) skill[j-1] * mana[i]; 
+                pre = pre + (j - 1 >= 0 ? (ll) skill[j-1] * mana[i] : 0); 
+                mx = max(mx, dp[j] - pre);
             }
-            mx = max(mx, dp[0]);
+            //mx = max(mx, dp[0]);
             dp[0] = mx + (ll) skill[0] * mana[i];
             for(int j = 1 ; j < n; j++) {
                 dp[j] = dp[j-1] + (ll) skill[j] * mana[i];
