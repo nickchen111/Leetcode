6a8bb93d52852b8d6c2e265cceee71f89b4f5bb1
Nick Chen <s27206383@gmail.com>
2025-03-25 22:15:35 +0800

    Create 2830. Maximize the Profit as the Salesman.py

---
+++ Dynamic_Programming/2830. Maximize the Profit as the Salesman.py
@@ -0,0 +1,14 @@
+# TC:O(n + m) SC:O(n)
+class Solution:
+    def maximizeTheProfit(self, n: int, offers: List[List[int]]) -> int:
+        groups = [[] for _ in range(n)]
+        for st, end, gold in offers:
+            groups[end].append((st,gold))
+        f = [0] * (n + 1)
+        for end, g in enumerate(groups):
+            f[end+1] = f[end] #不選
+            for start, gold in g:
+                if f[start] + gold > f[end+1]:
+                    f[end+1] = f[start] + gold
+        return f[n]
+
