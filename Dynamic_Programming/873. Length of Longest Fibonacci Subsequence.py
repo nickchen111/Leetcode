8ff4fa646df5a6a8b28a6ad18d999ff7871b7f98
Nick Chen <s27206383@gmail.com>
2025-03-31 22:19:23 +0800

    Update 873. Length of Longest Fibonacci Subsequence.py

--- Dynamic_Programming/873. Length of Longest Fibonacci Subsequence.py
+++ Dynamic_Programming/873. Length of Longest Fibonacci Subsequence.py
@@ -5,22 +5,20 @@ class Solution:
         只要第三個數字是由前兩個數字組合而成就可以算是本題的費氏數列的數字
         f[i][j] 以i, j 兩個數字作結尾可以組成的最長長度
         '''
-        n = len(nums)
+                n = len(nums)
         f = [[2] * n for _ in range(n)]
         ans = 0
         mp = defaultdict(int)
         for i in range(n):
             mp[nums[i]] = i
-        for i in range(n):
             for j in range(i+1, n):
                 diff = nums[j] - nums[i]
-                if diff in mp:
+                if diff in mp and mp[diff] < i:
                     index = mp[diff]
-                    if index < i:
-                        f[i][j] = max(f[i][j], f[index][i] + 1)
-                        ans = max(ans, f[i][j])
-        return ans
-        
-
+                    if f[index][i] + 1 > f[i][j]:
+                        f[i][j] = f[index][i] + 1
+                        if f[i][j] > ans:
+                            ans = f[i][j]
+        return ans if ans >= 3 else 0
 
 
