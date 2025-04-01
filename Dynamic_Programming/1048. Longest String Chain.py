15b4b7b82d6f2921cccc6384586fd6a4d27036ef
Nick Chen <s27206383@gmail.com>
2025-04-01 21:52:24 +0800

    Create 1048. Longest String Chain.py

---
+++ Dynamic_Programming/1048. Longest String Chain.py
@@ -0,0 +1,34 @@
+# TC:O(nlgn + n * n * k) SC:O(n * k)
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        '''
+        n^2 * k 大致上才能通過 問說最長長度
+        '''
+        n = len(words)
+        f = defaultdict(int)
+        words.sort(key=lambda x: len(x))
+        @cache
+        def dfs(i:int) -> int:
+            if i == n:
+                return 0
+            m =len(words[i])
+            ans = 1
+            for j in range(m):
+                s = words[i][:j] + words[i][j+1:]
+                if s in f:
+                    ans = max(ans, 1 + dfs(f[s]))
+            f[words[i]] = i
+            return ans
+        return max(dfs(i) for i in range(n))
+        '''
+        for word in words:
+            f[word] = 1
+            m = len(word)
+            for i in range(m):
+                s = word[:i] + word[i+1:]
+                if s in f and f[s] + 1 > f[word]:
+                    f[word] = f[s] + 1
+            if f[word] > ans:
+                ans = f[word]
+        return ans
+        '''
