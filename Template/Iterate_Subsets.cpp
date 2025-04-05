cbd392357398ebb61e09208810d928b6b0dd15c1
Nick Chen <s27206383@gmail.com>
2025-04-05 13:56:39 +0800

    Update Iterate_Subsets.cpp

--- Template/Iterate_Subsets.cpp
+++ Template/Iterate_Subsets.cpp
@@ -2,3 +2,14 @@ for (int subset=state; subset>0; subset=(subset-1)&state)
 {
    DoSomething(subset);
 }
+
+
+// 我比較常用的寫法
+int submask = mask;
+while (submask) {// 空集合改true
+   int remainging = submask ^ mask;
+   // 枚舉子集的子集...
+   submask = (submask - 1) & mask;
+   // 如果是包含空集合
+   if (submask == mask) break;
+}
