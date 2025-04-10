55d8f55334e2e77ed23ca3f3c763a3ea2155be9f
Nick Chen <s27206383@gmail.com>
2025-04-11 02:13:42 +0800

    Update 2999. Count the Number of Powerful Integers.py

--- Dynamic_Programming/2999. Count the Number of Powerful Integers.py
+++ Dynamic_Programming/2999. Count the Number of Powerful Integers.py
@@ -5,23 +5,26 @@ class Solution:
         就是數位DP
         '''
         l = len(s)
+        s_mod = pow(10, l)
+        s = int(s)
         #目前走到哪, 是否是數字，是否貼著下界, 程式中需要判斷剩下的字串是否要填上s了
         def countPowerful(num:int) -> int:
-            s1 = str(num)
+            s1 = list(map(int, str(num)))
             n = len(s1)
-            if l > n or (l == n and s1 < s): return 0
+            if l > n or (l == n and num < s): return 0
             @cache
             def dfs(i:int, is_num:bool, is_limit:bool) -> int:
                 if i == n - l:
-                    return 1 if not is_limit or s1[i:] >= s else 0
+                    return 1 if not is_limit or num % s_mod >= s else 0
                 ans = 0
                 if not is_num:
                     ans = dfs(i+1, False, False) #  不選
-                up = limit if not is_limit else min(limit, int(s1[i])) #最大值
+                up = limit if not is_limit else min(limit, s1[i]) #最大值
                 down = 1 if not is_num else 0
                 for j in range(down, up + 1):
-                    ans += dfs(i+1, True, is_limit and j == int(s1[i]))
+                    ans += dfs(i+1, True, is_limit and j == s1[i])
                 return ans
+            dfs.cache_clear()
             return dfs(0, False, True)
         return countPowerful(finish) - countPowerful(start - 1)
 
