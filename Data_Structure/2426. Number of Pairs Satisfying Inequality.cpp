509d1cf1c7336e544b89a3825b5e21d75334fd63
Nick Chen <s27206383@gmail.com>
2025-04-13 02:54:13 +0800

    Create 2426. Number of Pairs Satisfying Inequality.cpp

---
+++ Data_Structure/2426. Number of Pairs Satisfying Inequality.cpp
@@ -0,0 +1,64 @@
+// TC:O(nlgn) SC:O(n) 離散化BIT + 二分
+class BIT {
+private:
+    vector<int> tree;
+public:
+    BIT (int n) : tree(n) {}
+    void add (int x, int val) {
+        while (x < tree.size()) {
+            tree[x] += val;
+            x += x & -x;
+        }
+    }
+    int query (int x) {
+        int ans = 0;
+        while (x > 0) {
+            ans += tree[x];
+            x -= x & -x;
+        }
+        return ans;
+    }
+};
+class Solution {
+public:
+    long long numberOfPairs(vector<int>& a, vector<int>& nums2, int diff) {
+        /*
+        1. 樹狀數組  
+        2. 歸併排序  
+        3. 講怎麼做到 nums[i] <= 1e9
+
+        1. nums1[i] - nums1[j] <= nums2[i] - nums2[j] + diff  
+        2. nums1[i] - nums2[i] <= nums1[j] - nums2[j] + diff  
+        3. 設 a[i] = nums1[i] - nums2[i]  
+        4. a[i] <= a[j] + diff  
+        5. 從左到右遍歷 a[i]，統計每個元素的出現次數  
+            統計 <= a[i] + diff 的元素個數，就是答案要求的數對個數  
+                a[i] 加到統計的資料結構裡面  
+        6. 我們需要一個資料結構，能夠添加元素，並查詢 <= x 的元素個數  
+        7. 樹狀數組 線段樹 名次樹 Python SortedList
+
+        樹狀數組
+
+        1. 樹狀數組  
+        2. 線段樹的想法是，把區間表示成若干區間的並集  
+        樹狀數組的想法是，把區間表示成兩個前綴區間的差集  
+        前綴區間又可以表示成若干個區間的並集  
+        離散化概念是將數值轉到index上 並且重複的數值就對應相同index 來計算數量
+        */
+
+        int n = a.size();
+        for(int i = 0; i < n; i++) a[i] -= nums2[i];
+        vector<int> b = a;
+        sort(b.begin(), b.end());
+        b.erase(unique(b.begin(), b.end()), b.end());
+        int m = b.size();
+        // auto t = new BIT(m + 1);
+        BIT t(m + 1);
+        long long ans = 0L;
+        for (auto &x : a) {
+            ans += t.query(ranges::upper_bound(b, x + diff) - b.begin());
+            t.add(ranges::lower_bound(b, x) - b.begin() + 1, 1);
+        }
+        return ans;
+    }
+};
