aadaadb1f737d2b035110def7491f14dc2e95528
Nick Chen <s27206383@gmail.com>
2025-04-14 02:10:22 +0800

    Update 1483. Kth Ancestor of a Tree Node.cpp

--- Binary_Tree/1483. Kth Ancestor of a Tree Node.cpp
+++ Binary_Tree/1483. Kth Ancestor of a Tree Node.cpp
@@ -13,12 +13,10 @@ public:
         // 建造圖，如果要知道自己之前的parent 回頭traverse n^2
         next.resize(n);
         deep.resize(n);
-        power = log2(n);
-        stjump.resize(n, vector<int>(power+1));
-        for(int i = 0; i < parent.size(); i++) {
-            if(parent[i] != -1) {
-                next[parent[i]].push_back(i);
-            }
+        power = bit_width(unsigned(n));
+        stjump.resize(n, vector<int>(power));
+        for(int i = 1; i < n; i++) {
+            next[parent[i]].push_back(i); // pa -> child
         }
         dfs(0, 0);
         
@@ -27,7 +25,7 @@ public:
     int getKthAncestor(int node, int k) {
         if(deep[node] <= k) return -1;
         int s = deep[node] - k; // 想抵達的深度
-        for(int p = power; p >= 0; p--) {
+        for(int p = power-1; p >= 0; p--) {
             if(deep[stjump[node][p]] >= s) {
                 node = stjump[node][p];
             }
@@ -35,19 +33,19 @@ public:
 
         return node;
     }
-    int log2(int n) {
-        int ans = 0;
-        while((1<<ans) <= (n >> 1)) {
-            ans ++;
-        }
+    // int log2(int n) {
+    //     int ans = 0;
+    //     while((1<<ans) <= (n >> 1)) {
+    //         ans ++;
+    //     }
 
-        return ans;
-    }
+    //     return ans;
+    // }
     void dfs(int cur, int prev) {
-        if(cur == 0) deep[cur] = 1;
+        if(cur == 0) deep[cur] = 1; // 深度從1開始
         else deep[cur] = deep[prev] + 1;
-        stjump[cur][0] = prev;
-        for(int p = 1; (1 << p) <= deep[cur]; p++) {
+        stjump[cur][0] = prev; // 跳一步
+        for(int p = 1; (1 << p) < deep[cur]; p++) {
             stjump[cur][p] = stjump[stjump[cur][p-1]][p-1];
         }
         for(auto &nxt : next[cur]) {
