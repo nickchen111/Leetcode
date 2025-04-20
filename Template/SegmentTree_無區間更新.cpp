d1b8dda5deaa30cdb991707fa92f5b4c21267b75
Nick Chen <s27206383@gmail.com>
2025-04-20 15:45:39 +0800

    Update SegmentTree_無區間更新.cpp

--- Template/SegmentTree_無區間更新.cpp
+++ Template/SegmentTree_無區間更新.cpp
@@ -99,8 +99,3 @@ int main() {
     cout << t2.query(0, 7) << endl;
     return 0;
 }
-
-作者：灵茶山艾府
-链接：https://leetcode.cn/discuss/post/3583665/fen-xiang-gun-ti-dan-chang-yong-shu-ju-j-bvmv/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
