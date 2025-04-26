ee42fef8cc9020ce9b84e71855fc960015cc6c97
Nick Chen <s27206383@gmail.com>
2025-04-27 01:05:22 +0800

    Update 3530. Maximum Profit from Valid Topological Order in DAG.cpp

--- Dynamic_Programming/3530. Maximum Profit from Valid Topological Order in DAG.cpp
+++ Dynamic_Programming/3530. Maximum Profit from Valid Topological Order in DAG.cpp
@@ -1,5 +1,6 @@
 // TC:O(n * 2 ^ n) SC:O(2 ^ n)
 class Solution {
+    using ll = long long;
 public:
     long long maxProfit(int n, vector<vector<int>>& edges, vector<int>& score) {
         vector<int> pre(n, 0);
@@ -8,19 +9,18 @@ public:
             pre[v] |= (1 << u);
         }
         int N = 1 << n;
-        vector<long long> dp(N, LLONG_MIN / 2);
+        vector<ll> dp(N, LLONG_MIN/2);
         dp[0] = 0;
-        for (int mask = 0; mask < N; mask++) { // 2^22 * 22
-            if (dp[mask] == LLONG_MIN / 2) continue;
-            int k = __builtin_popcount(mask); // 之前已經用了多少個？
+        for (int mask = 0; mask < N; mask++) {
+            if (dp[mask] == LLONG_MIN/2) continue; // 沒辦法到達的條件
+            int k = __builtin_popcount(mask);
             for (int i = 0; i < n; i++) {
-                if (!(mask & (1 << i)) && ( (mask & pre[i]) == pre[i] )) {
-                    int nxt = mask | (1 << i);
-                    long long profit = dp[mask] + (long long)score[i] * (k + 1);
-                    dp[nxt] = max(dp[nxt], profit);
-                }
+                if(!((mask >> i) & 1) && (pre[i] & mask) == pre[i]) {
+                    dp[mask | (1 << i)] = max(dp[mask | (1 << i)], dp[mask] + (ll) score[i] * (k + 1));
+                } 
             }
         }
-        return dp[N - 1];
+        
+        return dp[N-1];
     }
 };
