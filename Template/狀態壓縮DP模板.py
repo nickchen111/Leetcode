840186bae4f9c07ecb3cfa63e72184aff8c34d9a
Nick Chen <s27206383@gmail.com>
2025-04-27 21:42:10 +0800

    Update 狀態壓縮DP模板.py

--- Template/狀態壓縮DP模板.py
+++ Template/狀態壓縮DP模板.py
@@ -24,3 +24,31 @@ class Solution:
                 if s >> (j - 1) & 1 and (i % j == 0 or j % i == 0):
                     f[s] += f[s ^ (1 << (j - 1))]
         return f[-1]
+
+# 枚舉子集的子集 LC. 3444
+class Solution:
+    def minimumIncrements(self, nums: List[int], target: List[int]) -> int:
+        # 预处理 target 的所有子集的 LCM
+        m = len(target)
+        lcms = [1] * (1 << m)
+        for i, t in enumerate(target):
+            bit = 1 << i
+            for mask in range(bit):
+                lcms[bit | mask] = lcm(t, lcms[mask])
+
+        @cache
+        def dfs(i: int, j: int) -> int:
+            if j == 0:
+                return 0
+            if i < 0: 
+                return inf
+            # 不修改 nums[i]
+            res = dfs(i - 1, j)
+            # 枚举 j 的所有非空子集 sub，把 nums[i] 改成 lcms[sub] 的倍数
+            sub = j
+            while sub:
+                l = lcms[sub]
+                res = min(res, dfs(i - 1, j ^ sub) + (l - nums[i] % l) % l)
+                sub = (sub - 1) & j
+            return res
+        return dfs(len(nums) - 1, (1 << m) - 1)
