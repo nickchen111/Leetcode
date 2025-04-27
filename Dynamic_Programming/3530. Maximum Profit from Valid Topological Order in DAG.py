d9211b23a35f1ab387e2df30f0c913b45c9c36c0
Nick Chen <s27206383@gmail.com>
2025-04-27 16:32:10 +0800

    Update 3530. Maximum Profit from Valid Topological Order in DAG.py

--- Dynamic_Programming/3530. Maximum Profit from Valid Topological Order in DAG.py
+++ Dynamic_Programming/3530. Maximum Profit from Valid Topological Order in DAG.py
@@ -1,32 +1,40 @@
 # TC:O(n * 2 ^ n) SC:O(2 ^ n)
 class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        path = [0] * n
-        ans = []
-        on_path = 0
-        def dfs(i:int):
-            nonlocal on_path
-            if i == n:
-                ans.append(path.copy())
-                return
-            for j in range(n):
-                if (on_path >> j) & 1:
-                    continue
-                path[i] = nums[j]
-                on_path |= (1 << j)
-                dfs(i + 1)
-                on_path ^= (1 << j)
-        dfs(0)
-        return ans
+    def maxProfit(self, n: int, edges: list[list[int]], score: list[int]) -> int:
+        if not edges:
+            score.sort()
+            return sum(s * i for i,s in enumerate(score,1))
+        pre = [0] * n
+        for e in edges:
+            u, v = e[0], e[1]
+            pre[v] |= (1 << u)
+        
+        N = 1 << n
+        dp = [float('-inf')] * N
+        dp[0] = 0
+       
+        for mask in range(N):
+            if dp[mask] == float('-inf'):
+                continue
+            
+            k = bin(mask).count('1')
+            for i in range(n):
+                if not (mask & (1 << i)) and ((mask & pre[i]) == pre[i]):
+                    nxt = mask | (1 << i)
+                    profit = dp[mask] + score[i] * (k + 1)
+                    dp[nxt] = max(dp[nxt], profit)
+       
+        return dp[N - 1]
         '''
-        def dfs(i:int, s:Set[int]):
-            if i == n:
-                ans.append(path.copy())
-                return
-            for x in s:
-                path[i] = x
-                dfs(i + 1, s - {x})
-        dfs(0, set(nums))
-        return ans
+        @cache
+        def dfs(s:int) -> int:
+            ans = 0
+            i = s.bit_count() + 1
+            for j, p in enumerate(pre):
+                if ((s >> j) & 1) == 0 and (s | p) == s:
+                    r = dfs(s | (1 << j)) + score[j] * i
+                    if r > ans:
+                        ans = r
+            return ans
+        return dfs(0)
         '''
