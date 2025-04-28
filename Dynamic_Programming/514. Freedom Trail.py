12210020db6531a3a79cde1378cd58495b6f98b1
Nick Chen <s27206383@gmail.com>
2025-04-29 01:37:05 +0800

    Update 514. Freedom Trail.py

--- Dynamic_Programming/514. Freedom Trail.py
+++ Dynamic_Programming/514. Freedom Trail.py
@@ -66,4 +66,52 @@ class Solution:
                         vis[i2][j] = True
                         q.append((i2, j))
             step += 1
+# DP 二分預處理做法 較慢
+class Solution:
+    def findRotateSteps(self, ring: str, key: str) -> int:
+        '''
+        問說最少需要幾次才能拼湊出 key string 那就是順時針逆時針都檢查一次
+        每一個位置到達他前後不同字都需要轉幾次
+        要如何預處理每個位置可以走到哪些其他char, 其實不用預處理也可過
+        需要這樣的資料結構 pos, 26個字母最近的兩個順時針跟逆時針是哪兩個
+        可以有種結構是 某個字母出現的位置們 然後對他二分
+        並且需要判斷這個字母是否是需要的
+        '''
+        n = len(ring)
+        needed = set(key)
+        ch_pos = {ch: [] for ch in needed}
+        for i, ch in enumerate(ring):
+            if ch in needed:
+                ch_pos[ch].append(i)
+        pos_ch = [
+            {ch: [-1, -1] for ch in needed}
+            for _ in range(n)
+        ]
+        # print(pos_ch)
+        for i in range(n):
+            for ch in needed:
+                lst = ch_pos[ch]
+                idx = bisect_left(lst, i)
+                cw = lst[idx] if idx < len(lst) else lst[0]
+                ccw = lst[idx-1] if idx > 0 else lst[-1]
+                pos_ch[i][ch][0] = ccw
+                pos_ch[i][ch][1] = cw
+
+        @cache
+        def dfs(key_i: int, ring_pos: int) -> int:
+            if key_i == len(key):
+                return 0
+            target = key[key_i]
+            best = inf
+            for nbr in pos_ch[ring_pos][target]:
+                diff = abs(nbr - ring_pos)
+                step = min(diff, n - diff)
+                cost = step + 1 + dfs(key_i + 1, nbr)
+                if cost < best:
+                    best = cost
+            return best
+        return dfs(0, 0)
+
+
+        
 
