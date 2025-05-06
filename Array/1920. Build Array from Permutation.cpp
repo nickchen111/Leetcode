ed2a9944ae842518f48d12cef99eeacdaf259e7d
Nick Chen <s27206383@gmail.com>
2025-05-06 17:30:17 +0800

    Update 1920. Build Array from Permutation.cpp

--- Array/1920. Build Array from Permutation.cpp
+++ Array/1920. Build Array from Permutation.cpp
@@ -2,10 +2,6 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        /*
-        index sorting!? 他在將數字做位置的轉換
-        也就是index = 0 該位置要放nums[nums[0]] 的數字
-        */
         vector<int> ans;
         for (int i = 0; i < nums.size(); i++) ans.emplace_back(nums[nums[i]]);
         return ans;
