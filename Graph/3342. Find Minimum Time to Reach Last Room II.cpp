052911c9dd060da2ce85cc99bbc20be4ade8aab0
Nick Chen <s27206383@gmail.com>
2025-05-07 22:36:21 +0800

    Update 3342. Find Minimum Time to Reach Last Room II.cpp

--- Graph/3342. Find Minimum Time to Reach Last Room II.cpp
+++ Graph/3342. Find Minimum Time to Reach Last Room II.cpp
@@ -2,42 +2,41 @@
 3342. Find Minimum Time to Reach Last Room II
 */
 
-// TC:O(2*n*m* lg(2*n*m)) SC:O(2*n*m)
+// TC:O(n*m* lg(n*m)) SC:O(n*m)
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        int n = moveTime.size();
-        int m = moveTime[0].size();
-        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq; // 使用時間, 座標x,y, 上次跳的時間
-        
-        pq.push({0,0,0,0});
-        
-        vector<vector<vector<int>>> dist(n, vector<vector<int>>(m, vector<int>(3, INT_MAX/2)));
-        dist[0][0][0] = 0;
+        /*
+        從左上移動到右下 最少需要多少時間 Dijkstra 
+        每動一次會根據上次的時間改變這次要移動的時間 1 2 1 2 交替
+        那要如何設計呢?
+        首先一樣會有一個array 紀錄當前走到該點的最小時間
+        PQ部分會有四個變量 x, y, 這次要交替的時間，抵達該點時間
+        演算法內部在跑的過程會是根據目前可走的點去動態更新最大時間 然後再後面重新跑一次
+        將可以被納入的點加入PQ
+        TC 會是 2mnlgmn
+        */
+        int m = moveTime.size(), n = moveTime[0].size();
+        vector<vector<int>> minCost(m, vector<int>(n, INT_MAX/2));
+        priority_queue<array<int,3>, vector<array<int,3>>, greater<>> pq;
+        pq.push({0,0,0});
         vector<int> dirs = {0,1,0,-1,0};
         while(!pq.empty()) {
-            vector<int> tmp = pq.top();
+            auto [c, x, y] = pq.top();
+            int step = (x + y) % 2 + 1;
             pq.pop();
-            int curTime = tmp[0];
-            int x = tmp[1];
-            int y = tmp[2];
-            if(x == n-1 && y == m-1) return curTime;
-            int curLast = tmp[3];
-            if(dist[x][y][curLast] < curTime) continue;
-            dist[x][y][curLast] = curTime;
-            for(int i = 1; i < dirs.size(); i++) {
-                int nx = dirs[i-1] + x;
-                int ny = dirs[i] + y;
-                int nextLast = (curLast == 0 || curLast == 2) ? 1 : 2;
-                if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                int nextTime = max(moveTime[nx][ny], curTime) + nextLast;
-                if(dist[nx][ny][nextLast] > nextTime) {
-                    dist[nx][ny][nextLast] = nextTime;
-                    pq.push({nextTime, nx, ny, nextLast});
-                }
+            if (x == m - 1 && y == n - 1) return c;
+            if (minCost[x][y] <= c) continue;
+            minCost[x][y] = c;
+            for (int k = 1; k < dirs.size(); k++) {
+                int nx = x + dirs[k-1];
+                int ny = y + dirs[k];
+                int nxtStep = 3 - step;
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n || minCost[nx][ny] != INT_MAX/2) continue;
+                int nc = max(c, moveTime[nx][ny])  + step;
+                pq.push({nc, nx, ny});
             }
         }
-        
         return -1;
     }
 };
