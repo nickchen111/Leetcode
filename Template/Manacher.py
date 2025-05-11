e315f0ce2a00f4b4ba3173736e84537d4d861d6f
Nick Chen <s27206383@gmail.com>
2025-05-12 02:42:35 +0800

    Update Manacher.py

--- Template/Manacher.py
+++ Template/Manacher.py
@@ -43,8 +43,3 @@ class Solution:
         # 所以要对应到 s，最长回文子串的下标是从 max_i-hl+2 到 max_i+hl-2
         # 结合上文的下标转换关系，得到其在 s 上的下标范围是从 (max_i-hl)/2 到 (max_i+hl)/2-2
         return s[(max_i - hl) // 2: (max_i + hl) // 2 - 1]
-
-作者：灵茶山艾府
-链接：https://leetcode.cn/problems/longest-palindromic-substring/solutions/2958179/mo-ban-on-manacher-suan-fa-pythonjavacgo-t6cx/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
