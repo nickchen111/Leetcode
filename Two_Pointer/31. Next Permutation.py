9e4af2a07b722ee9f78e7e708aeb4de507c9394e
Nick Chen <s27206383@gmail.com>
2025-05-31 17:40:08 +0800

    Create 31. Next Permutation.py

---
+++ Two_Pointer/31. Next Permutation.py
@@ -0,0 +1,18 @@
+# TC:O(nlgn) SC:O(1)
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        if i == -1:
+            nums.sort()
+            return nums
+        j = len(nums) - 1
+        while nums[j] <= nums[i]:
+            j -= 1
+        nums[i], nums[j] = nums[j], nums[i]
+        nums[i + 1:] = reversed(nums[i + 1:]) 
+        return nums
