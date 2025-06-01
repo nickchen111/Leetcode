347ffffdab1fe10456e3b247aad5c21bcecbb946
Nick Chen <s27206383@gmail.com>
2025-06-01 16:24:25 +0800

    Update Segment_Tree_lazy.py

--- Template/Segment_Tree_lazy.py
+++ Template/Segment_Tree_lazy.py
@@ -1,3 +1,81 @@
+# 實際調用
+class Node:
+    __slots__ = 'val', 'todo'
+
+class LazySegmentTree:
+    _TODO_INIT = 0 
+    
+    def __init__(self, n: int, init_val=0):
+        if isinstance(init_val, int):
+            init_val = [init_val] * n
+        self._n = n
+        self._tree = [Node() for _ in range(2 << (n - 1).bit_length())]
+        self._build(init_val, 1, 0, n - 1)
+
+    def _merge_val(self, a: int, b: int) -> int:
+        return a + b 
+        
+    def _merge_todo(self, a: int, b: int) -> int:
+        return a + b 
+        
+    def _apply(self, node: int, l: int, r: int, todo: int) -> None:
+        cur = self._tree[node]
+        cur.val += todo * (r - l + 1)
+        cur.todo = self._merge_todo(todo, cur.todo)
+        
+    def _spread(self, node: int, l: int, r: int) -> None:
+        todo = self._tree[node].todo
+        if todo == self._TODO_INIT: 
+            return
+        m = (l + r) // 2
+        self._apply(node * 2, l, m, todo)
+        self._apply(node * 2 + 1, m + 1, r, todo)
+        self._tree[node].todo = self._TODO_INIT 
+    def _maintain(self, node: int) -> None:
+        self._tree[node].val = self._merge_val(self._tree[node * 2].val, self._tree[node * 2 + 1].val)
+        
+    def _build(self, a: List[int], node: int, l: int, r: int) -> None:
+        self._tree[node].todo = self._TODO_INIT
+        if l == r:
+            self._tree[node].val = a[l]
+            return
+        m = (l + r) // 2
+        self._build(a, node * 2, l, m)
+        self._build(a, node * 2 + 1, m + 1, r)
+        self._maintain(node)
+
+    def _update(self, node: int, l: int, r: int, ql: int, qr: int, f: int) -> None:
+        if ql <= l and r <= qr:
+            self._apply(node, l, r, f)
+            return
+        self._spread(node, l, r)
+        m = (l + r) // 2
+        if ql <= m: 
+            self._update(node * 2, l, m, ql, qr, f)
+        if qr > m:
+            self._update(node * 2 + 1, m + 1, r, ql, qr, f)
+        self._maintain(node)
+
+    def _query(self, node: int, l: int, r: int, ql: int, qr: int) -> int:
+        if ql <= l and r <= qr:
+            return self._tree[node].val
+        self._spread(node, l, r)
+        m = (l + r) // 2
+        if qr <= m: 
+            return self._query(node * 2, l, m, ql, qr)
+        if ql > m: 
+            return self._query(node * 2 + 1, m + 1, r, ql, qr)
+        l_res = self._query(node * 2, l, m, ql, qr)
+        r_res = self._query(node * 2 + 1, m + 1, r, ql, qr)
+        return self._merge_val(l_res, r_res)
+        
+    def update(self, ql: int, qr: int, f: int) -> None:
+        self._update(1, 0, self._n - 1, ql, qr, f)
+        
+    def query(self, ql: int, qr: int) -> int:
+        return self._query(1, 0, self._n - 1, ql, qr)
+        
+# 講解 入門 線段樹模板
 class Solution:
     def handleQuery(self, nums1: List[int], nums2: List[int], queries: List[List[int]]) -> List[int]:
         '''
