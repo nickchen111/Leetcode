d493a5410aea6ffaf776a82c948a603d6cee54f8
Nick Chen <s27206383@gmail.com>
2025-06-08 16:31:29 +0800

    Update 3572. Maximize Y‑Sum by Picking a Triplet of Distinct X‑Values.py

--- Array/3572. Maximize Y‑Sum by Picking a Triplet of Distinct X‑Values.py
+++ Array/3572. Maximize Y‑Sum by Picking a Triplet of Distinct X‑Values.py
@@ -4,15 +4,20 @@ class Solution:
         '''
         選x都不相同的三個數字 然後挑出最大的三個可以選哪些
         '''
-        mp = defaultdict(list)
+        mp = defaultdict(lambda:-inf) # 這樣可以讓預設值為-inf 不要給參數 無參lambda
+        for num1, num2, in zip(x, y):
+            mp[num1] = max(mp[num1], num2)
+        if len(mp) < 3:
+            return -1
+        return sum(nlargest(3, mp.values()))
+        '''
         for i, num in enumerate(x):
             mp[num].append(i)
         mx, mx2, mx3 = 0, 0, 0
         for key, arr in mp.items():
             tmp = 0
             for idx in arr:
-                if y[idx] > tmp:
-                    tmp = y[idx]
+                tmp = max(tmp, y[idx])
             if tmp >= mx:
                 mx2, mx, mx3 = mx, tmp, mx2
             elif tmp >= mx2:
@@ -20,3 +25,4 @@ class Solution:
             elif tmp > mx3:
                 mx3 = tmp
         return mx + mx2 + mx3 if mx != 0 and mx2 != 0 and mx3 != 0 else -1
+        '''
