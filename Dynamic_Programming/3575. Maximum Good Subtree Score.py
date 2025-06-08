1178d5efa105f6071d833ce1fd7b9fb67fe097e6
Nick Chen <s27206383@gmail.com>
2025-06-08 16:25:55 +0800

    Update 3575. Maximum Good Subtree Score.py

--- Dynamic_Programming/3575. Maximum Good Subtree Score.py
+++ Dynamic_Programming/3575. Maximum Good Subtree Score.py
@@ -1,4 +1,4 @@
-# TC:O(n * (2^m * 2^m + m)) m = 10 SC:O(n + 2^m)
+# TC:O(n * (3^m + U)) m = 10 U = 數值十進制長度 SC:O(n + n * 2^m)
 class Solution:
     def goodSubtreeSum(self, vals: List[int], par: List[int]) -> int:
         '''
@@ -13,33 +13,35 @@ class Solution:
                 g[p].append(i)
         MOD = 10 ** 9 + 7
         def dfs(u:int) -> List[int]:
-            ans = [0] * (1 << 10)
+            # ans = [0] * (1 << 10)
+            ans = defaultdict(int)
+            ans[0] = 0
             # 找出目前有哪些元素
             ele = 0
             val = vals[u]
             while val:
-                if (ele >> (val % 10)) & 1: # 如果有相同元素 那麼根節點不選
-                    ele = 0
+                val,d = divmod(val, 10)
+                if (ele >> d) & 1: # 如果有相同元素 那麼根節點不選
                     break
                 else:
-                    ele |= (1 << (val % 10))
-                val //= 10
-            if ele:
+                    ele |= (1 << d)
+            else:
                 ans[ele] = vals[u]
             for v in g[u]: # 根據後面的子節點判斷目前數字扣掉前面那些的可以拼湊出的最大值是多少
                 nxt = dfs(v)
-                # 將之前元素的我當前有的元素扣掉 bit來說... ~
-                new_ans = ans[:]
-                for mask1 in range(1 << 10):
-                    if ans[mask1] == 0 and mask1 != 0: continue
-                    for mask2 in range(1 << 10):
-                        if nxt[mask2] == 0 and mask2 != 0: continue
+                # new_ans = ans[:]
+                new_ans = ans.copy()
+                # for mask1 in range(1 << 10):
+                for mask1, s in ans.items():
+                    # if ans[mask1] == 0 and mask1 != 0: continue
+                    # for mask2 in range(1 << 10):
+                    for mask2, s2 in nxt.items():
+                        # if nxt[mask2] == 0 and mask2 != 0: continue
                         if (mask1 & mask2) == 0:
                             new_mask = mask1 | mask2
                             new_ans[new_mask] = max(new_ans[new_mask], ans[mask1] + nxt[mask2])
-                ans = new_ans
-            maxScores[u] = max(ans)
+                ans = new_ans.copy()
+            maxScores[u] = max(ans.values())
             return ans
         dfs(0)
         return sum(x for x in maxScores) % MOD
-        
