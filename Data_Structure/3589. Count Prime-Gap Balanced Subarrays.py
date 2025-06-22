67ce40d1266bbf3fa99d4cc0eb1aaa990edec233
Nick Chen <s27206383@gmail.com>
2025-06-22 17:12:40 +0800

    Update 3589. Count Prime-Gap Balanced Subarrays.py

--- Data_Structure/3589. Count Prime-Gap Balanced Subarrays.py
+++ Data_Structure/3589. Count Prime-Gap Balanced Subarrays.py
@@ -8,29 +8,28 @@ for i in range(2, int(BASE ** 0.5) + 1):
 class Solution:
     def primeSubarray(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        ans = 0
-        l = 0
-        prime_positions = deque()
+        ans = left = 0
+        # prime_positions = deque() # 或者可以寫直接紀錄後兩個位置的參數
+        last = last2 = -1
         max_q = deque()
         min_q = deque()
-
         for r, x in enumerate(nums):
             if is_prime[x]:
-                prime_positions.append(r)
+                # prime_positions.append(r)
+                last2 = last
+                last = r
                 while max_q and nums[max_q[-1]] < x:
                     max_q.pop()
                 max_q.append(r)
                 while min_q and nums[min_q[-1]] > x:
                     min_q.pop()
                 min_q.append(r)
-
-            while len(prime_positions) >= 2 and nums[max_q[0]] - nums[min_q[0]] > k:
-                l = prime_positions.popleft() + 1
-                while max_q and max_q[0] < l:
-                    max_q.popleft()
-                while min_q and min_q[0] < l:
-                    min_q.popleft()
-            if len(prime_positions) >= 2:
-                ans += prime_positions[-2] - l + 1  
-
+                while nums[max_q[0]] - nums[min_q[0]] > k:
+                    # l = prime_positions.popleft() + 1
+                    left += 1
+                    while max_q and max_q[0] < left:
+                        max_q.popleft()
+                    while min_q and min_q[0] < left:
+                        min_q.popleft()
+            ans += last2 - left + 1  
         return ans
