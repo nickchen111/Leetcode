8c3130c97361c85610c2a400920f938d4212a8b9
Nick Chen <s27206383@gmail.com>
2025-07-20 22:30:00 +0800

    Update 3620. Network Recovery Pathways.py

--- Graph/3620. Network Recovery Pathways.py
+++ Graph/3620. Network Recovery Pathways.py
@@ -1,4 +1,36 @@
-# TC:O(ElogElogU) SC:O(E)
+# DP TC:O((n + m) * logU) 其中 n 是 online 的長度，m 是 edges 的長度 U 為邊權最大值 SC:O(n + m)
+class Solution:
+    def findMaxPathScore(self, edges: List[List[int]], online: List[bool], k: int) -> int:
+        n = len(online)
+        g = [[] for _ in range(n)]
+        max_wt = 0
+        for u, v, cost in edges:
+            if online[u] and online[v]:
+                g[u].append((v, cost))
+                max_wt = max(max_wt, cost)
+
+        def check(min_cost: int) -> bool:
+            @cache
+            def dfs(x:int) -> int:
+                if x == n - 1:
+                    return 0
+                ans = inf
+                for y, cost in g[x]:
+                    if cost >= min_cost:
+                        ans = min(ans, dfs(y) + cost)
+                return ans
+            return dfs(0) <= k
+
+        left, right = -1, max_wt
+        while left < right:
+            mid = (left + right + 1) // 2
+            if check(mid):
+                left = mid
+            else:
+                right = mid - 1
+        return left
+
+# Dijkstra TC:O((n+m)logmlogU) SC:O(n + m)
 class Solution:
     def findMaxPathScore(self, edges: List[List[int]], online: List[bool], k: int) -> int:
         n = len(online)
