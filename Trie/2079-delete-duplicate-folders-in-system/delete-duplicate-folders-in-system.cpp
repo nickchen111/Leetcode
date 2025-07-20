91e5d71c9d54ef95dd27e1cd8bd84fa9269c87cb
Nick Chen <s27206383@gmail.com>
2025-07-20 18:32:09 +0800

    Time: 177 ms (85.92%) | Memory: 263.5 MB (76.06%) - LeetSync

--- Trie/2079-delete-duplicate-folders-in-system/delete-duplicate-folders-in-system.cpp
+++ Trie/2079-delete-duplicate-folders-in-system/delete-duplicate-folders-in-system.cpp
@@ -1,68 +1,79 @@
+struct TrieNode {
+    unordered_map<string, TrieNode*> son;
+    string name; // 文件夹名称
+    bool deleted = false; // 删除标记
+};
+
 class Solution {
-    struct TreeNode{
-        string val;
-        TreeNode(string s): val(s){}
-        map<string, TreeNode*> next;
-    };
-    TreeNode* root;
-    unordered_map<string, int> key2Count;
-    unordered_map<string, int> key2Id;
-    unordered_map<TreeNode*, string> node2Key;
-    vector<vector<string>> res;
 public:
     vector<vector<string>> deleteDuplicateFolder(vector<vector<string>>& paths) {
-        root = new TreeNode("/");
-        for(auto path : paths){
-            TreeNode* node = root;
-            for(auto x : path){
-                if(node->next.find(x) == node->next.end()) node->next[x] = new TreeNode(x);
-                node = node->next[x];
+        TrieNode* root = new TrieNode();
+        for (auto& path : paths) {
+            // 把 path 插到字典树中，见 208. 实现 Trie
+            TrieNode* cur = root;
+            for (auto& s : path) {
+                if (!cur->son.contains(s)) {
+                    cur->son[s] = new TrieNode();
+                }
+                cur = cur->son[s];
+                cur->name = s;
             }
         }
 
-        getId(root);
+        unordered_map<string, TrieNode*> expr_to_node; // 子树括号表达式 -> 子树根节点
 
-        vector<string> track;
-        DFS(root, track);
+        auto gen_expr = [&](this auto&& gen_expr, TrieNode* node) -> string {
+            if (node->son.empty()) { // 叶子
+                return node->name; // 表达式就是文件夹名
+            }
 
-        return res;
-    }
-    int getId(TreeNode* node){
-        if(node == NULL) return 0;
-        string key;
+            vector<string> expr;
+            for (auto& [_, son] : node->son) {
+                // 每个子树的表达式外面套一层括号
+                expr.emplace_back("(" + gen_expr(son) + ")");
+            }
+            ranges::sort(expr);
 
-        for(auto x : node->next){
-            TreeNode* child = x.second;
-            key += to_string(getId(child)) + "#" + child->val + "#";
+            string sub_tree_expr;
+            for (auto& e : expr) {
+                sub_tree_expr += e; // 按字典序拼接所有子树的表达式
+            }
+
+            if (expr_to_node.contains(sub_tree_expr)) { // 哈希表中有 sub_tree_expr，说明有重复的文件夹
+                expr_to_node[sub_tree_expr]->deleted = true; // 哈希表中记录的节点标记为删除
+                node->deleted = true; // 当前节点标记为删除
+            } else {
+                expr_to_node[sub_tree_expr] = node;
+            }
+
+            return node->name + sub_tree_expr;
+        };
+
+        for (auto& [_, son] : root->son) {
+            gen_expr(son);
         }
 
-        node2Key[node] = key;
-        key2Count[key] += 1;
-        if(key2Count[key] == 1){
-            key2Id[key] = key2Id.size() + 1;
+        vector<vector<string>> ans;
+        vector<string> path;
+
+        // 在字典树上回溯，仅访问未被删除的节点，并将路径记录到答案中
+        // 类似 257. 二叉树的所有路径
+        auto dfs = [&](this auto&& dfs, TrieNode* node) -> void {
+            if (node->deleted) {
+                return;
+            }
+            path.push_back(node->name);
+            ans.push_back(path);
+            for (auto& [_, son] : node->son) {
+                dfs(son);
+            }
+            path.pop_back(); // 恢复现场
+        };
+
+        for (auto& [_, son] : root->son) {
+            dfs(son);
         }
 
-        return key2Id[key];
-    }
-
-    void DFS(TreeNode* node, vector<string>& track){
-        
-        string key = node2Key[node];
-        if(key != "" && key2Count[key] >= 2){
-            return;
-        }
-
-        if(node->val != "/"){
-            track.push_back(node->val);
-            res.push_back(track);
-        }
-
-        for(auto x : node->next){
-            DFS(x.second, track);
-        }
-
-        if(node->val != "/"){
-            track.pop_back();
-        }
+        return ans;
     }
 };
