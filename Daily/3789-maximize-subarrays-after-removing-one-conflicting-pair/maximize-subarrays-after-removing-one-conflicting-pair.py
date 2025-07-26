41bc952b3e26a0f4256bd227b40267308824fc3e
Nick Chen <s27206383@gmail.com>
2025-07-26 17:25:32 +0800

    Time: 716 ms (63.06%) | Memory: 87.2 MB (59.46%) - LeetSync

--- Daily/3789-maximize-subarrays-after-removing-one-conflicting-pair/maximize-subarrays-after-removing-one-conflicting-pair.py
+++ Daily/3789-maximize-subarrays-after-removing-one-conflicting-pair/maximize-subarrays-after-removing-one-conflicting-pair.py
@@ -5,20 +5,14 @@ class Solution:
             if a > b:
                 a, b = b, a
             groups[a].append(b)
-        max_extra = extra = ans = 0
-        b0 = b1 = n + 1
-        for i in range(n, 0, -1):
-            pre_b0 = b0
-            for b in groups[i]:
-                if b <= b0:
-                    b0, b1 = b, b0
-                elif b < b1:
-                    b1 = b
-            ans += b0 - i
-            if pre_b0 != b0:
-                extra = 0
-            extra += b1 - b0
-            max_extra = max(max_extra, extra)
-        return ans + max_extra
 
+        ans = 0
+        extra = [0] * (n + 2)
+        b = [n + 1, n + 1]
+        for i in range(n, 0, -1):
+            b = sorted(b + groups[i])[:2]  # 维護最小 b 和次小 b
+            ans += b[0] - i
+            extra[b[0]] += b[1] - b[0]
+
+        return ans + max(extra)
 
