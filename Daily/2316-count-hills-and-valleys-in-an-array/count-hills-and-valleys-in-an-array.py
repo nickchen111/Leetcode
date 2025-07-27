b3e6f3e7d24255f3f852230628bbddd212b1a47d
Nick Chen <s27206383@gmail.com>
2025-07-27 21:57:10 +0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (64.58%) - LeetSync

---
+++ Daily/2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        n = len(nums)
+        i = 1
+        ans = 0
+        while i < n:
+            j = i + 1
+            while j < n and nums[j] == nums[i]:
+                j += 1
+            if j < n and ((nums[i] > nums[i - 1] and nums[i] > nums[j]) or (nums[i] < nums[i - 1] and nums[i] < nums[j])):
+                ans += 1
+            i = j
+        return ans
