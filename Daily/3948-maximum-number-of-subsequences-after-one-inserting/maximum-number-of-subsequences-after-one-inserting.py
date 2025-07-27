fa7726743571d35d0d44f4f5042127a07396eaab
Nick Chen <s27206383@gmail.com>
2025-07-27 22:28:14 +0800

    Time: 1341 ms (5.05%) | Memory: 18.3 MB (82.13%) - LeetSync

--- Daily/3948-maximum-number-of-subsequences-after-one-inserting/maximum-number-of-subsequences-after-one-inserting.py
+++ Daily/3948-maximum-number-of-subsequences-after-one-inserting/maximum-number-of-subsequences-after-one-inserting.py
@@ -3,13 +3,21 @@ class Solution:
         n, m = len(s), len(t)
         if n < m:
             return 0
-        dp = [[0] * (m + 1) for _ in range(n + 1)]
-        dp[0][0] = 1
+        dp = [1] + [0] * m
         for i, x in enumerate(s):
-            dp[i + 1][0] = 1
+            prev = dp[0]
             for j, y in enumerate(t):
-                dp[i + 1][j + 1] = dp[i][j] + dp[i][j + 1] if x == y else dp[i][j + 1]
-        return dp[-1][-1]
+                tmp = dp[j+1]
+                dp[j+1] = prev + dp[j+1] if x == y else dp[j+1]
+                prev = tmp
+        return dp[m]
+        # dp = [[0] * (m + 1) for _ in range(n + 1)]
+        # dp[0][0] = 1
+        # for i, x in enumerate(s):
+        #     dp[i + 1][0] = 1
+        #     for j, y in enumerate(t):
+        #         dp[i + 1][j + 1] = dp[i][j] + dp[i][j + 1] if x == y else dp[i][j + 1]
+        # return dp[-1][-1]
     def calcInsertC(self, s:str) -> int:
         t = s.count('T')
         l = ans = 0
