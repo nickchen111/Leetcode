385451c944158b5013822828c998e57ccc19f166
Nick Chen <s27206383@gmail.com>
2025-07-31 22:29:52 +0800

    Time: 572 ms (8.93%) | Memory: 43.6 MB (88.69%) - LeetSync

--- Daily/934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.py
+++ Daily/934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.py
@@ -2,12 +2,22 @@ class Solution:
     def subarrayBitwiseORs(self, nums: List[int]) -> int:
         n = len(nums)
         st = set()
-        for i in range(n):
-            st.add(nums[i])
-            for j in range(i - 1, -1, -1):
-                if nums[j] | nums[i] == nums[j]:
-                    break
-                nums[j] |= nums[i]
-                st.add(nums[j])
+        ors = []
+        for i in range(n - 1, -1, -1): # 固定右端點 去看左端點可以到哪
+            ors.append([nums[i], i])
+            ors[0][0] |= nums[i]
+            k = 0
+            for j in range(1, len(ors)):
+                ors[j][0] |= nums[i]
+                if ors[j][0] == ors[k][0]:
+                    ors[k][1] = ors[j][1]
+                else:
+                    k += 1
+                    ors[k] = ors[j]
+            ors = ors[:k + 1]
+            for num, idx in ors:
+                st.add(num)
         return len(st)
+
+
             
