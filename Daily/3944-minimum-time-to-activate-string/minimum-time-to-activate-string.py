7248f1f5cd281f14928a5bded6dedc344c618d3b
Nick Chen <s27206383@gmail.com>
2025-08-03 22:17:33 +0800

    Time: 829 ms (0.00%) | Memory: 29.2 MB (50.00%) - LeetSync

--- Daily/3944-minimum-time-to-activate-string/minimum-time-to-activate-string.py
+++ Daily/3944-minimum-time-to-activate-string/minimum-time-to-activate-string.py
@@ -4,12 +4,26 @@ class Solution:
         total = n * (n + 1) // 2
         if total < k:
             return -1
-
-        def check(t):
+        star = [0] * n
+        def check(m:int):
+            m += 1
+            for i in range(m):
+                star[order[i]] = m
+            cnt = 0
+            last = -1
+            for i, x in enumerate(star):
+                if x == m:
+                    last = i
+                cnt += last + 1
+                if cnt >= k:
+                    return True
+            return False
+            '''
+            容斥
             star_pos = order[:t+1]
             star_pos.sort()
             star_pos = [-1] + star_pos + [n]
-
+            
             clean = 0
             for i in range(1, len(star_pos)):
                 l = star_pos[i-1]
@@ -18,6 +32,7 @@ class Solution:
                 if len_clean > 0:
                     clean += len_clean * (len_clean + 1) // 2
             return total - clean >= k
+            '''
         lo, hi = 0, n - 1
         ans = -1
         while lo < hi:
