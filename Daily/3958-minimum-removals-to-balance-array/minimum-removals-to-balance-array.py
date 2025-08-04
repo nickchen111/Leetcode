45ae480dba0b5fea26d32c77c14e9ebe804135f5
Nick Chen <s27206383@gmail.com>
2025-08-04 21:14:29 +0800

    Time: 121 ms (100.00%) | Memory: 29.2 MB (0.00%) - LeetSync

--- Daily/3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.py
+++ Daily/3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.py
@@ -1,12 +1,14 @@
 min = lambda a, b: a if a < b else b
+max = lambda a, b: a if a > b else b
 class Solution:
     def minRemoval(self, nums: List[int], k: int) -> int:
         nums.sort()
         n = len(nums)
-        ans = n
+        # ans = n
+        ans = 0
         i = 0
         for j in range(n):
             while nums[i] * k < nums[j]:
                 i += 1
-            ans = min(ans, n - (j - i + 1))
-        return ans
+            ans = max(ans, j - i + 1)
+        return n - ans
