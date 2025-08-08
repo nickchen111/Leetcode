307afeff68c9c4a3c9107b0d7f59607820ee12bc
Nick Chen <s27206383@gmail.com>
2025-08-08 21:32:17 +0800

    Create 808. Soup Servings.py

---
+++ Dynamic_Programming/808. Soup Servings.py
@@ -0,0 +1,15 @@
+# TC:O((min(n, U) / L) ^ 2) U 為4451, L = 25, SC:O((min(n, U) / L) ^ 2)
+class Solution:
+    def soupServings(self, n: int) -> float:
+        if n >= 4451:
+            return 1
+        @cache
+        def dfs(a:int, b:int) -> float:
+            if a <= 0 and b <= 0:
+                return  0.5
+            if a <= 0 and b > 0:
+                return 1
+            if a > 0 and b <= 0:
+                return 0
+            return (dfs(a - 100, b - 0) + dfs(a - 75, b - 25) + dfs(a - 50, b - 50) + dfs(a - 25, b - 75)) / 4
+        return dfs(n, n)
