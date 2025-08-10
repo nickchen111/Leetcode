54e8b57d799618a1fe124828907f56b27d7b7a01
Nick Chen <s27206383@gmail.com>
2025-08-10 17:26:41 +0800

    Time: 10 ms (99.56%) | Memory: 29.7 MB (68.21%) - LeetSync

--- Daily/3950-maximum-k-to-sort-a-permutation/maximum-k-to-sort-a-permutation.py
+++ Daily/3950-maximum-k-to-sort-a-permutation/maximum-k-to-sort-a-permutation.py
@@ -3,6 +3,8 @@ class Solution:
         '''
         只有當兩個數值可以and == k 才可以翻轉 問說k可以多大 可以翻轉數組變成遞增
         '''
+        if nums[0] != 0:
+            return 0
         n = len(nums)
         ans = -1
         for i in range(n):
