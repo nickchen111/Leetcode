83e00afa31593ab8fcf72eccdecef324330be3d1
Nick Chen <s27206383@gmail.com>
2025-08-10 17:32:10 +0800

    Time: 278 ms (86.45%) | Memory: 48.4 MB (34.00%) - LeetSync

---
+++ Daily/3959-maximum-total-from-optimal-activation-order/maximum-total-from-optimal-activation-order.py
@@ -0,0 +1,37 @@
+class Solution:
+    def maxTotal(self, value: List[int], limit: List[int]) -> int:
+        # 全貪心寫法
+        groups = defaultdict(list)
+        for l, v in zip(limit, value):
+            groups[l].append(v)
+        ans = 0
+        for lim, v in groups.items():
+            ans += sum(nlargest(lim, v))
+        return ans
+
+        '''
+        # not_choose: (limit, -value)  => 按 limit 小到大，value 大到小
+        not_choose = []
+        for v, l in zip(value, limit):
+            heapq.heappush(not_choose, [l, -v])
+        active = []
+        total = 0
+        cnt = 0
+        while not_choose:
+            limit_val, neg_val = heappop(not_choose)
+            val = -neg_val
+            if cnt < limit_val:
+                # print(total, val)
+                total += val
+                cnt += 1
+                heappush(active, limit_val)
+            
+                while active and active[0] <= cnt:
+                    l = heappop(active)
+                    
+                while not_choose and not_choose[0][0] <= cnt:
+                    heappop(not_choose)
+                    
+                cnt = len(active)
+        return total
+        '''
