b191f8539410b0e6d5c6f792bfab8dbacb12a266
Nick Chen <s27206383@gmail.com>
2025-08-10 21:58:23 +0800

    Create 3643. Flip Square Submatrix Vertically.py

---
+++ Array/3643. Flip Square Submatrix Vertically.py
@@ -0,0 +1,12 @@
+# TC:O(k^2) SC:O(1)
+class Solution:
+    def reverseSubmatrix(self, grid: List[List[int]], x: int, y: int, k: int) -> List[List[int]]:
+        n = len(grid)
+        l = x
+        r = x + k - 1
+        while l < r:
+            for j in range(y, y + k):
+                grid[l][j], grid[r][j] = grid[r][j], grid[l][j]
+            l += 1
+            r -= 1
+        return grid
