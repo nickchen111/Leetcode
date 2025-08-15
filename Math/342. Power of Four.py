7de02c78122bf8c6103a5392379d4eacf0abbaee
Nick Chen <s27206383@gmail.com>
2025-08-15 21:03:15 +0800

    Create 342. Power of Four.py

---
+++ Math/342. Power of Four.py
@@ -0,0 +1,8 @@
+# TC:O(logn) SC:O(1)
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n % 4 == 0:
+            n //= 4
+        return True if n == 1 else False
