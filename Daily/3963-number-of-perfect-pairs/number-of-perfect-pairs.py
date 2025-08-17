b493da3b799cd2c0dfcf2a90703fd37f2ce39b66
Nick Chen <s27206383@gmail.com>
2025-08-17 22:18:53 +0800

    Time: 251 ms (100.00%) | Memory: 31.5 MB (0.00%) - LeetSync

--- Daily/3963-number-of-perfect-pairs/number-of-perfect-pairs.py
+++ Daily/3963-number-of-perfect-pairs/number-of-perfect-pairs.py
@@ -1,10 +1,10 @@
 class Solution:
     def perfectPairs(self, nums: List[int]) -> int:
-        arr = sorted(abs(x) for x in nums)
+        nums.sort(key = abs)
         ans = 0
         i = 0
-        for j, v in enumerate(arr):
-            while v > 2 * arr[i]:
+        for j, v in enumerate(nums):
+            while abs(v) > 2 * abs(nums[i]):
                 i += 1
             ans += j - i
         return ans
