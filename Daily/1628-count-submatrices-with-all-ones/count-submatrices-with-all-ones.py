5703bf3f60a20f1263a8d28064097224a7d3ef1b
Nick Chen <s27206383@gmail.com>
2025-08-21 23:53:57 +0800

    Time: 423 ms (22.74%) | Memory: 18.9 MB (28.97%) - LeetSync

--- Daily/1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.py
+++ Daily/1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.py
@@ -2,27 +2,15 @@ class Solution:
     def numSubmat(self, mat: List[List[int]]) -> int:
         m, n = len(mat), len(mat[0])
         ans = 0
-        cnt = [0] * (n + 1)
-        def cal(cnt: List[int]) -> int:
-            st = []
-            st.append(0)
-            tot = ret = 0
-            for i in range(1, len(cnt)):
-                while st and cnt[st[-1]] > cnt[i]:
-                    h = cnt[st[-1]] - cnt[i]
-                    w1 = st[-1]
-                    st.pop()
-                    w2 = st[-1]
-                    tot -= h * (w1 - w2)
-                st.append(i)
-                tot += cnt[i]
-                ret += tot
-            return ret
-        for i in range(m):
-            for j in range(n):
-                if mat[i][j]:
-                    cnt[j + 1] += 1
-                else:
-                    cnt[j + 1] = 0
-            ans += cal(cnt)
+        for top in range(m):
+            a = [0] * n
+            for bottom in range(top, m):
+                h = bottom - top + 1
+                last = -1
+                for j in range(n):
+                    a[j] += mat[bottom][j]
+                    if a[j] != h:
+                        last = j
+                    else:
+                        ans += j - last
         return ans
