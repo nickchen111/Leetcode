4ee6c0773b83441a6c6242b5077f3160f04dc0dc
Nick Chen <s27206383@gmail.com>
2025-09-01 21:58:40 +0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (100.00%) - LeetSync

--- Daily/3994-find-the-least-frequent-digit/find-the-least-frequent-digit.py
+++ Daily/3994-find-the-least-frequent-digit/find-the-least-frequent-digit.py
@@ -1,12 +1,5 @@
 class Solution:
     def getLeastFrequentDigit(self, n: int) -> int:
         cnt = Counter(str(n))
-        ans = 0
-        f = inf
-        for x, freq in cnt.items():
-            if freq < f:
-                f = freq
-                ans = int(x)
-            elif freq == f:
-                ans = min(ans, int(x))
-        return ans
+        min_ch = min((c, ch) for ch, c in cnt.items())[1] # 這樣就可以順便比較頻率相同的狀況選最小的ch 不用額外寫if else
+        return int(min_ch)
