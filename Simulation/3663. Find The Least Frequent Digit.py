773ab9d8f4a8778bf63da0216436aac4496cc030
Nick Chen <s27206383@gmail.com>
2025-09-01 22:01:14 +0800

    Create 3663. Find The Least Frequent Digit.py

---
+++ Simulation/3663. Find The Least Frequent Digit.py
@@ -0,0 +1,6 @@
+# TC:O(logn or D) D為cnt大小 SC:O(logn or D) 
+class Solution:
+    def getLeastFrequentDigit(self, n: int) -> int:
+        cnt = Counter(str(n))
+        min_ch = min((c, ch) for ch, c in cnt.items())[1] # 這樣就可以順便比較頻率相同的狀況選最小的ch 不用額外寫if else
+        return int(min_ch)
