0929eb9d1488536fa6124e68355daf81ad2cf3a9
Nick Chen <s27206383@gmail.com>
2025-09-04 17:58:35 +0800

    Create 3651. Minimum Cost Path with Teleportations.py

---
+++ Dynamic_Programming/3651. Minimum Cost Path with Teleportations.py
@@ -0,0 +1,33 @@
+# TC:O((m * n + U)k) U為數值最大值 SC:O(n + U)
+class Solution:
+    def minCost(self, grid: List[List[int]], k: int) -> int:
+        m, n = len(grid), len(grid[0])
+        mx = max(map(max, grid))
+        suf_min_f = [inf] * (mx + 2)
+        # 空間優化
+        for _ in range(k + 1):
+            min_f = [inf] * (mx + 1)
+
+            f = [inf] * (n + 1)
+            f[1] = -grid[0][0]
+            for i in range(m):
+                for j in range(n):
+                    f[j + 1] = min(min(f[j], f[j + 1]) + grid[i][j], suf_min_f[grid[i][j]])
+                    min_f[grid[i][j]] = min(min_f[grid[i][j]], f[j + 1])
+            for i in range(mx, -1, -1):
+                suf_min_f[i] = min(suf_min_f[i + 1], min_f[i])
+        return f[n]
+        '''
+        for _ in range(k + 1):
+            min_f = [inf] * (mx + 1)
+
+            f = [[inf] * (n + 1) for _ in range(m + 1)]
+            f[0][1] = f[1][0] = -grid[0][0]
+            for i in range(m):
+                for j in range(n):
+                    f[i + 1][j + 1] = min(min(f[i + 1][j], f[i][j + 1]) + grid[i][j], suf_min_f[grid[i][j]])
+                    min_f[grid[i][j]] = min(min_f[grid[i][j]], f[i + 1][j + 1])
+            for i in range(mx, -1, -1):
+                suf_min_f[i] = min(suf_min_f[i + 1], min_f[i])
+        return f[m][n]
+        '''
