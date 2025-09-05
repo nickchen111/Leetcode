7e9e4196e543cac358f4d59140216d252c359ed0
Nick Chen <s27206383@gmail.com>
2025-09-05 15:29:34 +0800

    Create 2749. Minimum Operations to Make the Integer Zero.py

---
+++ Bit_Manipulation/2749. Minimum Operations to Make the Integer Zero.py
@@ -0,0 +1,20 @@
+# TC:O(C) C上限為32 SC:O(1)
+class Solution:
+    def makeTheIntegerZero(self, num1: int, num2: int) -> int:
+        '''
+        就是要把num1拆解成最少的 num2 + 2 ^ i 最少可以怎拆
+        DP!? 
+        有正有負 情況比較複雜 DP來做數值範圍太大了感覺會爆
+        數學拆解?! num1 = k * num2 + sum(k 個 2 ^ i)
+        num1 - k * num2 = sum(k 個 2 ^ i) 
+        num1 - k * num2 >= 0
+        bit_count(num1 - k * num2) <= k <= num1 - k * num2 (全部都選2 ^ 0)
+        那當然會選最少的bit_count()
+        '''
+        for k in range(1, 33):
+            t = num1 - k * num2
+            if t < 0:
+                continue
+            if t.bit_count() <= k <= t:
+                return k
+        return -1
