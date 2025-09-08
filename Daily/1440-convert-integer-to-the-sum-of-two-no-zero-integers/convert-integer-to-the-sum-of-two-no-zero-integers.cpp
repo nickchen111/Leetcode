32efd4c01fc10a82db9548aadda56d490cf5b2a7
Nick Chen <s27206383@gmail.com>
2025-09-08 11:50:54 +0800

    Time: 0 ms (100.00%) | Memory: 8.2 MB (85.27%) - LeetSync

---
+++ Daily/1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        int cur = n / 2;
+        auto check = [](int m) -> bool {
+            while (m) {
+                int tmp = m % 10;
+                if (tmp == 0) return false;
+                m /= 10;
+            }
+            return true;
+        };
+        while (true) {
+            if (check(cur) && check(n - cur)) return {cur, n - cur};
+            cur += 1;
+        }
+
+        return {};
+    }
+};
