7fa40ad463d52fc34cb943116d626a8792e93aa7
Nick Chen <s27206383@gmail.com>
2025-09-12 22:37:30 +0800

    Time: 6 ms (63.25%) | Memory: 19.7 MB (8.21%) - LeetSync

---
+++ Daily/3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        string s1;
+        int start = -1;
+        int end = s.size()-1;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'){
+                s1 += s[i];
+            }
+        }
+        
+        int n = s1.size();
+        if(n == 0) return false;
+        else return true;
+    }
+};
+
+/*
+A 勝利條件是 讓對方 剩下讓他只有一個母音可以選擇
+B 的話是讓字串沒有母音
+leetcode -> eeoe
+xxxxxx 偶數的話必贏 除了 0個
+x x 
+x x x x x
+"ifld"
+"sloalo"
+*/
