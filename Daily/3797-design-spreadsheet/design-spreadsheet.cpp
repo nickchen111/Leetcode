b701ee987746b1156b66a844101507d578a0a750
Nick Chen <s27206383@gmail.com>
2025-09-19 19:11:27 +0800

    Time: 69 ms (80.05%) | Memory: 127.9 MB (80.85%) - LeetSync

---
+++ Daily/3797-design-spreadsheet/design-spreadsheet.cpp
@@ -0,0 +1,47 @@
+class Spreadsheet {
+    unordered_map<string, int> mp;
+public:
+    Spreadsheet(int rows) {
+        
+    }
+    
+    void setCell(string cell, int value) {
+        mp[cell] = value;
+    }
+    
+    void resetCell(string cell) {
+        mp[cell] = 0;
+    }
+    
+    int getValue(string formula) {
+        if (formula[0] == '=') formula = formula.substr(1);
+        int left = 0, right = 0;
+        int n = formula.size();
+        int pos = -1;
+        string tmp;
+        for (int i = 0; i < n; i++) {
+            if (formula[i] == '+') {
+                pos = i;
+                break;
+            }
+            tmp += formula[i];
+        }
+        if (formula[0] >= 'A' && formula[0] <= 'Z') left = mp[tmp];
+        else left = stoi(tmp);
+        
+
+        tmp = formula.substr(pos + 1);
+        if (formula[pos + 1] >= 'A' && formula[pos + 1] <= 'Z') right = mp[tmp];
+        else right = stoi(tmp);
+        
+        return left + right;
+    }
+};
+
+/**
+ * Your Spreadsheet object will be instantiated and called as such:
+ * Spreadsheet* obj = new Spreadsheet(rows);
+ * obj->setCell(cell,value);
+ * obj->resetCell(cell);
+ * int param_3 = obj->getValue(formula);
+ */
