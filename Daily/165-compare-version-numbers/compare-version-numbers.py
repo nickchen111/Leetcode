87838e80f76ef87201d6b7d8ad1fab7108717b10
Nick Chen <s27206383@gmail.com>
2025-09-23 15:35:03 +0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (60.47%) - LeetSync

--- Daily/165-compare-version-numbers/compare-version-numbers.py
+++ Daily/165-compare-version-numbers/compare-version-numbers.py
@@ -1,34 +1,18 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        def str_to_int (s:str) -> List[int]:
-            arr = []
-            n = len(s)
-            i = 0
-            while i < n:
-                j = i
-                tmp = ""
-                while j < n and s[j] != '.':
-                    tmp += s[j]
-                    j += 1
-                arr.append(int(tmp))
-                i = j + 1
-            return arr
-        arr1 = str_to_int(version1)
-        arr2 = str_to_int(version2)
-        i = j = 0
-        while i < len(arr1) and j < len(arr2):
-            if arr1[i] > arr2[j]:
-                return 1
-            elif arr1[i] < arr2[j]:
+        version1=version1.split('.')
+        version2=version2.split('.')
+        m=len(version1)
+        n=len(version2)
+        while m<n:
+            version1.append('0')
+            m+=1
+        while n<m:
+            version2.append('0')
+            n+=1
+        for i,j in zip(version1,version2):
+            if (int(i))<(int(j)):
                 return -1
-            i += 1
-            j += 1
-        while i < len(arr1):
-            if arr1[i] > 0:
+            elif (int(i))>(int(j)):
                 return 1
-            i += 1
-        while j < len(arr2):
-            if arr2[j] > 0:
-                return -1
-            j += 1
         return 0
