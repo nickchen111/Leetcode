016de73e635de77567bf32366c0681115c68d9eb
Nick Chen <s27206383@gmail.com>
2025-09-24 21:31:16 +0800

    Time: 354 ms (80.95%) | Memory: 153.7 MB (91.66%) - LeetSync

--- Daily/4007-maximum-total-subarray-value-ii/maximum-total-subarray-value-ii.cpp
+++ Daily/4007-maximum-total-subarray-value-ii/maximum-total-subarray-value-ii.cpp
@@ -1,54 +1,74 @@
-template<typename T>
-class SparseTable {
-    vector<vector<int>> st_min;
-    vector<vector<int>> st_max;
-public:
-    // 創建表的build function
-    SparseTable (vector<T>& a) {
-        size_t n = a.size();
-        int sz = bit_width(unsigned(n));
-        st_min.resize(n, vector<int>(sz));
-        st_max.resize(n, vector<int>(sz));
-        // 走2^0步的時候所有點兩個區間加總
-        for (int i = 0; i < n; i++) {
-            st_min[i][0] = a[i];
-            st_max[i][0] = a[i];
-        }
-        for(int j = 1; j < sz; j++) {
-            for (int i = 0; i + (1 << j) <= n; i++) {
-                st_min[i][j] = min(st_min[i][j-1], st_min[i + (1 << (j - 1))][j-1]); // 改成題目要做的事情
-                st_max[i][j] = max(st_max[i][j-1], st_max[i + (1 << (j - 1))][j-1]);
-            }
-        }
-    }
-    int query(int l, int r) const { // 左閉右開 查詢完整區間的最大值 只有一個區間不算
-        if (l >= r) return 0;
-        int k = bit_width((uint32_t) r - l) - 1;
-        int mx = max(st_max[l][k], st_max[r - (1 << k)][k]);
-        int mn = min(st_min[l][k], st_min[r - (1 << k)][k]);
-        return mx - mn;
-    }
+struct Node {
+    int mx, mn;
 };
 
+class SegmentTree {
+    int n;
+    vector<Node> tree;
+
+    Node merge_val(const Node& a, const Node& b) const {
+        return {max(a.mx, b.mx), min(a.mn, b.mn)};
+    }
+
+    void maintain(int node) {
+        tree[node] = merge_val(tree[node*2], tree[node*2+1]);
+    }
+
+    void build(const vector<int>& a, int node, int l, int r) {
+        if (l == r) {
+            tree[node] = {a[l], a[l]};
+            return;
+        }
+        int m = (l + r) / 2;
+        build(a, node*2, l, m);
+        build(a, node*2+1, m+1, r);
+        maintain(node);
+    }
+
+    Node query(int node, int l, int r, int ql, int qr) const {
+        if (ql <= l && r <= qr) return tree[node];
+        int m = (l + r) / 2;
+        if (qr <= m) return query(node*2, l, m, ql, qr);
+        if (ql > m) return query(node*2+1, m+1, r, ql, qr);
+        Node left = query(node*2, l, m, ql, qr);
+        Node right = query(node*2+1, m+1, r, ql, qr);
+        return merge_val(left, right);
+    }
+
+public:
+    SegmentTree(const vector<int>& a) : n(a.size()), tree(4*a.size()) {
+        build(a, 1, 0, n-1);
+    }
+
+    Node query(int l, int r) const {
+        return query(1, 0, n-1, l, r);
+    }
+};
 
 class Solution {
 public:
     long long maxTotalValue(vector<int>& nums, int k) {
         int n = nums.size();
-        SparseTable st(nums);
+        SegmentTree st(nums);
+
+        // 優先隊列存 {最大值, 左邊界, 右邊界}
         priority_queue<tuple<int,int,int>> pq;
         for (int i = 0; i < n; i++) {
-            pq.emplace(st.query(i, n), i, n);
+            auto node = st.query(i, n-1);
+            pq.emplace(node.mx - node.mn, i, n-1);
         }
 
         long long ans = 0;
-        while (k-- && get<0>(pq.top())) {
+        while (k-- && !pq.empty() && get<0>(pq.top()) > 0) {
             auto [d, l, r] = pq.top();
             pq.pop();
             ans += d;
-            pq.emplace(st.query(l, r - 1), l, r - 1);
+            // cout << d << endl;
+            if (l <= r-1) {
+                auto node = st.query(l, r-1);
+                pq.emplace(node.mx - node.mn, l, r-1);
+            }
         }
-
         return ans;
     }
 };
