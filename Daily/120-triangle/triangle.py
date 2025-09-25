b9a75d6c9f6f3642beaeb02f09a2bc10b67c008a
Nick Chen <s27206383@gmail.com>
2025-09-25 13:56:02 +0800

    Time: 3 ms (86.00%) | Memory: 19.6 MB (20.17%) - LeetSync

--- Daily/120-triangle/triangle.py
+++ Daily/120-triangle/triangle.py
@@ -2,17 +2,9 @@ min = lambda a, b : a if a < b else b
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
-        dp = [inf] * n
-        dp[0] = triangle[0][0]
-        for i in range(1, n):
-            tmp = [inf] * n
-            m = len(triangle[i])
-            for j in range(m - 1):
-                tmp[j] = min(tmp[j], dp[j] + triangle[i][j])
-                tmp[j + 1] = min(tmp[j + 1], dp[j] + triangle[i][j + 1])
-            dp = tmp
-        # print(dp)
-        ans = inf
-        for i in range(n):
-            ans = min(ans, dp[i])
-        return ans
+        f = [[0] * (i + 1) for i in range(n)]
+        f[-1] = triangle[-1]
+        for i in range(n - 2, -1, -1):
+            for j, x in enumerate(triangle[i]):
+                f[i][j] = min(f[i + 1][j], f[i + 1][j + 1]) + x
+        return f[0][0]
