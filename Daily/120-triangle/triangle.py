5c50a6d5e217aa1fe02165989cc44ef70c941bd8
Nick Chen <s27206383@gmail.com>
2025-09-25 13:58:57 +0800

    Time: 4 ms (54.43%) | Memory: 19.2 MB (39.88%) - LeetSync

--- Daily/120-triangle/triangle.py
+++ Daily/120-triangle/triangle.py
@@ -5,3 +5,25 @@ class Solution:
             for j in range(i + 1):
                 f[i][j] += min(f[i + 1][j], f[i + 1][j + 1])
         return f[0][0]
+
+
+        '''
+        遞推
+        n = len(triangle)
+        f = [[0] * (i + 1) for i in range(n)]
+        f[-1] = triangle[-1]
+        for i in range(n - 2, -1, -1):
+            for j, x in enumerate(triangle[i]):
+                f[i][j] = min(f[i + 1][j], f[i + 1][j + 1]) + x
+        return f[0][0]
+        '''
+        '''
+        遞歸
+        n = len(triangle)
+        @cache
+        def dfs(i: int, j: int) -> int:
+            if i == n - 1:
+                return triangle[i][j]
+            return min(dfs(i + 1, j), dfs(i + 1, j + 1)) + triangle[i][j]
+        return dfs(0, 0)
+        '''
