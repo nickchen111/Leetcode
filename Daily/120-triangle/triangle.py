70f99d2f886bb9f2c49b8088080603fb97f7164a
Nick Chen <s27206383@gmail.com>
2025-09-25 13:56:28 +0800

    Time: 2 ms (90.10%) | Memory: 18.9 MB (62.18%) - LeetSync

--- Daily/120-triangle/triangle.py
+++ Daily/120-triangle/triangle.py
@@ -1,10 +1,7 @@
 min = lambda a, b : a if a < b else b
 class Solution:
-    def minimumTotal(self, triangle: List[List[int]]) -> int:
-        n = len(triangle)
-        f = [[0] * (i + 1) for i in range(n)]
-        f[-1] = triangle[-1]
-        for i in range(n - 2, -1, -1):
-            for j, x in enumerate(triangle[i]):
-                f[i][j] = min(f[i + 1][j], f[i + 1][j + 1]) + x
+    def minimumTotal(self, f: List[List[int]]) -> int:
+        for i in range(len(f) - 2, -1, -1):
+            for j in range(i + 1):
+                f[i][j] += min(f[i + 1][j], f[i + 1][j + 1])
         return f[0][0]
