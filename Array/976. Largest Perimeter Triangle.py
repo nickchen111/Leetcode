60acfe722149d24b937ac28295fb54e95944f381
Nick Chen <s27206383@gmail.com>
2025-09-28 22:36:42 +0800

    Create 976. Largest Perimeter Triangle.py

---
+++ Array/976. Largest Perimeter Triangle.py
@@ -0,0 +1,8 @@
+# TC:O(nlogn) SC:O(1)
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(len(nums) - 1, 1, -1):
+            if nums[i] < nums[i - 1] + nums[i - 2]:
+                return nums[i] + nums[i - 1] + nums[i - 2]
+        return 0
