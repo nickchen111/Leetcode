51306385b19c756591330b3872732de76fc50f7a
Nick Chen <s27206383@gmail.com>
2025-10-07 22:09:47 +0800

    Time: 500 ms (5.12%) | Memory: 34.2 MB (66.05%) - LeetSync

--- Daily/1612-avoid-flood-in-the-city/avoid-flood-in-the-city.py
+++ Daily/1612-avoid-flood-in-the-city/avoid-flood-in-the-city.py
@@ -4,24 +4,22 @@ class Solution:
         將這些機會存起來 當有個湖被flood時 找一個在他之前 但是在第一個位置之後的dry
         '''
         mp = defaultdict(int)
-        st = []
+        st = SortedList()
         ans = [-1] * len(rains)
         for i, r in enumerate(rains):
             if r == 0:
-                st.append(i)
+                st.add(i)
                 ans[i] = 1
-            else:
-                if r in mp:
-                    prev_pos = mp[r] # 找他之後的第一個位置
-                    idx = bisect_left(st, prev_pos)
-                    if idx < len(st):
-                        ans[st[idx]] = r
-                        st.remove(st[idx])
-                        mp[r] = i
-                    else:
-                        return [] 
-                else:
-                    mp[r] = i
+                continue
+            if r in mp:
+                prev_pos = mp[r] # 找他之後的第一個位置
+                idx = bisect_left(st, prev_pos)
+                if idx == len(st):
+                    return []
+                d = st[idx]
+                ans[d] = r
+                st.discard(d)
+            mp[r] = i
         return ans
                     
 
