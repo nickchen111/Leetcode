68e2be5a3073d109a6768268b4124c6cabc936a1
Nick Chen <s27206383@gmail.com>
2025-10-13 21:39:46 +0800

    Create 3712. Sum of Elements With Frequency Divisible by K.cpp

---
+++ Array/3712. Sum of Elements With Frequency Divisible by K.cpp
@@ -0,0 +1,14 @@
+// TC:O(n) SC:O(n)
+class Solution {
+public:
+    int sumDivisibleByK(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        for (auto &num : nums) mp[num] += 1;
+        int ans = 0;
+        for (auto &[key, v] : mp) {
+            if ((v % k) == 0) ans += key * v;
+        }
+        return ans;
+    }
+};
