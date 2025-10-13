52234a513b845ae3d5efcc93470ea8461aa40ae1
Nick Chen <s27206383@gmail.com>
2025-10-14 03:08:58 +0800

    Time: 1311 ms (50.06%) | Memory: 484.3 MB (7.22%) - LeetSync

---
+++ Daily/4056-longest-balanced-substring-ii/longest-balanced-substring-ii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int longestBalanced(string s) {
+        /*
+        前綴和? abc, ab, bc, ac, aaa, bbb, ccc
+        考慮這幾種情況 求出最長前綴和
+        要設計怎樣的資料結構去存? abc -> 333用十進制表示
+        */
+        int n = s.size();
+        int ans = 0;
+
+        // 第一種情況都是a or b or c
+        int i = 0;
+        while (i < n) {
+            int start = i + 1;
+            while (start < n && s[start] == s[i]) start += 1;
+            ans = max(ans, start - i);
+            i = start;
+        }
+        auto f2 = [&](char x, char y) -> void {
+            // x, y 表示 這次要收集的字母
+            int i = 0, d = 0;
+            while (i < n) {
+                unordered_map<int, int> pos;
+                pos[0] = i - 1;
+                int d = 0;
+                while (i < n && s[i] == x || s[i] == y) {
+                    d += s[i] == x ? 1 : -1;
+                    if (pos.count(d)) ans = max(ans, i - pos[d]);
+                    else pos[d] = i;
+                    i += 1;
+                }
+                i += 1;
+            }
+        };
+        f2('a', 'b');
+        f2('a', 'c');
+        f2('b', 'c');
+
+        unordered_map<long long, int> pos;
+        pos[1LL * n << 32 | n] = -1;
+        int cnt[3]{};
+        for (int i = 0; i < n; i++) {
+            cnt[s[i] - 'a'] += 1;
+            long long p = 1LL * (cnt[0] - cnt[1] + n) << 32 | (cnt[1] - cnt[2] + n);
+            if (pos.count(p)) ans = max(ans, i - pos[p]);
+            else pos[p] = i;
+        }
+
+        return ans;
+    }
+};
