56a73661dbaa68fbd3107d907ed82ebd7975afff
Nick Chen <s27206383@gmail.com>
2025-10-20 15:37:13 +0800

    Create 2011. Final Value of Variable After Performing Operations.cpp

---
+++ String/2011. Final Value of Variable After Performing Operations.cpp
@@ -0,0 +1,12 @@
+// TC:O(n) SC:O(1)
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int ans = 0;
+        for (auto &op : operations) {
+            if (op[0] == '-' || op[2] == '-') ans -= 1;
+            else ans += 1;
+        }
+        return ans;
+    }
+};
