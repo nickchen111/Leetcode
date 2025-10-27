ab083ef38e11ce1ebcc13d53a04e640bc035820c
Nick Chen <s27206383@gmail.com>
2025-10-27 17:45:01 +0800

    Time: 1 ms (88.98%) | Memory: 27.3 MB (84.17%) - LeetSync

---
+++ Daily/2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int prev = 0, ans = 0;
+        for (auto &s : bank) {
+            int tmp = 0;
+            for (auto &ch : s) {
+                if (ch == '1') tmp += 1;
+            }
+            ans += tmp * prev;
+            if(tmp) prev = tmp;
+        }
+        return ans;
+    }
+};
