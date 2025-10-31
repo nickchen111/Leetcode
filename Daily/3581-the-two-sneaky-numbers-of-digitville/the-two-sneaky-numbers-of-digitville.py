dafa48efd084b8464a479762f2d7d5a4ba2745e3
Nick Chen <s27206383@gmail.com>
2025-11-01 00:59:54 +0800

    Time: 3 ms (51.08%) | Memory: 17.8 MB (66.29%) - LeetSync

---
+++ Daily/3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        st = set()
+        ans = []
+        for x in nums:
+            if x in st:
+                ans.append(x)
+            st.add(x)
+        return ans
