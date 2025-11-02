eb52b918e7d9027213e65e5d94781d60b7b0a6ba
Nick Chen <s27206383@gmail.com>
2025-11-02 22:20:31 +0800

    Create 3732. Maximum Product of Three Elements After One Replacement.py

---
+++ Greedy/3732. Maximum Product of Three Elements After One Replacement.py
@@ -0,0 +1,12 @@
+# TC:O(n) SC:O(1)
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        mx1 = mx2 = -inf
+        for x in nums:
+            x = abs(x)
+            if x > mx1:
+                mx2, mx1 = mx1, x
+            elif x > mx2:
+                mx2 = x
+        return mx1 * mx2 * 10 ** 5
+        
