998ec5586b0ab6ffbaca07a6c223ae9263ce8604
Nick Chen <s27206383@gmail.com>
2025-11-08 17:36:43 +0800

    Time: 352 ms (70.53%) | Memory: 110.4 MB (40.28%) - LeetSync

--- Daily/3863-power-grid-maintenance/power-grid-maintenance.py
+++ Daily/3863-power-grid-maintenance/power-grid-maintenance.py
@@ -1,17 +1,15 @@
-# Heap: TC:O(clogc + n + qlogc) SC:O(n + c) UF: TC:O((c + q)logc) SC:O(n + c)
 class Solution:
     def processQueries(self, c: int, connections: List[List[int]], queries: List[List[int]]) -> List[int]:
-        # 懶刪除堆
         g = [[] for _ in range(c + 1)]
-        for u, v in connections:
-            g[u].append(v)
-            g[v].append(u)
+        for x, y in connections:
+            g[x].append(y)
+            g[y].append(x)
         heaps = []
         belongs = [-1] * (c + 1)
-        def dfs(u:int) -> None:
-            belongs[u] = len(heaps)
-            h.append(u)
-            for y in g[u]:
+        def dfs(x:int) -> None:
+            belongs[x] = len(heaps)
+            h.append(x)
+            for y in g[x]:
                 if belongs[y] < 0:
                     dfs(y)
         for i in range(1, c + 1):
@@ -35,54 +33,3 @@ class Solution:
                 heappop(h)
             ans.append(h[0] if h else -1)
         return ans
-
-        '''
-        UF + set + SortedList寫法
-        fa = list(range(c + 1))
-        def find(x: int) -> int:
-            if fa[x] != x:
-                fa[x] = find(fa[x])
-            return fa[x]
-        def merge(u: int, v: int) -> None:
-            ru, rv = find(u), find(v)
-            if ru != rv:
-                if ru < rv:
-                    fa[rv] = ru
-                else:
-                    fa[ru] = rv
-        for u, v in connections:
-            merge(u, v)
-        mp = defaultdict(SortedList)
-        for i in range(1, c + 1):
-            root = find(i)
-            if i != root:
-                mp[root].add(i)
-
-        ans = []
-        removed = set()
-        for qtype, node in queries:
-            if qtype == 1:
-                if node not in removed:
-                    ans.append(node)
-                else:
-                    root = find(node)
-                    if node == root:
-                        if mp[root]:
-                            ans.append(mp[root][0])
-                        else:
-                            ans.append(-1)
-                    else:
-                        if root in removed:
-                            if mp[root]:
-                                ans.append(mp[root][0])
-                            else:
-                                ans.append(-1)
-                        else:
-                            ans.append(root)
-            else:
-                removed.add(node)
-                root = find(node)
-                if node in mp[root]:
-                    mp[root].remove(node)
-        return ans
-        '''
