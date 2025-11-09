c2073d699dbbe01c1687d7cb396ee1d121f4d7ce
Nick Chen <s27206383@gmail.com>
2025-11-09 21:26:01 +0800

    Time: 0 ms (100.00%) | Memory: 18 MB (13.37%) - LeetSync

---
+++ Daily/2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        ans = 0
+        while num2 > 0:
+            ans += num1 // num2
+            num1, num2 = num2, num1 % num2
+        return ans
