509d92cdba02e3fa25d91d83bccf293bf1ba1c67
Nick Chen <s27206383@gmail.com>
2025-11-09 21:27:06 +0800

    Create 2169. Count Operations to Obtain Zero.py

---
+++ Math/2169. Count Operations to Obtain Zero.py
@@ -0,0 +1,8 @@
+# TC:O(logn) SC:O(1)
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        ans = 0
+        while num2 > 0:
+            ans += num1 // num2
+            num1, num2 = num2, num1 % num2
+        return ans
