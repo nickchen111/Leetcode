35d2689907914b92e0b5f6e05c7a14f40693e735
Nick Chen <s27206383@gmail.com>
2025-11-12 21:41:23 +0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (97.65%) - LeetSync

---
+++ Daily/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        if all(x == 1 for x in nums):
+            return 0
+        one = 0
+        for x in nums:
+            if x == 1:
+                one += 1
+        if one:
+            return n - one
+        ans = inf
+        # 找出gcd = 1 最短的array
+        for i in range(n):
+            g = nums[i]
+            end = -1
+            for j in range(i, n):
+                g = gcd(g, nums[j])
+                if g == 1:
+                    end = j
+                    break
+            if g == 1:
+                ans = min(ans, n - 1 + j - i)
+        return ans if ans != inf else -1
