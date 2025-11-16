e63b2e168083ac1e8ee97db9d3c02c7a08fd0d46
Nick Chen <s27206383@gmail.com>
2025-11-16 13:57:49 +0800

    Time: 23 ms (29.21%) | Memory: 18.3 MB (40.82%) - LeetSync

---
+++ Daily/1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numSub(self, s: str) -> int:
+        MOD = 10 ** 9 + 7
+        i = ans = 0
+        n = len(s)
+        while i < n:
+            j = i
+            while j < n and s[j] == '1':
+                j += 1
+                ans += j - i
+            i = j + 1
+        return ans % MOD
