9d4b39add91966fa3069667a2e53a85b5f6de031
Nick Chen <s27206383@gmail.com>
2025-11-18 15:27:35 +0800

    Time: 5 ms (3.95%) | Memory: 18.8 MB (23.71%) - LeetSync

---
+++ Daily/717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        n = len(bits)
+        @cache
+        def dfs(i:int) -> bool:
+            if i == n - 1:
+                return True
+            if i >= n:
+                return False
+            if bits[i] == 0:
+                return dfs(i + 1)
+            return dfs(i + 2)
+        return dfs(0)
