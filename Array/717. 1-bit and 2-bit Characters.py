d919ca196c535966a463c2308cb404c0666eb16a
Nick Chen <s27206383@gmail.com>
2025-11-18 15:48:28 +0800

    Create 717. 1-bit and 2-bit Characters.py

---
+++ Array/717. 1-bit and 2-bit Characters.py
@@ -0,0 +1,24 @@
+# TC:O(n) SC:O(1)
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        n = len(bits)
+        i = 0
+        while i < n - 1:
+            if bits[i] == 0:
+                i += 1
+                continue
+            i += 2
+        return i == n - 1
+
+        '''
+        @cache
+        def dfs(i:int) -> bool:
+            if i == n - 1:
+                return True
+            if i == n:
+                return False
+            if bits[i] == 0:
+                return dfs(i + 1)
+            return dfs(i + 2)
+        return dfs(0)
+        '''
