3243c59fcabdf75884938e9da8b23a2db9b820a6
Nick Chen <s27206383@gmail.com>
2025-11-24 21:56:23 +0800

    Create 1018. Binary Prefix Divisible By 5.py

---
+++ Array/1018. Binary Prefix Divisible By 5.py
@@ -0,0 +1,9 @@
+# TC:O(n) SC:O(1)
+class Solution:
+    def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
+        cur = 0
+        ans = []
+        for x in nums:
+            cur = (cur * 2 + x) % 5
+            ans.append(True if cur % 5 == 0 else False)
+        return ans
