f1ad0216e7cf8651a3c5080f625d08593746a2d5
Nick Chen <s27206383@gmail.com>
2025-11-26 22:14:40 +0800

    Time: 2213 ms (34.08%) | Memory: 99.2 MB (35.43%) - LeetSync

--- Daily/2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.py
+++ Daily/2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.py
@@ -5,6 +5,22 @@ class Solution:
         '''
         m, n = len(grid), len(grid[0])
         MOD = 10 ** 9 + 7
+        f = [[[0] * (k + 1) for _ in range(n)] for _ in range(m)]
+        f[0][0][grid[0][0] % k] = 1
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    continue
+                x = grid[i][j]
+                for r in range(k):
+                    if i - 1 >= 0 and f[i - 1][j][r]:
+                        f[i][j][(r + x) % k] += f[i - 1][j][r]
+                    if j - 1 >= 0 and f[i][j - 1][r]:
+                        f[i][j][(r + x) % k] += f[i][j - 1][r]
+                    f[i][j][(r + x) % k] %= MOD
+        return f[m - 1][n - 1][0]
+
+        '''
         @cache
         def dfs(i:int, j:int, r:int) ->int:
             if i == 0 and j == 0:
@@ -15,3 +31,4 @@ class Solution:
         ans = dfs(m - 1, n - 1, 0) % MOD
         dfs.cache_clear()
         return ans
+        '''
