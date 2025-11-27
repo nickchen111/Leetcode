15494bed1d411e579ba6faf0c720b5d65da1aba5
Nick Chen <s27206383@gmail.com>
2025-11-27 23:17:50 +0800

    Time: 515 ms (6.89%) | Memory: 61.9 MB (24.83%) - LeetSync

--- Daily/3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.py
+++ Daily/3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.py
@@ -1,17 +1,13 @@
 class Solution:
     def maxSubarraySum(self, nums: List[int], k: int) -> int:
-        mp = {}
+        mp = defaultdict(int)
         tot = 0
         mp[0] = 0
         ans = -inf
         for i, x in enumerate(nums):
             tot += x
             length = i + 1
-            m = length % k
-            if m in mp:
-                ans = max(ans, tot - mp[m])
-                mp[m] = min(mp[m], tot)
-            else:
-                mp[m] = min(inf, tot)
-
+            if (length % k) in mp:
+                ans = max(ans, tot - mp[length % k])
+            mp[length % k] = min(mp[length % k] if length % k in mp else inf, tot)
         return ans
