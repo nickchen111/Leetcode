cfca1a2c523a65f03e4cb83808448a206decdf2f
Nick Chen <s27206383@gmail.com>
2025-11-30 14:35:27 +0800

    Time: 88 ms (100.00%) | Memory: 32.4 MB (100.00%) - LeetSync

--- Daily/4141-count-elements-with-at-least-k-greater-values/count-elements-with-at-least-k-greater-values.py
+++ Daily/4141-count-elements-with-at-least-k-greater-values/count-elements-with-at-least-k-greater-values.py
@@ -1,13 +1,7 @@
 class Solution:
     def countElements(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        mp = Counter(nums)
-        arr = list(set(nums))
-        arr.sort()
-        cnt = ans = 0
-        for i in range(len(arr) - 1, -1, -1):
-            if cnt >= k:
-                ans += mp[arr[i]]
-                continue
-            cnt += mp[arr[i]]
-        return ans
+        if k == 0:
+            return n
+        nums.sort()
+        return bisect_left(nums, nums[-k])
