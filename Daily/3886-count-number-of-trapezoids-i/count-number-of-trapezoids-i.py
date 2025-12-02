1d26aac87178349baec1074cfd7cf0ed6222bfef
Nick Chen <s27206383@gmail.com>
2025-12-02 21:28:22 +0800

    Time: 114 ms (5.03%) | Memory: 63.7 MB (43.12%) - LeetSync

---
+++ Daily/3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countTrapezoids(self, points: List[List[int]]) -> int:
+        '''
+        x -> y 
+        然後就是乘法原理
+        '''
+        MOD = 10 ** 9 + 7
+        mp = defaultdict(list)
+        for x, y in points:
+            mp[y].append(x)
+        if len(mp) == 1:
+            return 0
+        tot = ans = 0
+        for arr in mp.values():
+            x = len(arr)
+            tot += x * (x - 1) // 2
+        for arr in mp.values():
+            x = len(arr)
+            cur = x * (x - 1) // 2
+            if x > 1:
+                ans += cur * (tot - cur)
+        return ans // 2 % MOD
+        
