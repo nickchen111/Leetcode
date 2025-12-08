72afccefb0bee685be47490fc1e7211c5b2b5e0b
Nick Chen <s27206383@gmail.com>
2025-12-08 13:49:54 +0800

    Time: 78 ms (89.13%) | Memory: 17.8 MB (33.15%) - LeetSync

---
+++ Daily/2037-count-square-sum-triples/count-square-sum-triples.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countTriples(self, n: int) -> int:
+        '''
+        可以用st 讓他n^2解
+        '''
+        st = set()
+        for i in range(1, n + 1):
+            st.add(i * i)
+        ans = 0
+        print(st)
+        for i in st:
+            for j in st:
+                if i - j in st and (i - j) < i and j < i:
+                    ans += 1
+                    # print(i, i - j, j)
+        return ans
