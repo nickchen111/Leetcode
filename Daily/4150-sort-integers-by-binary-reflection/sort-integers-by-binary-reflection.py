8111ec78eb4b4e60fdf9110b19c9eee1418e8dd8
Nick Chen <s27206383@gmail.com>
2025-12-13 02:24:38 +0800

    Time: 4 ms (90.90%) | Memory: 18 MB (23.85%) - LeetSync

--- Daily/4150-sort-integers-by-binary-reflection/sort-integers-by-binary-reflection.py
+++ Daily/4150-sort-integers-by-binary-reflection/sort-integers-by-binary-reflection.py
@@ -1,19 +1,4 @@
 class Solution:
     def sortByReflection(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        arr = []
-        for i, x in enumerate(nums):
-            tmp = []
-            while x:
-                x, d = divmod(x, 2)
-                tmp.append(d)
-            cur = 0
-            for j in range(len(tmp)):
-                cur = cur * 2 + tmp[j]
-            # cur 代表目前數字
-            arr.append((cur, i))
-        arr.sort(key=lambda t: (t[0], nums[t[1]]))
-        ans = []
-        for i in range(n):
-            ans.append(nums[arr[i][1]])
-        return ans
+        nums.sort(key=lambda x: (int(bin(x)[2:][::-1], 2), x))
+        return nums
