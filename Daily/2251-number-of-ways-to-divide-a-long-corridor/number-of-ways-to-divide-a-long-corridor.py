24d59594e04d1a8c8285afe1f31f4d6facac9fed
Nick Chen <s27206383@gmail.com>
2025-12-14 16:40:17 +0800

    Time: 255 ms (71.43%) | Memory: 22.6 MB (27.62%) - LeetSync

--- Daily/2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.py
+++ Daily/2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.py
@@ -1,26 +1,15 @@
 class Solution:
     def numberOfWays(self, corridor: str) -> int:
-        s = 0
-        for c in corridor:
-            if c == 'S':
-                s += 1
-        if s % 2 or s == 0:
-            return 0
+        pos = []
         MOD = 10 ** 9 + 7
         ans = 1
-        i = 0
-        n = len(corridor)
-        while i < n:
-            j = i
-            cnt_s = 0
-            # 先找出兩個S
-            while j < n and cnt_s < 2:
-                cnt_s += 1 if corridor[j] == 'S' else 0
-                j += 1
-            tmp = j - 1
-            while j < n and corridor[j] != 'S':
-                j += 1
-            if j != n:
-                ans *= j - tmp
-            i = j
-        return ans % MOD
+        for i, c in enumerate(corridor):
+            if c == 'S':
+                pos.append(i)
+        n = len(pos)
+        if n % 2 or n == 0:
+            return 0
+        for i in range(2, n, 2):
+            ans *= (pos[i] - pos[i - 1])
+            ans %= MOD
+        return ans
