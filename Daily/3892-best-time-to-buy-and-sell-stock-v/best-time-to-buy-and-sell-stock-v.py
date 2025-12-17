6f37723118f67eb5de8cb02465d7aaf515f2fc5c
Nick Chen <s27206383@gmail.com>
2025-12-17 20:42:32 +0800

    Time: 3931 ms (21.70%) | Memory: 96.5 MB (17.52%) - LeetSync

--- Daily/3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.py
+++ Daily/3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.py
@@ -1,21 +1,20 @@
 class Solution:
     def maximumProfit(self, prices: List[int], k: int) -> int:
         n = len(prices)
-        @cache
-        def dfs(i:int, x:int, cnt:int) -> int:
-            if i == n:
-                return 0 if x == 0 else -inf
-            if cnt == 0:
-                return 0 if x == 0 else -inf
-            # 之前狀態維持, 現在賣or買 看狀況決定可以做哪個操作
-            ans = dfs(i + 1, x, cnt)
-            if x == 0:
-                ans = max(ans, dfs(i + 1, 1, cnt) - prices[i], dfs(i + 1, 2, cnt) + prices[i])
-            if x == 1: # 之前是買 
-                ans = max(ans, dfs(i + 1, 0, cnt - 1) + prices[i])
-            else:
-                ans = max(ans, dfs(i + 1, 0, cnt - 1) - prices[i])
-            return ans
-        ret = dfs(0, 0, k)
-        dfs.cache_clear()
-        return ret
+        dp = [[[-inf] * 3 for _ in range(k + 1)] for _ in range(n + 1)]
+        dp[0][0][0] = 0
+        for i in range(n):
+            for cnt in range(k + 1):
+                for op in range(3):
+                    cur = dp[i][cnt][op]
+                    if cur == -inf:
+                        continue
+                    dp[i + 1][cnt][op] = max(dp[i + 1][cnt][op], cur)
+                    if op == 0 and cnt <= k:
+                        dp[i + 1][cnt][1] = max(dp[i + 1][cnt][1], cur - prices[i])
+                        dp[i + 1][cnt][2] = max(dp[i + 1][cnt][2], cur + prices[i])
+                    elif op == 1 and cnt + 1 <= k:
+                        dp[i + 1][cnt + 1][0] = max(dp[i + 1][cnt + 1][0], cur + prices[i])
+                    elif op == 2 and cnt + 1 <= k:
+                        dp[i + 1][cnt + 1][0] = max(dp[i + 1][cnt + 1][0], cur - prices[i])
+        return max(dp[n][cnt][0] for cnt in range(k + 1))
