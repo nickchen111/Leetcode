b1b3b02e163624476972de30adcae6b7f7f7f80f
Nick Chen <s27206383@gmail.com>
2026-01-04 18:07:32 +0800

    Create 3800. Minimum Cost to Make Two Binary Strings Equal.py

---
+++ Greedy/3800. Minimum Cost to Make Two Binary Strings Equal.py
@@ -0,0 +1,20 @@
+# TC:O(n) SC:O(n)
+class Solution:
+    def minimumCost(self, s: str, t: str, flipCost: int, swapCost: int, crossCost: int) -> int:
+        '''
+        a = 1, b = 3 -> avg = (a + b) // 2 來盡量增加swapCost
+        01, 10, 10, 10 -> 0111, 1000
+        a -> 01 對
+        b -> 10 對
+        先做串間交換 s[i] t[i]
+        '''
+        cnt = Counter(x + y for x, y in zip(s, t))
+        a = cnt["01"]
+        b = cnt["10"]
+        if a > b:
+            a, b = b, a
+        cand1 = (a + b) * flipCost
+        cand2 = (b - a) * flipCost + a * swapCost
+        avg, mod = divmod(a + b, 2)
+        cand3 = (avg - a) * crossCost + avg * swapCost + mod * flipCost
+        return min(cand1, cand2, cand3) 
