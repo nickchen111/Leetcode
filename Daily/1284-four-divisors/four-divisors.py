7f25cd6bf782cf8e1ed55a75124b80582d7896f6
Nick Chen <s27206383@gmail.com>
2026-01-04 17:47:17 +0800

    Time: 3 ms (98.95%) | Memory: 54.4 MB (5.23%) - LeetSync

---
+++ Daily/1284-four-divisors/four-divisors.py
@@ -0,0 +1,18 @@
+MAX_NUM = 100001
+divisors = [[] for _ in range(MAX_NUM)]
+divisors_sum = [0] * MAX_NUM
+for i in range(1, MAX_NUM):
+    for j in range(1, isqrt(i) + 1):
+        if i % j == 0:
+            if i // j != j:
+                divisors[i].append(i // j)
+            divisors[i].append(j)
+    if len(divisors[i]) == 4:
+        divisors_sum[i] = sum(divisors[i])
+class Solution:
+    def sumFourDivisors(self, nums: List[int]) -> int:
+        ans = 0
+        for x in nums:
+            if divisors_sum[x]:
+                ans += divisors_sum[x]
+        return ans
