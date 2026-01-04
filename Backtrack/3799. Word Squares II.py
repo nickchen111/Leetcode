14dc7150a6d6f2ade832457f496dfb84af054031
Nick Chen <s27206383@gmail.com>
2026-01-04 17:56:15 +0800

    Create 3799. Word Squares II.py

---
+++ Backtrack/3799. Word Squares II.py
@@ -0,0 +1,5 @@
+# TC:O(n ^ 4) SC:O(M) M為字串長度
+class Solution:
+    def wordSquares(self, words: List[str]) -> List[List[str]]:
+        words.sort()
+        return [[top, left, right, bottom] for top, left, right, bottom in permutations(words, 4) if top[0] == left[0] and top[3] == right[0] and bottom[0] == left[3] and bottom[3] == right[3]]
