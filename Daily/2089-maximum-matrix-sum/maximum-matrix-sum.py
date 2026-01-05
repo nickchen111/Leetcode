2047c7d1d2ead4f9d40b1fa51ea7497ab30b1417
Nick Chen <s27206383@gmail.com>
2026-01-05 20:46:56 +0800

    Time: 123 ms (6.33%) | Memory: 26.9 MB (6.96%) - LeetSync

--- Daily/2089-maximum-matrix-sum/maximum-matrix-sum.py
+++ Daily/2089-maximum-matrix-sum/maximum-matrix-sum.py
@@ -3,14 +3,12 @@ class Solution:
         '''
         就看有多少個neg odd 就選最大的那個不變
         '''
-        cnt = ans = cnt0 = 0
+        cnt = ans = 0
         mn = inf
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if matrix[i][j] < 0:
                     cnt += 1
-                elif matrix[i][j] == 0:
-                    cnt0 += 1
                 ans += abs(matrix[i][j])
                 mn = min(mn, abs(matrix[i][j]))
-        return ans if cnt % 2 == 0  or cnt0 else ans - 2 * mn
+        return ans if cnt % 2 == 0 else ans - 2 * mn
