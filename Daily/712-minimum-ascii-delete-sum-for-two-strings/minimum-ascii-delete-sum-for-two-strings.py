4978d5bebdaf916e027c06e1b59df0686fee1a74
Nick Chen <s27206383@gmail.com>
2026-01-10 14:52:57 +0800

    Time: 720 ms (22.51%) | Memory: 225.5 MB (5.10%) - LeetSync

---
+++ Daily/712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        m, n = len(s1), len(s2)
+        @cache
+        def dfs(i, j) -> int:
+            if i < 0 or j < 0:
+                return sum(ord(s2[ch]) for ch in range(j+1)) + sum(ord(s1[ch]) for ch in range(i+1))
+            if s1[i] == s2[j]:
+                return dfs(i - 1, j - 1)
+            else:
+                return min(dfs(i - 1, j) + ord(s1[i]), dfs(i, j - 1) + ord(s2[j]))
+
+        return dfs(m - 1, n - 1)
