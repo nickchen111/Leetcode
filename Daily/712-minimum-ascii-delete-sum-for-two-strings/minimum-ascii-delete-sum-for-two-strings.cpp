4cc3e7dc644c13a5d837ce4b237e373a8fc1041e
Nick Chen <s27206383@gmail.com>
2026-01-10 14:49:48 +0800

    Time: 37 ms (23.87%) | Memory: 18.3 MB (23.97%) - LeetSync

---
+++ Daily/712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimumDeleteSum(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+        vector memo(m, vector<int>(n, INT_MAX));
+        auto dfs = [&](this auto&& dfs, int i, int j) -> int {
+            if (i < 0 || j < 0) {
+                int cur = 0;
+                for (int idx = 0; idx <= i; idx++) cur += s1[idx];
+                for (int idx = 0; idx <= j; idx++) cur += s2[idx];
+                return cur;
+            }
+            if (memo[i][j] != INT_MAX) return memo[i][j];
+            int &ans = memo[i][j];
+            if (s1[i] == s2[j])
+                ans = min(ans, dfs(i - 1, j - 1));
+
+            ans = min(ans,
+                      min(dfs(i - 1, j) + s1[i],
+                          dfs(i, j - 1) + s2[j]));
+            return ans;
+        };
+
+        return dfs(m - 1, n - 1);
+    }
+};
