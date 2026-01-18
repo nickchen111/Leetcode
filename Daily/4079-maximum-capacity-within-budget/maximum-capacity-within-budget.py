c8eedb39ab2813c74dd8472a9102fd366a73aa88
Nick Chen <s27206383@gmail.com>
2026-01-18 15:34:42 +0800

    Time: 583 ms (57.40%) | Memory: 43 MB (41.39%) - LeetSync

---
+++ Daily/4079-maximum-capacity-within-budget/maximum-capacity-within-budget.py
@@ -0,0 +1,34 @@
+from bisect import bisect_right
+from math import inf
+
+class Solution:
+    def maxCapacity(self, costs: list[int], capacity: list[int], budget: int) -> int:
+        nums = sorted(zip(costs, capacity))
+        n = len(nums)
+        
+        prev_max = [0] * n
+        curr_max = 0
+        for k in range(n):
+            curr_max = max(curr_max, nums[k][1])
+            prev_max[k] = curr_max
+            
+        ans = 0
+        for i in range(n):
+            cost_i, cap_i = nums[i]
+            
+            if cost_i < budget:
+                ans = max(ans, cap_i)
+            
+            rem_budget = budget - cost_i - 1
+            if rem_budget <= 0:
+                continue
+            
+            limit = i - 1
+            if limit >= 0:
+                found_idx = bisect_right(nums, (rem_budget, inf)) - 1
+                j = min(limit, found_idx)
+                
+                if j >= 0:
+                    ans = max(ans, cap_i + prev_max[j])
+                    
+        return ans
