59a325a929564612ed0e53a201d89f693c0fda83
Nick Chen <s27206383@gmail.com>
2026-01-22 22:53:13 +0800

    Update 3814. Maximum Capacity Within Budget.py

--- Greedy/3814. Maximum Capacity Within Budget.py
+++ Greedy/3814. Maximum Capacity Within Budget.py
@@ -1,28 +1,33 @@
+# Stack
+class Solution:
+    def maxCapacity(self, costs: list[int], capacity: list[int], budget: int) -> int:
+        nums = [(cost, cap) for cost, cap in zip(costs, capacity) if cost < budget]
+        nums.sort(key=lambda x:x[0])
+        n = len(nums)
+        
+        st = [(0, 0)]
+        ans = 0
+        for i, (cost, cap) in enumerate(nums):
+            while st and st[-1][0] + cost >= budget:
+                st.pop()
+            ans = max(ans, cap + st[-1][1])
+            if cap > st[-1][1]:
+                st.append((cost, cap))
+        return ans
+
+
 # TC:O(nlogn) SC:O(n)
 class Solution:
     def maxCapacity(self, costs: list[int], capacity: list[int], budget: int) -> int:
-        nums = sorted(zip(costs, capacity))
+        nums = [(cost, cap) for cost, cap in zip(costs, capacity) if cost < budget]
+        nums.sort(key=lambda x:x[0])
         n = len(nums)
         
-        prev_max = [0] * n
-        for k in range(n):
-            prev_max[k] = max(nums[k][1], prev_max[k - 1])
-            
+        prev_max = [0] * (n + 1)
         ans = 0
-        for i in range(n):
-            cost_i, cap_i = nums[i]
-            if cost_i < budget:
-                ans = max(ans, cap_i)
-            
-            rem_budget = budget - cost_i - 1
-            if rem_budget <= 0:
-                continue
-            
-            limit = i - 1
-            if limit >= 0:
-                found_idx = bisect_right(nums, (rem_budget, inf)) - 1
-                j = min(limit, found_idx)
-                if j >= 0:
-                    ans = max(ans, cap_i + prev_max[j])
+        for i, (cost, cap) in enumerate(nums):
+            j = bisect_left(range(i), budget - cost, key=lambda j:nums[j][0])
+            ans = max(ans, cap + prev_max[j])
+            prev_max[i + 1] = max(prev_max[i], cap)
                     
         return ans
