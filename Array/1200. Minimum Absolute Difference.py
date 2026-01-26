b171bb6a8c0d96a84e193e7f197e92112adb862f
Nick Chen <s27206383@gmail.com>
2026-01-26 16:02:31 +0800

    Create 1200. Minimum Absolute Difference.py

---
+++ Array/1200. Minimum Absolute Difference.py
@@ -0,0 +1,6 @@
+# TC:O(nlogn) SC:O(1)
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        mn = min(b - a for a, b in pairwise(arr))
+        return [[a, b] for a, b in pairwise(arr) if b - a == mn]
