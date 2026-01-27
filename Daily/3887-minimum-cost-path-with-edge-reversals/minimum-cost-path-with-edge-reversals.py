a93c98317766d675160659ff170c5fc96c41ef4e
Nick Chen <s27206383@gmail.com>
2026-01-27 18:34:32 +0800

    Time: 466 ms (90.45%) | Memory: 67.7 MB (92.36%) - LeetSync

--- Daily/3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.py
+++ Daily/3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.py
@@ -3,19 +3,19 @@ class Solution:
         g = [[] for _ in range(n)]
         for x, y, w in edges:
             g[x].append((y, w))
-            g[y].append((x, 2 * w))
-        h = [(0, 0)]
-        dis = [inf] * n
-        dis[0] = 0
-
+            g[y].append((x, 2*w))
+        dist = [inf] * n
+        dist[0] = 0
+        h = [(0,0)]
         while h:
-            dist, node = heappop(h)
-            if dist > dis[node]:
+            dis, node = heappop(h)
+            if dis > dist[node]:
                 continue
             if node == n - 1:
-                return dist
+                return dis
             for y, wt in g[node]:
-                if wt + dist < dis[y]:
-                    dis[y] = wt + dist
-                    heappush(h, (wt + dist, y))
+                if wt + dis < dist[y]:
+                    dist[y] = wt + dis
+                    heappush(h, (wt + dis, y))
         return -1
+            
