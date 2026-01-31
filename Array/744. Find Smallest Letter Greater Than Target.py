fbbbf6269e330502feaf546d8a4df804ccb032b9
Nick Chen <s27206383@gmail.com>
2026-01-31 14:58:30 +0800

    Create 744. Find Smallest Letter Greater Than Target.py

---
+++ Array/744. Find Smallest Letter Greater Than Target.py
@@ -0,0 +1,4 @@
+# TC:O(n) SC:O(1)
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        return min((c for c in letters if c > target), default=letters[0])
