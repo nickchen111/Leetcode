f97839e78f6fa29a5f171a71337fb17fb02a608d
Nick Chen <s27206383@gmail.com>
2026-02-03 16:15:27 +0800

    Time: 3 ms (87.93%) | Memory: 19.4 MB (19.11%) - LeetSync

--- Daily/3952-trionic-array-i/trionic-array-i.py
+++ Daily/3952-trionic-array-i/trionic-array-i.py
@@ -8,7 +8,7 @@ class Solution:
             start = i
             while i + 1 < n and (nums[i + 1] - nums[i]) * sign > 0:
                 i += 1
-            return i - start + 1 >= 2
+            return i - start >= 1
 
         pattern = (1, -1, 1)
         i = 0
