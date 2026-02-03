a8fe6e29c2095783bf801af9dd6b608bab29c539
Nick Chen <s27206383@gmail.com>
2026-02-03 16:18:16 +0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (42.44%) - LeetSync

--- Daily/3952-trionic-array-i/trionic-array-i.py
+++ Daily/3952-trionic-array-i/trionic-array-i.py
@@ -1,17 +1,11 @@
 class Solution:
     def isTrionic(self, nums: List[int]) -> bool:
-        n = len(nums)
-        i = 0
-
-        def consume(sign: int) -> bool:
-            nonlocal i
-            start = i
-            while i + 1 < n and (nums[i + 1] - nums[i]) * sign > 0:
-                i += 1
-            return i - start > 0
-
-        pattern = (1, -1, 1)
-        i = 0
-        if all(consume(s) for s in pattern) and i == n - 1:
-            return True
-        return False
+        if nums[0] >= nums[1]:
+            return False
+        cnt = 1
+        for i in range(2, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return False
+            if (nums[i - 2] < nums[i - 1]) != (nums[i - 1] < nums[i]):
+                cnt += 1
+        return cnt == 3
