909e67e1fc5a78a57c9a9ded7e708e7070a61d87
Nick Chen <s27206383@gmail.com>
2026-02-07 23:45:31 +0800

    Time: 922 ms (5.20%) | Memory: 28.1 MB (5.21%) - LeetSync

--- Daily/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
+++ Daily/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
@@ -5,14 +5,18 @@ class Solution:
         要構造 ex aaaa or bbbb or aaaabbb
         '''
         n = len(s)
-        suf = [0] * n
-        cnt = 0
-        for i in range(n - 1, -1, -1):
-            suf[i] = cnt
-            cnt += 1 if s[i] == 'a' else 0
-        ans = min(n - cnt, cnt)
-        cnt = 0
+        suf_a = [0] * (n + 1)
+        suf_b = [0] * (n + 1)
+        cnt_a = cnt_b = pre_a = pre_b = 0
+        for i in range(n - 2, -1, -1):
+            suf_a[i] = suf_a[i + 1] + (1 if s[i + 1] == 'a' else 0)
+            suf_b[i] = suf_b[i + 1] + (1 if s[i + 1] == 'b' else 0)
+        ans = inf
         for i in range(n):
-            ans = min(ans, suf[i] + cnt)
-            cnt += 1 if s[i] == 'b' else 0
+            if s[i] == 'a':
+                ans = min(ans, pre_b + min(suf_a[i], suf_b[i]), pre_a + 1 + suf_a[i])
+                pre_a += 1
+            else:
+                ans = min(ans, pre_b + 1 + suf_b[i], pre_a + suf_a[i])
+                pre_b += 1
         return ans
