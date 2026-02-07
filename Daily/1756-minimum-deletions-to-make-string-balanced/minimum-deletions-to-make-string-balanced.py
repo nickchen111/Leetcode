6ef8c1d2d4edb7cfbe36d48f672574a5c349d3e9
Nick Chen <s27206383@gmail.com>
2026-02-07 23:46:51 +0800

    Time: 151 ms (82.19%) | Memory: 20.1 MB (55.07%) - LeetSync

--- Daily/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
+++ Daily/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
@@ -1,22 +1,14 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        '''
-        需要判斷前面後面還有多少個a or b 
-        要構造 ex aaaa or bbbb or aaaabbb
-        '''
-        n = len(s)
-        suf_a = [0] * (n + 1)
-        suf_b = [0] * (n + 1)
-        cnt_a = cnt_b = pre_a = pre_b = 0
-        for i in range(n - 2, -1, -1):
-            suf_a[i] = suf_a[i + 1] + (1 if s[i + 1] == 'a' else 0)
-            suf_b[i] = suf_b[i + 1] + (1 if s[i + 1] == 'b' else 0)
-        ans = inf
-        for i in range(n):
-            if s[i] == 'a':
-                ans = min(ans, pre_b + min(suf_a[i], suf_b[i]), pre_a + 1 + suf_a[i])
-                pre_a += 1
-            else:
-                ans = min(ans, pre_b + 1 + suf_b[i], pre_a + suf_a[i])
-                pre_b += 1
-        return ans
+        dp = 0        # 最小刪除次數
+        b_cnt = 0     # 目前保留的 b 數量
+
+        for c in s:
+            if c == 'b':
+                b_cnt += 1
+            else:  # c == 'a'
+                # 刪掉這個 a (dp + 1)
+                # 或刪掉前面所有 b (b_cnt)
+                dp = min(dp + 1, b_cnt)
+
+        return dp
