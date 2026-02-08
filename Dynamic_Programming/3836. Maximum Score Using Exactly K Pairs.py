e68993d269c41d025ada333b07ec71d8299e6707
Nick Chen <s27206383@gmail.com>
2026-02-08 17:17:50 +0800

    Update 3836. Maximum Score Using Exactly K Pairs.py

--- Dynamic_Programming/3836. Maximum Score Using Exactly K Pairs.py
+++ Dynamic_Programming/3836. Maximum Score Using Exactly K Pairs.py
@@ -15,3 +15,15 @@ class Solution:
         ans = dfs(len(nums1) - 1, len(nums2) - 1, K)
         dfs.cache_clear()
         return ans
+
+# 遞推
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], K: int) -> int:
+        n, m = len(nums1), len(nums2)
+        f = [[[-inf] * (m + 1) for _ in range(n + 1)] for _ in range(K + 1)]
+        f[0] = [[0] * (m + 1) for _ in range(n + 1)]
+        for k in range(1, K + 1):
+            for i in range(k - 1, n):
+                for j in range(k - 1, m):
+                    f[k][i + 1][j + 1] = max(f[k][i][j + 1], f[k][i + 1][j], f[k - 1][i][j] + nums1[i] * nums2[j])
+        return f[K][n][m]
