3ebd38b8d49545bc235ffdf3a25e4612240a09a3
Nick Chen <s27206383@gmail.com>
2026-02-08 17:06:08 +0800

    Create 3834. Merge Adjacent Equal Elements.py

---
+++ Stack/3834. Merge Adjacent Equal Elements.py
@@ -0,0 +1,10 @@
+# TC:O(n) SC:O(n)
+class Solution:
+    def mergeAdjacent(self, nums: List[int]) -> List[int]:
+        st = []
+        for x in nums:
+            st.append(x)
+            while len(st) > 1 and st[-1] == st[-2]:
+                st.pop()
+                st[-1] *= 2
+        return st
