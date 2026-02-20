0fb56c73b6156aebfea4f0017443cea604654877
Nick Chen <s27206383@gmail.com>
2026-02-21 01:00:00 +0800

    Create 761. Special Binary String.py

---
+++ DivideConquer/761. Special Binary String.py
@@ -0,0 +1,20 @@
+# TC:O(n^2) SC:O(n)
+class Solution:
+    def makeLargestSpecial(self, s: str) -> str:
+        if len(s) <= 2:
+            return s
+
+        substrings = []
+        diff = 0
+        start = 0
+        for i, ch in enumerate(s):
+            if ch == '1':
+                diff += 1
+            else:
+                diff -= 1
+                if diff == 0:
+                    substrings.append("1" + self.makeLargestSpecial(s[start + 1: i]) + "0")
+                    start = i + 1
+
+        substrings.sort(reverse=True)
+        return ''.join(substrings)
