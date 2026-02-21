f599a1da9b02f3bc151f2c027bcf59bd9ae70e49
Nick Chen <s27206383@gmail.com>
2026-02-21 23:34:00 +0800

    Update 3841. Palindromic Path Queries in a Tree.py

--- Data_Structure/3841. Palindromic Path Queries in a Tree.py
+++ Data_Structure/3841. Palindromic Path Queries in a Tree.py
@@ -112,3 +112,145 @@ class Solution:
                 ans.append(res & (res - 1) == 0)
 
         return ans
+
+
+# 
+class FenwickTree:
+    def __init__(self, n: int):
+        self.tree = [0] * (n + 1)
+
+    def update(self, i: int, val: int) -> None:
+        while i < len(self.tree):
+            self.tree[i] ^= val
+            i += i & -i
+
+    def pre(self, i: int) -> int:
+        res = 0
+        while i > 0:
+            res ^= self.tree[i]
+            i &= i - 1
+        return res
+
+    def query(self, l: int, r: int) -> int:
+        if r < l:
+            return 0
+        return self.pre(r) ^ self.pre(l - 1)
+
+
+class LcaBinaryLifting:
+    def __init__(self, edges: List[List[int]]):
+        n = len(edges) + 1
+        self.m = m = n.bit_length()
+        g = [[] for _ in range(n)]
+        for x, y, w in edges:
+            g[x].append((y, w))
+            g[y].append((x, w))
+
+        depth = [0] * n
+        dis = [0] * n
+        pa = [[-1] * m for _ in range(n)]
+
+        def dfs(x: int, fa: int) -> None:
+            pa[x][0] = fa
+            for y, w in g[x]:
+                if y != fa:
+                    depth[y] = depth[x] + 1
+                    dis[y] = dis[x] + w
+                    dfs(y, x)
+
+        dfs(0, -1)
+
+        for i in range(m - 1):
+            for x in range(n):
+                if (p := pa[x][i]) != -1:
+                    pa[x][i + 1] = pa[p][i]
+
+        self.depth = depth
+        self.dis = dis
+        self.pa = pa
+
+    def get_kth_ancestor(self, node: int, k: int) -> int:
+        for i in range(k.bit_length()):
+            if k >> i & 1:
+                node = self.pa[node][i]
+        return node
+
+    def get_lca(self, x: int, y: int) -> int:
+        if self.depth[x] > self.depth[y]:
+            x, y = y, x
+        y = self.get_kth_ancestor(y, self.depth[y] - self.depth[x])
+        if y == x:
+            return x
+        for i in range(self.m - 1, -1, -1):
+            px, py = self.pa[x][i], self.pa[y][i]
+            if px != py:
+                x, y = px, py
+        return self.pa[x][0]
+
+    def get_dis(self, x: int, y: int) -> int:
+        return self.dis[x] + self.dis[y] - self.dis[self.get_lca(x, y)] * 2
+
+    def upto_dis(self, x: int, d: int) -> int:
+        dx = self.dis[x]
+        for i in range(self.m - 1, -1, -1):
+            p = self.pa[x][i]
+            if p != -1 and dx - self.dis[p] <= d:
+                x = p
+        return x
+
+
+class Solution:
+    def palindromePath(self, n: int, edges: list[list[int]], s: str, queries: list[str]) -> list[bool]:
+        g = [[] for _ in range(n)]
+        for u, v in edges:
+            g[u].append(v)
+            g[v].append(u)
+
+        tin = [0] * n
+        tout = [0] * n
+        clock = 0
+
+        def dfs(u: int, p: int):
+            nonlocal clock
+            clock  += 1
+            tin[u] = clock
+            for v in g[u]:
+                if v == p:
+                    continue
+                dfs(v, u)
+            tout[u] = clock
+
+        dfs(0, -1)
+
+        ledges = []
+        for u, v in edges:
+            ledges.append([u, v, 0])
+        lca = LcaBinaryLifting(ledges)
+
+        bit = FenwickTree(n + 2)
+        cur = [1 << (ord(c) - 97) for c in s]
+
+        def range_xor(l, r, val):
+            bit.update(l, val)
+            bit.update(r + 1, val)
+
+        for i in range(n):
+            range_xor(tin[i], tout[i], cur[i])
+
+        ans = []
+        for q in queries:
+            sp = q.split()
+            if sp[0] == "update":
+                u = int(sp[1])
+                new_mask = 1 << (ord(sp[2]) - ord('a'))
+                diff = cur[u] ^ new_mask
+                if diff:
+                    range_xor(tin[u], tout[u], diff)
+                    cur[u] = new_mask
+            else:
+                a = int(sp[1])
+                b = int(sp[2])
+                L = lca.get_lca(a, b)
+                mask = bit.pre(tin[a]) ^ bit.pre(tin[b]) ^ cur[L]
+                ans.append(mask == 0 or (mask & (mask - 1)) == 0)
+        return ans
