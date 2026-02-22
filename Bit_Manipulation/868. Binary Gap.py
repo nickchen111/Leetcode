cf6390e72f1e496c456ab569341ac025c34e6a3b
Nick Chen <s27206383@gmail.com>
2026-02-22 15:14:11 +0800

    Create 868. Binary Gap.py

---
+++ Bit_Manipulation/868. Binary Gap.py
@@ -0,0 +1,10 @@
+# TC:O(logn) SC:O(1)
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        n //= (n & -n) * 2
+        ans = 0
+        while n > 0:
+            gap = (n & -n).bit_length()
+            ans = max(ans, gap)
+            n >>= gap
+        return ans
