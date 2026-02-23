7261bd872e0b81e1120307efe6675dad9e078b4f
Nick Chen <s27206383@gmail.com>
2026-02-23 11:01:38 +0800

    Create 1461. Check If a String Contains All Binary Codes of Size K.py

---
+++ Bit_Manipulation/1461. Check If a String Contains All Binary Codes of Size K.py
@@ -0,0 +1,17 @@
+# TC:O(n) SC:O(min(n - k, 2 ^ k))
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        if len(s) - k + 1 < (1 << k):
+            return False
+        MASK = (1 << k) - 1
+        has = [False] * (1 << k)
+        cnt = x = 0
+        for i, ch in enumerate(s):
+            x = (x << 1 & MASK) | int(ch)
+            if i < k - 1 or has[x]:
+                continue
+            has[x] = True
+            cnt += 1
+            if cnt == 1 << k:
+                return True
+        return False
