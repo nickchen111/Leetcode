4a7ceca047b1c70cd69eee9263f4de9ad50cf104
Nick Chen <s27206383@gmail.com>
2026-02-25 11:05:16 +0800

    Time: 3 ms (47.07%) | Memory: 14.3 MB (43.16%) - LeetSync

--- Daily/1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ Daily/1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
         auto cmp = [&](int a, int b) {
-            int ca = __builtin_popcount(a);
-            int cb = __builtin_popcount(b);
+            int ca = popcount((uint32_t)a);
+            int cb = popcount((uint32_t)b);
             return ca == cb ? a < b : ca < cb;
         };
         sort(arr.begin(), arr.end(), cmp);
