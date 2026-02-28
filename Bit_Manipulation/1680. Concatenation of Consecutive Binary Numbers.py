e164b8fe7a52c45f755a9ea67d112ee31b3a2537
Nick Chen <s27206383@gmail.com>
2026-02-28 16:41:17 +0800

    Create 1680. Concatenation of Consecutive Binary Numbers.py

---
+++ Bit_Manipulation/1680. Concatenation of Consecutive Binary Numbers.py
@@ -0,0 +1,9 @@
+# TC:O(n) SC:O(1)
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        ans = 0
+        MOD = 10 ** 9 + 7
+        for i in range(1, n + 1):
+            w = i.bit_length()
+            ans = (ans << w | i) % MOD
+        return ans
