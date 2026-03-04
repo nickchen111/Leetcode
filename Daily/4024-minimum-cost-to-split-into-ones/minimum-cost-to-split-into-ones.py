b65ff83907ed57b06904e6976680a533fdf56518
Nick Chen <s27206383@gmail.com>
2026-03-04 23:39:18 +0800

    Time: 0 ms (100.00%) | Memory: 19.2 MB (66.50%) - LeetSync

---
+++ Daily/4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCost(self, n: int) -> int:
+        return n * (n - 1) // 2 # 這個是從完全圖(每個點都與另一點聯通一條線) 推導出來的 nb
+        @cache
+        def dfs(num:int) -> int:
+            if num == 1:
+                return 0
+            ans = inf
+            for i in range(1, num // 2 + 1):
+                ans = min(ans, dfs(i) + dfs(num - i) + i * (num - i))
+            return ans
+        return dfs(n)
+                
