c9f7b8221882ea794031ff56d6705fa22de3b381
Nick Chen <s27206383@gmail.com>
2026-03-04 23:39:58 +0800

    Time: 0 ms (100.00%) | Memory: 19.2 MB (90.60%) - LeetSync

--- Daily/4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.py
+++ Daily/4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.py
@@ -1,6 +1,6 @@
 class Solution:
     def minCost(self, n: int) -> int:
-        return n * (n - 1) // 2 # 這個是從完全圖(每個點都與另一點聯通一條線) 推導出來的 nb
+        return n * (n - 1) // 2 # 這個是從完全圖(每個點都與另一點聯通一條線) 推導出來的 等價轉換 一種組合意義上的解釋
         @cache
         def dfs(num:int) -> int:
             if num == 1:
