3bf215ec5d49e37d5400a1d8c48f98c435e94208
Nick Chen <s27206383@gmail.com>
2026-03-07 17:05:17 +0800

    Create 3857. Minimum Cost to Split into Ones.py

---
+++ Math/3857. Minimum Cost to Split into Ones.py
@@ -0,0 +1,18 @@
+# Math TC:O(1) SC:O(1)
+class Solution:
+    def minCost(self, n: int) -> int:
+        return n * (n - 1) // 2 # 這個是從完全圖(每個點都與另一點聯通一條線) 推導出來的 等價轉換 一種組合意義上的解釋
+
+#DP O(n ^ 2) SC:O(n)
+class Solution:
+    def minCost(self, n: int) -> int:
+        @cache
+        def dfs(num:int) -> int:
+            if num == 1:
+                return 0
+            ans = inf
+            for i in range(1, num // 2 + 1):
+                ans = min(ans, dfs(i) + dfs(num - i) + i * (num - i))
+            return ans
+        return dfs(n)
+                
