45cb8db3cf2a8893b1d4fcfc4d3004eb566ba232
Nick Chen <s27206383@gmail.com>
2026-03-08 14:13:48 +0800

    Time: 0 ms (100.00%) | Memory: 19.1 MB (99.21%) - LeetSync

--- Daily/2107-find-unique-binary-string/find-unique-binary-string.py
+++ Daily/2107-find-unique-binary-string/find-unique-binary-string.py
@@ -1,5 +1,12 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
+        # cantor 對角線
+        ans = [''] * len(nums)
+        for i, s in enumerate(nums):
+            ans[i] = '1' if s[i] == '0' else '0'
+        return "".join(ans)
+
+        #暴力枚舉
         st = {int(s,2) for s in nums}
         ans = 0
         while ans in st:
