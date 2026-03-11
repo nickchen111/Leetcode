ed754f93059873a392c688bf6017e5524a5bf9d8
Nick Chen <s27206383@gmail.com>
2026-03-11 10:54:10 +0800

    Create 1009. Complement of Base 10 Integer.py

---
+++ Bit_Manipulation/1009. Complement of Base 10 Integer.py
@@ -0,0 +1,8 @@
+# TC:O(1) SC:O(1)
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        # 取反
+        if n == 0:
+            return 1
+        w = n.bit_length()
+        return ((1 << w) - 1) ^ n
