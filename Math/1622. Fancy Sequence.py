772616423cd80e8311c2ed3877226a4f3fc8514f
Nick Chen <s27206383@gmail.com>
2026-03-15 21:31:01 +0800

    Create 1622. Fancy Sequence.py

---
+++ Math/1622. Fancy Sequence.py
@@ -0,0 +1,30 @@
+# TC:O(logMOD) SC:O(q) q = append調用次數
+MOD = 10 ** 9 + 7
+class Fancy:
+
+    def __init__(self):
+        self.add = 0
+        self.mul = 1
+        self.vals = []
+    def append(self, val: int) -> None:
+        self.vals.append(((val - self.add) * pow(self.mul, -1, MOD)) % MOD)
+
+    def addAll(self, inc: int) -> None:
+        self.add += inc
+
+    def multAll(self, m: int) -> None:
+        self.add = self.add * m % MOD
+        self.mul = self.mul * m % MOD
+
+    def getIndex(self, idx: int) -> int:
+        if idx >= len(self.vals):
+            return -1
+        return (self.vals[idx] * self.mul + self.add) % MOD
+
+
+# Your Fancy object will be instantiated and called as such:
+# obj = Fancy()
+# obj.append(val)
+# obj.addAll(inc)
+# obj.multAll(m)
+# param_4 = obj.getIndex(idx)
