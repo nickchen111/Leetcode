d37688352ef3926f95e5a1934dc0e0abbb7e14b8
Nick Chen <s27206383@gmail.com>
2026-03-16 15:29:18 +0800

    Update 3872. Longest Arithmetic Sequence After Changing At Most One Element.py

--- Dynamic_Programming/3872. Longest Arithmetic Sequence After Changing At Most One Element.py
+++ Dynamic_Programming/3872. Longest Arithmetic Sequence After Changing At Most One Element.py
@@ -30,3 +30,43 @@ class Solution:
                 R = suf[i + 1][1] if suf[i + 1][0] == d else 1
                 ans = max(ans, L + 1 + R)
         return ans
+
+# 另一種思維的寫法 
+class Solution:
+    def calc(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        pre = [0] * n
+        pre[0] = 1
+        pre[1] = 2
+        for i in range(2, n):
+            if nums[i - 2] + nums[i] == nums[i - 1] * 2:
+                pre[i] = pre[i - 1] + 1
+            else:
+                pre[i] = 2
+        return pre
+
+    def longestArithmetic(self, nums: List[int]) -> int:
+        n = len(nums)
+        pre = self.calc(nums)
+        ans = max(pre) + 1
+        if ans >= n:
+            return n
+
+        suf = self.calc(nums[::-1])[::-1]
+
+        for i in range(1, n - 1):
+            d2 = nums[i + 1] - nums[i - 1]
+            if d2 % 2:
+                continue
+
+            ok_left = i > 1 and nums[i - 1] - nums[i - 2] == d2 // 2
+            ok_right = i + 2 < n and nums[i + 2] - nums[i + 1] == d2 // 2
+
+            if ok_left and ok_right:
+                ans = max(ans, pre[i - 1] + 1 + suf[i + 1])
+            elif ok_left:
+                ans = max(ans, pre[i - 1] + 2)
+            elif ok_right:
+                ans = max(ans, suf[i + 1] + 2)
+
+        return ans
