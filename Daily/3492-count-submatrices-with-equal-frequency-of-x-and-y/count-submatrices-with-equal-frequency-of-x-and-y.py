1b251bca279e1fa01a6fc95e1f3ddbac8504e04b
Nick Chen <s27206383@gmail.com>
2026-03-19 11:30:11 +0800

    Time: 307 ms (92.47%) | Memory: 103.6 MB (89.25%) - LeetSync

--- Daily/3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.py
+++ Daily/3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.py
@@ -1,20 +1,14 @@
 class Solution:
     def numberOfSubmatrices(self, grid: List[List[str]]) -> int:
-        col_sum = [0] * len(grid[0])
+        col_cnt = [[0, 0] for _ in grid[0]]
         ans = 0
-        col_check = [False] * len(grid[0])
         for row in grid:
-            s = 0
-            flag = False
-            for j, x in enumerate(row):
-                if x == 'X':
-                    flag = True
-                    col_sum[j] += 1
-                elif x == 'Y':
-                    col_sum[j] -= 1
-                flag = flag or col_check[j]
-                col_check[j] = (col_check[j - 1] if j - 1 >= 0 else False) or flag 
-                s += col_sum[j]
-                if s == 0 and col_check[j]:
+            s0 = s1 = 0
+            for j, c in enumerate(row):
+                if c != '.':
+                    col_cnt[j][ord(c) & 1] += 1
+                s0 += col_cnt[j][0]
+                s1 += col_cnt[j][1]
+                if s0 == s1 > 0:
                     ans += 1
         return ans
