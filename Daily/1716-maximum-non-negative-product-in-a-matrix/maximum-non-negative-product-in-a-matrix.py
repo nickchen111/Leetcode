a4af1e6ba01cfbd0ccc127205797bfc7dd2e6df6
Nick Chen <s27206383@gmail.com>
2026-03-23 11:39:16 +0800

    Time: 6 ms (51.75%) | Memory: 21.6 MB (14.91%) - LeetSync

--- Daily/1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.py
+++ Daily/1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.py
@@ -1,27 +1,22 @@
 class Solution:
     def maxProductPath(self, grid: List[List[int]]) -> int:
-        MOD = 10 ** 9 + 7
-        m,n = len(grid), len(grid[0])
         @cache
-        def dfs(i:int, j:int, k:int) -> int:
-            if i == m - 1 and j == n - 1:
-                return grid[i][j]
-            mx, mn = -inf, inf
+        def dfs(i: int, j: int) -> Tuple[int, int]:
             x = grid[i][j]
-            if i + 1 < m:
-                down_mx, down_mn = dfs(i + 1, j, 0), dfs(i + 1, j, 1)
-                if k:
-                    mx = max(mx, x * down_mx, x * down_mn)
-                else:
-                    mn = min(mn, x * down_mx, x * down_mn)
-            if j + 1 < n:
-                right_mx, right_mn = dfs(i, j + 1, 0), dfs(i, j + 1, 1)
-                if k:
-                    mx = max(mx, x * right_mx, x * right_mn)
-                else:
-                    mn = min(mn, x * right_mx, x * right_mn)
-            return mx if k else mn
-        ans = max(dfs(0, 0, 0), dfs(0, 0, 1))
-        return (ans % MOD) if ans >= 0 else -1
-        
+            if i == j == 0:
+                return x, x
 
+            res_min, res_max = inf, -inf
+            if i > 0:
+                mn, mx = dfs(i - 1, j)
+                res_min = min(mn * x, mx * x)
+                res_max = max(mn * x, mx * x)
+            if j > 0:
+                mn, mx = dfs(i, j - 1)
+                res_min = min(res_min, mn * x, mx * x)
+                res_max = max(res_max, mn * x, mx * x)
+
+            return res_min, res_max
+
+        ans = dfs(len(grid) - 1, len(grid[0]) - 1)[1]
+        return -1 if ans < 0 else ans % 1_000_000_007
