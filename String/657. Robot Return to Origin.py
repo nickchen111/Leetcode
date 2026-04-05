dfcc9512cc540626fe16634656522d1108b1fd80
Nick Chen <s27206383@gmail.com>
2026-04-05 16:14:48 +0800

    Create 657. Robot Return to Origin.py

---
+++ String/657. Robot Return to Origin.py
@@ -0,0 +1,6 @@
+# TC:O(n) SC:O(1)
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        return moves.count('R') == moves.count('L') and \
+               moves.count('U') == moves.count('D')
+
