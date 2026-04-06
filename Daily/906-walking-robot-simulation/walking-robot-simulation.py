97f7874b3a5540f08bd95adfd1bef53307cfb1a7
Nick Chen <s27206383@gmail.com>
2026-04-06 15:28:50 +0800

    Time: 43 ms (37.52%) | Memory: 23.7 MB (93.49%) - LeetSync

---
+++ Daily/906-walking-robot-simulation/walking-robot-simulation.py
@@ -0,0 +1,21 @@
+class Solution:
+    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+        dirs = {0:(0,1), 1:(1,0), 2:(0,-1), 3:(-1,0)}
+        dir = 0
+        MOD = 4
+        obs = {tuple(o) for o in obstacles}
+        ans = x = y = 0
+        for c in commands:
+            if c == -1: # turn right
+                dir = (dir + 1) % MOD
+            elif c == -2: # turn left
+                dir = (dir - 1 + MOD) % MOD
+            else:
+                # 一步一步走 否則可能超過obstacle
+                for i in range(1, c + 1):
+                    nx, ny = x + dirs[dir][0], y + dirs[dir][1]
+                    if (nx, ny) in obs:
+                        break
+                    x, y = nx, ny
+            ans = max(ans, x * x + y * y)
+        return ans
