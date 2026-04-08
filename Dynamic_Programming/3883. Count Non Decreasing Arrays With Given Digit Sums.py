cff46525341856fba7d7972dbf3ad4104e8e8f00
Nick Chen <s27206383@gmail.com>
2026-04-08 23:17:32 +0800

    Update 3883. Count Non Decreasing Arrays With Given Digit Sums.py

--- Dynamic_Programming/3883. Count Non Decreasing Arrays With Given Digit Sums.py
+++ Dynamic_Programming/3883. Count Non Decreasing Arrays With Given Digit Sums.py
@@ -1,4 +1,35 @@
 # Two Pointer 可以將時間優化到n * 360左右 也就是在嘗試放一個數字的時候對他前面的那段做前綴和 後續用雙指針不斷往後走
+MOD = 1_000_000_007
+MX = 5001
+MAX_DIGIT_SUM = 31
+sum_to_nums = [[] for _ in range(MAX_DIGIT_SUM + 1)]
+dig_sum = [0] * MX
+for x in range(MX):
+    dig_sum[x] = dig_sum[x // 10] + x % 10
+    sum_to_nums[dig_sum[x]].append(x)
+
+class Solution:
+    def countArrays(self, digitSum: List[int]) -> int:
+        f = [0] * MX  # f[x] 表示以 x 结尾的有效数组的个数
+        f[0] = 1
+        pre = 0
+
+        for cur in digitSum:
+            if cur > MAX_DIGIT_SUM:
+                return 0
+            a = sum_to_nums[pre]
+            j, m = 0, len(a)
+            s = 0
+            for x in sum_to_nums[cur]:
+                # 有效数组的前一个数只要 <= x 就行
+                while j < m and a[j] <= x:
+                    s += f[a[j]]
+                    j += 1
+                # s 现在就是以 x 结尾的有效数组的个数
+                f[x] = s % MOD
+            pre = cur
+
+        return sum(f[x] for x in sum_to_nums[pre]) % MOD
 # prefix: TC:O(nU) SC:O(U)
 MOD = 10**9 + 7
 MAX_V = 5000
