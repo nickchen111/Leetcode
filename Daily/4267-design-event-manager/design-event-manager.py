349775e8515f05fd3ba4f4fbcfb98bdaea21898b
Nick Chen <s27206383@gmail.com>
2026-04-18 16:33:05 +0800

    Time: 255 ms (82.90%) | Memory: 85.4 MB (71.85%) - LeetSync

---
+++ Daily/4267-design-event-manager/design-event-manager.py
@@ -0,0 +1,27 @@
+class EventManager:
+    def __init__(self, events: List[List[int]]):
+        self.id_to_priority = {}
+        self.h = []
+        for eid, priority in events:
+            self.id_to_priority[eid] = priority
+            self.h.append((-priority, eid))
+        heapify(self.h)
+
+    def updatePriority(self, eventId: int, newPriority: int) -> None:
+        self.id_to_priority[eventId] = newPriority
+        heappush(self.h, (-newPriority, eventId))
+
+    def pollHighest(self) -> int:
+        while self.h:
+            priority, eid = heappop(self.h)
+            if self.id_to_priority.get(eid, -1) == -priority:
+                del self.id_to_priority[eid]
+                return eid
+        return -1
+            
+
+
+# Your EventManager object will be instantiated and called as such:
+# obj = EventManager(events)
+# obj.updatePriority(eventId,newPriority)
+# param_2 = obj.pollHighest()
