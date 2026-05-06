98d909990110efaf66224f7d6fa4f7f63328c196
Nick Chen <s27206383@gmail.com>
2026-05-06 20:34:17 +0800

    Time: 8 ms (81.33%) | Memory: 56.6 MB (90.17%) - LeetSync

---
+++ Daily/1972-rotating-the-box/rotating-the-box.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        int m = box.size();
+        int n = box[0].size();
+        vector<vector<char>> res(n, vector<char>(m,'.'));
+        for(int i = 0; i < m; i++) {
+            int left = n-1, right = n-1;
+            while(left >= 0) {
+                if(box[i][left] == '*') {
+                    res[left][m-1-i] = '*';
+                    right = left - 1;
+                }
+                else if(box[i][left] == '#') {
+                    res[right][m-1-i] = '#';
+                    right--;
+                }
+                
+                left--;
+            }
+        }
+
+        return res;
+
+    }
+};
