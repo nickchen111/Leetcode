032c50fc8b65f98456688f86d095d579d88c0d2f
Nick Chen <s27206383@gmail.com>
2026-05-17 18:34:59 +0800

    Time: 1146 ms (82.38%) | Memory: 31.2 MB (77.17%) - LeetSync

--- Daily/4257-sum-of-sortable-integers/sum-of-sortable-integers.py
+++ Daily/4257-sum-of-sortable-integers/sum-of-sortable-integers.py
@@ -1,39 +1,80 @@
 class Solution:
-    def sortableIntegers(self, nums: List[int]) -> int:
+    def sortableIntegers(self, nums: list[int]) -> int:
+        '''
+        一定要是類似這種的才能rotate
+        8,9,10,1,2,3,4,5,6
+        也就是第二段的尾不能超過第一段的頭
+        8,9,19,4,5,6,1,2,3 這樣就不行 一定要是兩段
+        兩段都是non-decreasing
+        解法就會是枚舉他的因數有哪些 這些因數都去跑一次拆解看是否符合狀況
+        做法:
+        1. 先做完因式分解
+        2. 枚舉因式
+            2.1取每段subarray 都是因式長度
+            2.2這段subarray 要馬都是排序好的 要馬是兩段式 滿足第二段的尾不能超過第一段的頭
+            2.3 記錄這段的最大數字 到時候要比下一段的頭 <=
+            2.4將結果記錄下來有幾個
+        每段確認 cur_head <= prev_tail;
+        內部確認 1. 兩段式 or 一段 2. 兩段的話要確認 1st head >= 2nd tail
+        '''
+
         n = len(nums)
-        next_dec = [0] * n  # nums[next_dec[i]] > nums[next_dec[i] + 1]
-        next_dec[-1] = p = n
-        # 对于每个 i，记录下一个递减的位置
-        for i in range(n - 2, -1, -1):
-            if nums[i] > nums[i + 1]:
-                p = i
-            next_dec[i] = p
-
-        def solve(k: int) -> None:
-            last_max = 0  # 上一段的最大值
-            for r in range(k - 1, n, k):
-                l = r - k + 1
-                m = next_dec[l]
-                if m >= r:
-                    # [l, r] 是递增的，最小值为 nums[l]，最大值为 nums[r]
-                    # 最小值必须 >= 上一段的最大值
-                    if nums[l] < last_max:
-                        return
-                    last_max = nums[r]
-                else:
-                    # [l, m] 是第一段，[m+1, r] 是第二段
-                    # 第二段必须是递增的，且第二段的最小值必须 >= 上一段的最大值，且第二段的最大值必须 <= 第一段的最小值
-                    if next_dec[m + 1] < r or nums[m + 1] < last_max or nums[r] > nums[l]:
-                        return
-                    last_max = nums[m]
-            nonlocal ans
-            ans += k  # 满足要求
-
+        
+        # 找所有 n 的因數
+        divisors = []
+        for i in range(1, isqrt(n) + 1):
+            if n % i == 0:
+                divisors.append(i)
+                if i != n // i:
+                    divisors.append(n // i)
+        
         ans = 0
-        # 枚举 n 的因子 k
-        for k in range(1, isqrt(n) + 1):
-            if n % k == 0:
-                solve(k)
-                if k * k < n:
-                    solve(n // k)
+        for k in divisors:
+            prev_max = -inf
+            ok = True
+            
+            for start in range(0, n, k):
+                end = start + k  # 這段是 nums[start:end]
+                
+                # 找下降點數量
+                drop = -1
+                for i in range(start, end - 1):
+                    if nums[i] > nums[i + 1]:
+                        if drop != -1:   # 已經有一個了 → 超過一個
+                            ok = False
+                            break
+                        drop = i
+                if not ok:
+                    break
+                
+                # 有下降點時，還要確認 nums[end-1] <= nums[start]
+                if drop != -1 and nums[end - 1] > nums[start]:
+                    ok = False
+                    break
+                
+                # 這段 rotate 後的 min / max
+                # min 是整段最小值，max 是整段最大值
+                # 但因為這段是 sorted 的 rotation，min = nums[drop+1]（若有下降點）或 nums[start]
+                # max = nums[drop]（若有下降點）或 nums[end-1]
+                if drop == -1:
+                    seg_min = nums[start]
+                    seg_max = nums[end - 1]
+                else:
+                    seg_min = nums[drop + 1]
+                    seg_max = nums[drop]
+                
+                # 段間條件
+                if seg_min < prev_max:
+                    ok = False
+                    break
+                prev_max = seg_max
+            
+            if ok:
+                ans += k
+        
         return ans
+                    
+                    
+                    
+                
+                
