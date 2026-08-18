d781439b082c80fafb05e4fdbc26c245f00ef359
Nick Chen <s27206383@gmail.com>
2026-08-19 00:34:38 +0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (35.66%) - LeetSync

--- Daily/3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.py
+++ Daily/3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.py
@@ -1,20 +1,15 @@
 class Solution:
+    def f(self, nums: List[int], x: int) -> int:
+        return -1 if x in nums else x
+
     def largestInteger(self, nums: List[int], k: int) -> int:
-        mp = defaultdict(int)
-        mp2 = defaultdict(int)
-        n = len(nums)
-        i = 0
-        for j in range(n):
-            mp[nums[j]] += 1
-            if j - i + 1 == k:
-                for key,v in mp.items():
-                    mp2[key] += 1
-                mp[nums[i]] -= 1
-                if mp[nums[i]] == 0:
-                    del mp[nums[i]]
-                i += 1
-        ans = -1
-        for key, v in mp2.items():
-            if v == 1:
-                ans = max(ans, key)
-        return ans
+        if k == len(nums):
+            return max(nums)
+        if k == 1:
+            ans = -1
+            for x, c in Counter(nums).items():
+                if c == 1:
+                    ans = max(ans, x)
+            return ans
+        # nums[0] 不能出现在其他地方，nums[-1] 同理
+        return max(self.f(nums[1:], nums[0]), self.f(nums[:-1], nums[-1]))
